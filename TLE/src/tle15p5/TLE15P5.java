e9d028c46b06730e35846c1b6defe15f844a8fda
wesley-a-leung <wesley.a.leung@gmail.com>
2017-04-23 01:51:12 -0400

    Completed TLE15P5 (Prefix Array)

--- TLE/src/tle15p5/TLE15P5.java
+++ TLE/src/tle15p5/TLE15P5.java
@@ -1,4 +1,4 @@
-package template;
+package tle15p5;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -10,8 +10,8 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
-public class Template {
-	private static Template o = new Template();
+public class TLE15P5 {
+	private static TLE15P5 o = new TLE15P5();
 	public class Reader {
 		private BufferedReader in;
 		private StringTokenizer st;
@@ -48,10 +48,48 @@ public class Template {
 		} // nextLine method
 	} // Reader class
 	
+	private static final int MOD = (int)(1e9 + 7);
+	
 	public static void main(String[] args) throws IOException {
 		Reader in = o.new Reader(System.in);
 		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
-		// TODO INSERT CODE HERE
+		int N = in.nextInt();
+		long[] arr = new long[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+		int M = in.nextInt();
+		long[] output = new long[N];
+		long multiplier = -1L;
+		for (int i = 0; i < N; i++) {
+			if (multiplier == -1) {
+				multiplier = 1;
+			} else {
+				multiplier = (multiplier * (i + M - 1)) % MOD;
+				multiplier = divMod(multiplier, (i));
+			}
+			for (int j = i; j < N; j++) {
+				output[j] = (output[j] + multiplier * arr[j - i]) % MOD;
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			out.print(output[i] + " ");
+		}
+		out.println();
 		out.close();
+	}
+	
+	private static long divMod(long i, long j) {
+		return i * pow(j, MOD - 2) % MOD;
+	}
+
+	private static long pow(long base, long pow) {
+		if (pow == 0)
+			return 1;
+		if (pow == 1)
+			return base;
+		if (pow % 2 == 0)
+			return pow(base * base % MOD, pow / 2);
+		return base * pow(base * base % MOD, pow / 2) % MOD;
 	}
 }
