832046207adae84c2d9d81390f039d3a5582c533
wesley-a-leung <wesley.a.leung@gmail.com>
2017-04-25 15:41:28 -0400

    Completed CCC15S3 in Java and CCO10P3 in C++ (partial points for Java)

---
+++ CCC/src/ccc15s3/CCC15S3.java
@@ -0,0 +1,74 @@
+package ccc15s3;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class CCC15S3 {
+	private static CCC15S3 o = new CCC15S3();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	public static void main(String[] args) throws IOException {
+		Reader in = o.new Reader(System.in);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		int G = in.nextInt();
+		int P = in.nextInt();
+		TreeSet<Integer> gates = new TreeSet<Integer>();
+		for (int i = 1; i <= G; i++) {
+			gates.add(i);
+		}
+		int count = 0;
+		for (int i = 0; i < P; i++) {
+			Integer x = gates.floor(in.nextInt());
+			if (x != null) {
+				count++;
+				gates.remove(x);
+			} else {
+				break;
+			}
+		}
+		out.println(count);
+		out.close();
+	}
+}
---
+++ CCO C++/cco10p3/cco10p3.cpp
@@ -0,0 +1,85 @@
+/*
+ * cco10p3.cpp
+ *
+ *  Created on: Apr 25, 2017
+ *      Author: wleung
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int SIZE = 1000002;
+
+int N;
+int num = 0;
+char c[SIZE];
+int X[SIZE];
+int R[SIZE];
+int K[SIZE];
+int idToRating[SIZE];
+int ratingToId[SIZE];
+vector<int> ratings;
+long long FenwickTree[SIZE];
+unordered_map<int, int> ratingMap;
+
+long long rsq(int ind) {
+	long long sum = 0;
+	while (ind > 0) {
+		sum += FenwickTree[ind];
+		ind -= ind & (-ind);
+	}
+	return sum;
+}
+
+void update(int ind, int value) {
+	while (ind < SIZE) {
+		FenwickTree[ind] += value;
+		ind += ind & (-ind);
+	}
+}
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%c", &c[i]);
+		if (c[i] == 'N' || c[i] == 'M') {
+			scanf("%d %d", &X[i], &R[i]);
+			ratings.push_back(R[i]);
+		} else if (c[i] == 'Q'){
+			scanf("%d", &K[i]);
+		} else {
+			i--;
+		}
+	}
+	sort(ratings.begin(), ratings.end());
+	for (int i = 0; i < ratings.size(); i++) {
+		ratingMap[ratings[i]] = i + 1;
+	}
+	for (int i = 0; i < N; i++) {
+		if (c[i] == 'N') {
+			idToRating[X[i]] = ratingMap[R[i]];
+			ratingToId[ratingMap[R[i]]] = X[i];
+			update(ratingMap[R[i]], 1);
+			num++;
+		} else if (c[i] == 'M') {
+			// no need to remove old ratingToId reference
+			update(idToRating[X[i]], -1);
+			update(ratingMap[R[i]], 1);
+			idToRating[X[i]] = ratingMap[R[i]];
+			ratingToId[ratingMap[R[i]]] = X[i];
+		} else {
+			int lo = 1;
+			int hi = ratings.size();
+			while (lo <= hi) {
+				int mid = lo + (hi - lo) / 2;
+				if (rsq(mid) > num - K[i]) {
+					hi = mid - 1;
+				} else {
+					lo = mid + 1;
+				}
+			}
+			printf("%d\n", ratingToId[lo]);
+		}
+	}
+}
