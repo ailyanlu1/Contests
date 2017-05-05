c049deac6e006538296179b61288b1386a42cf99
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-04 23:18:43 -0400

    Fixed error in comment

--- TLE/tle16c7p4_abstract_problem/TLE16C7P4.java
+++ TLE/tle16c7p4_abstract_problem/TLE16C7P4.java
@@ -56,7 +56,7 @@ public class TLE16C7P4 {
 			long x = in.nextLong();
 			int count = 0;
 			while (x != 0) {
-				if ((x & 1) != 0) { // same as x != 0
+				if ((x & 1) != 0) { // same as x % 2 != 0
 					if ((x & 2) != 0 && x != 3) {
 						x++;
 					} else {
