cbb85d65ddfb143a48852d92f3d023e8180b42a5
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-05 13:54:27 -0400

    DMPG15S3 (Zen Garden) Completed

---
+++ DMOJ/dmpg15s3_zen_garden/DMPG15S3.java
@@ -0,0 +1,72 @@
+package dmpg15s3_zen_garden;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class DMPG15S3 {
+	private static DMPG15S3 o = new DMPG15S3();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	private static Reader in = o.new Reader(System.in);
+	private static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws IOException {
+		int N = in.nextInt();
+		int[] flowers = new int[N];
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			int a = in.nextInt();
+			flowers[i] = a;
+			sum += a;
+		}
+		int M = in.nextInt();
+		for (int i = 0; i < M; i++) {
+			int a = in.nextInt();
+			int d = in.nextInt();
+			sum -= Integer.min(Integer.min(flowers[a], flowers[a - 1]), d);
+		}
+		out.println(sum);
+		out.close();
+	}
+}
