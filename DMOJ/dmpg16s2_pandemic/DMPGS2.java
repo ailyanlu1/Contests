57fed4236c59288a3fb106169ce33a490c00e13d
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-06 00:37:32 -0400

    DMPG16S2 (Pandemic) Completed

---
+++ DMOJ/dmpg16s2_pandemic/DMPGS2.java
@@ -0,0 +1,69 @@
+package dmpg16s2_pandemic;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class DMPGS2 {
+	private static DMPGS2 o = new DMPGS2();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	private static Reader in = o.new Reader(System.in);
+	private static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws IOException {
+		long N = in.nextLong();
+		long K = in.nextInt();
+		long D = in.nextInt();
+		long ans = 0;
+		while (D > 0) {
+			long mod = N % K;
+			if (mod != 0) ans += mod;
+			N /= K;
+			D--;
+		}
+		ans += N;
+		out.println(ans);
+		out.close();
+	}
+}
