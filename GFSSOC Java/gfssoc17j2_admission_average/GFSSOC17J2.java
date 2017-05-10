363831da8441eb8bc12dd97f65e3713e6a38917d
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-10 00:31:04 -0400

    GFSSOC17J2 (Admission Average) Completed

---
+++ GFSSOC Java/gfssoc17j2_admission_average/GFSSOC17J2.java
@@ -0,0 +1,66 @@
+package gfssoc17j2_admission_average;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class GFSSOC17J2 {
+	private static GFSSOC17J2 o = new GFSSOC17J2();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	private static Reader in = o.new Reader(System.in);
+	private static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws IOException {
+		double avg = 0;
+		for (int i = 0; i < 6; i++) {
+			avg += in.nextDouble();
+		}
+		avg /= 6.0;
+		avg += in.nextDouble();
+		double req = in.nextDouble();
+		if (avg >= req) out.println("yes");
+		else out.println("no");
+		out.close();
+	}
+}
