9f06c622e6aa15ce4501223e35e5c337599b7084
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-11 11:41:13 -0400

    DMOPC15C5P5 (World Line Convergence) Completed

---
+++ DMOJ Java/dmopc15c5p5_world_line_convergence/DMOPC15C5P5.java
@@ -0,0 +1,395 @@
+package dmopc15c5p5_world_line_convergence;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+import java.util.Stack;
+
+public class DMOPC15C5P5 {
+	private static DMOPC15C5P5 o = new DMOPC15C5P5();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	public class Bag<Item> implements Iterable<Item> {
+	    private Node<Item> first;    // beginning of bag
+	    private int n;               // number of elements in bag
+
+	    // helper linked list class
+	    private class Node<Item> {
+	        private Item item;
+	        private Node<Item> next;
+	    }
+
+	    /**
+	     * Initializes an empty bag.
+	     */
+	    public Bag() {
+	        first = null;
+	        n = 0;
+	    }
+
+	    /**
+	     * Returns true if this bag is empty.
+	     *
+	     * @return {@code true} if this bag is empty;
+	     *         {@code false} otherwise
+	     */
+	    public boolean isEmpty() {
+	        return first == null;
+	    }
+
+	    /**
+	     * Returns the number of items in this bag.
+	     *
+	     * @return the number of items in this bag
+	     */
+	    public int size() {
+	        return n;
+	    }
+
+	    /**
+	     * Adds the item to this bag.
+	     *
+	     * @param  item the item to add to this bag
+	     */
+	    public void add(Item item) {
+	        Node<Item> oldfirst = first;
+	        first = new Node<Item>();
+	        first.item = item;
+	        first.next = oldfirst;
+	        n++;
+	    }
+
+
+	    /**
+	     * Returns an iterator that iterates over the items in this bag in arbitrary order.
+	     *
+	     * @return an iterator that iterates over the items in this bag in arbitrary order
+	     */
+	    public Iterator<Item> iterator()  {
+	        return new ListIterator<Item>(first);  
+	    }
+
+	    // an iterator, doesn't implement remove() since it's optional
+	    private class ListIterator<Item> implements Iterator<Item> {
+	        private Node<Item> current;
+
+	        public ListIterator(Node<Item> first) {
+	            current = first;
+	        }
+
+	        public boolean hasNext()  { return current != null;                     }
+	        public void remove()      { throw new UnsupportedOperationException();  }
+
+	        public Item next() {
+	            if (!hasNext()) throw new NoSuchElementException();
+	            Item item = current.item;
+	            current = current.next; 
+	            return item;
+	        }
+	    }
+	}
+	
+	public class Digraph {
+	    private final String NEWLINE = System.getProperty("line.separator");
+
+	    private final int V;           // number of vertices in this digraph
+	    private int E;                 // number of edges in this digraph
+	    private Bag<Integer>[] adj;    // adj[v] = adjacency list for vertex v
+	    private int[] indegree;        // indegree[v] = indegree of vertex v
+	    
+	    /**
+	     * Initializes an empty digraph with <em>V</em> vertices.
+	     *
+	     * @param  V the number of vertices
+	     * @throws IllegalArgumentException if {@code V < 0}
+	     */
+	    public Digraph(int V) {
+	        if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+	        this.V = V;
+	        this.E = 0;
+	        indegree = new int[V];
+	        adj = (Bag<Integer>[]) new Bag[V];
+	        for (int v = 0; v < V; v++) {
+	            adj[v] = new Bag<Integer>();
+	        }
+	    }
+
+	    /**
+	     * Initializes a new digraph that is a deep copy of the specified digraph.
+	     *
+	     * @param  G the digraph to copy
+	     */
+	    public Digraph(Digraph G) {
+	        this(G.V());
+	        this.E = G.E();
+	        for (int v = 0; v < V; v++)
+	            this.indegree[v] = G.indegree(v);
+	        for (int v = 0; v < G.V(); v++) {
+	            // reverse so that adjacency list is in same order as original
+	            Stack<Integer> reverse = new Stack<Integer>();
+	            for (int w : G.adj[v]) {
+	                reverse.push(w);
+	            }
+	            for (int w : reverse) {
+	                adj[v].add(w);
+	            }
+	        }
+	    }
+	        
+	    /**
+	     * Returns the number of vertices in this digraph.
+	     *
+	     * @return the number of vertices in this digraph
+	     */
+	    public int V() {
+	        return V;
+	    }
+
+	    /**
+	     * Returns the number of edges in this digraph.
+	     *
+	     * @return the number of edges in this digraph
+	     */
+	    public int E() {
+	        return E;
+	    }
+
+
+	    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+	    private void validateVertex(int v) {
+	        if (v < 0 || v >= V)
+	            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+	    }
+
+	    /**
+	     * Adds the directed edge v→w to this digraph.
+	     *
+	     * @param  v the tail vertex
+	     * @param  w the head vertex
+	     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+	     */
+	    public void addEdge(int v, int w) {
+	        validateVertex(v);
+	        validateVertex(w);
+	        adj[v].add(w);
+	        indegree[w]++;
+	        E++;
+	    }
+
+	    /**
+	     * Returns the vertices adjacent from vertex {@code v} in this digraph.
+	     *
+	     * @param  v the vertex
+	     * @return the vertices adjacent from vertex {@code v} in this digraph, as an iterable
+	     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+	     */
+	    public Iterable<Integer> adj(int v) {
+	        validateVertex(v);
+	        return adj[v];
+	    }
+
+	    /**
+	     * Returns the number of directed edges incident from vertex {@code v}.
+	     * This is known as the <em>outdegree</em> of vertex {@code v}.
+	     *
+	     * @param  v the vertex
+	     * @return the outdegree of vertex {@code v}               
+	     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+	     */
+	    public int outdegree(int v) {
+	        validateVertex(v);
+	        return adj[v].size();
+	    }
+
+	    /**
+	     * Returns the number of directed edges incident to vertex {@code v}.
+	     * This is known as the <em>indegree</em> of vertex {@code v}.
+	     *
+	     * @param  v the vertex
+	     * @return the indegree of vertex {@code v}               
+	     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+	     */
+	    public int indegree(int v) {
+	        validateVertex(v);
+	        return indegree[v];
+	    }
+
+	    /**
+	     * Returns the reverse of the digraph.
+	     *
+	     * @return the reverse of the digraph
+	     */
+	    public Digraph reverse() {
+	        Digraph reverse = new Digraph(V);
+	        for (int v = 0; v < V; v++) {
+	            for (int w : adj(v)) {
+	                reverse.addEdge(w, v);
+	            }
+	        }
+	        return reverse;
+	    }
+
+	    /**
+	     * Returns a string representation of the graph.
+	     *
+	     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,  
+	     *         followed by the <em>V</em> adjacency lists
+	     */
+	    public String toString() {
+	        StringBuilder s = new StringBuilder();
+	        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+	        for (int v = 0; v < V; v++) {
+	            s.append(String.format("%d: ", v));
+	            for (int w : adj[v]) {
+	                s.append(String.format("%d ", w));
+	            }
+	            s.append(NEWLINE);
+	        }
+	        return s.toString();
+	    }
+	}
+	
+	public class FenwickTree {
+	
+	    long[] array; // 1-indexed array, In this array We save cumulative information to perform efficient range queries and updates
+	
+	    public FenwickTree(int size) {
+	        array = new long[size + 1];
+	    }
+	
+	    /**
+	     * Range Sum query from 1 to ind
+	     * ind is 1-indexed
+	     * <p>
+	     * Time-Complexity:    O(log(n))
+	     *
+	     * @param  ind index
+	     * @return sum
+	     */
+	    public long rsq(int ind) {
+	        long sum = 0;
+	        for (int x = ind; x > 0; x -= (x & -x)) {
+	        	sum = (sum + array[x]) % MOD;
+	        }
+	        return sum;
+	    }
+	
+	    /**
+	     * Range Sum Query from a to b.
+	     * Search for the sum from array index from a to b
+	     * a and b are 1-indexed
+	     * <p>
+	     * Time-Complexity:    O(log(n))
+	     *
+	     * @param  a left index
+	     * @param  b right index
+	     * @return sum
+	     */
+	    public long rsq(int a, int b) {
+	        return (rsq(b) - rsq(a - 1) + MOD) % MOD;
+	    }
+	
+	    /**
+	     * Update the array at ind and all the affected regions above ind.
+	     * ind is 1-indexed
+	     * <p>
+	     * Time-Complexity:    O(log(n))
+	     *
+	     * @param  ind   index
+	     * @param  value value
+	     */
+	    public void update(int ind, long value) {
+	    	for (int x = ind; x < array.length; x += (x & -x)) {
+	    		array[x] = (array[x] + value) % MOD;
+	    	}
+	    }
+	
+	    public int size() {
+	        return array.length - 1;
+	    }
+	}
+
+	private static Reader in = o.new Reader(System.in);
+	private static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+	private static Digraph G;
+	private static boolean[] marked;
+	private static int[] vToI;
+	private static FenwickTree ft;
+	private static final long MOD = 1000000007;
+	
+	public static void main(String[] args) throws IOException {
+		int N = in.nextInt();
+		vToI = new int[N];
+		marked = new boolean[N];
+		G = o.new Digraph(N);
+		ft = o.new FenwickTree(N);
+		int root = 0;
+		for (int i = 0; i < N; i++) {
+			int v = in.nextInt() - 1;
+			if (v != -1) G.addEdge(v, i);
+			else root = i;
+		}
+		for (int i = 1; i <= N; i++) {
+			vToI[in.nextInt() - 1] = i;
+		}
+		dfs(root);
+		for (int i = 0; i < N; i++) {
+			out.print(ft.rsq(vToI[i], vToI[i]));
+		}
+		out.println();
+		out.close();
+	}
+	
+	private static void dfs(int v) {
+        marked[v] = true;
+        long cur = ft.rsq(vToI[v]);
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+            	dfs(w);
+            }
+        }
+        ft.update(vToI[v], ft.rsq(vToI[v]) + 1 - cur);
+    }
+}
