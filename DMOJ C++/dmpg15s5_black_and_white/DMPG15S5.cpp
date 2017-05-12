55774672fb5e205ffd3217626f38a6b679306f46
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-11 23:33:46 -0400

    DMPG15S5 (Black and White) C++ Completed

---
+++ DMOJ C++/dmpg15s5_black_and_white/DMPG15S5.cpp
@@ -0,0 +1,46 @@
+/*
+ * DMPG15S5.cpp
+ *
+ *  Created on: May 11, 2017
+ *      Author: wleung
+ */
+
+
+#include <bits/stdc++.h>
+
+#define MAXN 10000
+
+using namespace std;
+
+bool board[MAXN + 1][MAXN + 1];
+int N, M, x, y, w, h;
+
+int main() {
+	scanf("%d%d", &N, &M);
+	for (int i = 0; i < M; i++) {
+		scanf("%d%d%d%d", &x, &y, &w, &h);
+		board[x][y] ^= true;
+		board[x][y + h] ^= true;
+		board[x + w][y] ^= true;
+		board[x + w][y + h] ^= true;
+	}
+
+	for (int i = 0; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			board[i][j] ^= board[i][j - 1];
+		}
+	}
+	for (int i = 1; i <= N; i++) {
+		for (int j = 0; j <= N; j++) {
+			board[i][j] ^= board[i - 1][j];
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i <= N; i++) {
+		for (int j = 0; j <= N; j++) {
+			if (board[i][j]) ans ++;
+		}
+	}
+	printf("%d", ans);
+	return 0;
+}
