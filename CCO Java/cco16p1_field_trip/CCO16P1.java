2361e4e970324e51b567aa3b9aa713de443b285d
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-12 01:08:58 -0400

    CCO16P1 (Field Trip) Completed

---
+++ CCO Java/cco16p1_field_trip/CCO16P1.java
@@ -0,0 +1,197 @@
+package cco16p1_field_trip;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CCO16P1 {
+	private static CCO16P1 o = new CCO16P1();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	public class UF {
+
+	    private int[] parent;  // parent[i] = parent of i
+	    private int[] size;
+	    private byte[] rank;   // rank[i] = rank of subtree rooted at i (never more than 31)
+	    private boolean[] cut;
+	    private int count;     // number of components
+
+	    /**
+	     * Initializes an empty union–find data structure with {@code n} sites
+	     * {@code 0} through {@code n-1}. Each site is initially in its own 
+	     * component.
+	     *
+	     * @param  n the number of sites
+	     * @throws IllegalArgumentException if {@code n < 0}
+	     */
+	    public UF(int n) {
+	        if (n < 0) throw new IllegalArgumentException();
+	        count = n;
+	        parent = new int[n];
+	        size = new int[n];
+	        rank = new byte[n];
+	        cut = new boolean[n];
+	        for (int i = 0; i < n; i++) {
+	            parent[i] = i;
+	            rank[i] = 0;
+	            size[i] = 1;
+	        }
+	    }
+
+	    /**
+	     * Returns the component identifier for the component containing site {@code p}.
+	     *
+	     * @param  p the integer representing one site
+	     * @return the component identifier for the component containing site {@code p}
+	     * @throws IndexOutOfBoundsException unless {@code 0 <= p < n}
+	     */
+	    public int find(int p) {
+	        validate(p);
+	        while (p != parent[p]) {
+	            parent[p] = parent[parent[p]];    // path compression by halving
+	            p = parent[p];
+	        }
+	        return p;
+	    }
+
+	    /**
+	     * Returns the number of components.
+	     *
+	     * @return the number of components (between {@code 1} and {@code n})
+	     */
+	    public int count() {
+	        return count;
+	    }
+	    
+	    public int size(int p) {
+	    	return size[p];
+	    }
+	    
+	    public boolean cut(int p) {
+	    	return cut[p];
+	    }
+	  
+	    /**
+	     * Returns true if the the two sites are in the same component.
+	     *
+	     * @param  p the integer representing one site
+	     * @param  q the integer representing the other site
+	     * @return {@code true} if the two sites {@code p} and {@code q} are in the same component;
+	     *         {@code false} otherwise
+	     * @throws IndexOutOfBoundsException unless
+	     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+	     */
+	    public boolean connected(int p, int q) {
+	        return find(p) == find(q);
+	    }
+	  
+	    /**
+	     * Merges the component containing site {@code p} with the 
+	     * the component containing site {@code q}.
+	     *
+	     * @param  p the integer representing one site
+	     * @param  q the integer representing the other site
+	     * @throws IndexOutOfBoundsException unless
+	     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+	     */
+	    public void union(int p, int q) {
+	        int rootP = find(p);
+	        int rootQ = find(q);
+	        if (rootP == rootQ) {
+	        	cut[rootP] = true;
+	        	return;
+	        }
+
+	        // make root of smaller rank point to root of larger rank
+	        if (rank[rootP] < rank[rootQ]) {
+	        	parent[rootP] = rootQ;
+	        	size[rootQ] += size[rootP];
+	        } else if (rank[rootP] > rank[rootQ]) {
+	        	parent[rootQ] = rootP;
+	        	size[rootP] += size[rootQ];
+	        } else {
+	            parent[rootQ] = rootP;
+	            rank[rootP]++;
+	            size[rootP] += size[rootQ];
+	        }
+	        count--;
+	    }
+
+	    // validate that p is a valid index
+	    private void validate(int p) {
+	        int n = parent.length;
+	        if (p < 0 || p >= n) {
+	            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n-1));  
+	        }
+	    }
+	}
+	
+	private static Reader in = o.new Reader(System.in);
+	private static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws IOException {
+		int N = in.nextInt();
+		int M = in.nextInt();
+		int K = in.nextInt();
+		int students = 0;
+		int cut = 0;
+		boolean[] visited = new boolean[N];
+		UF uf = o.new UF(N);
+		for (int i = 0; i < M; i++) {
+			uf.union(in.nextInt() - 1, in.nextInt() - 1);
+		}
+		for (int i = 0; i < N; i++) {
+			int c = uf.find(i);
+			if (!visited[c]) {
+				visited[c] = true;
+				students += uf.size(c) / K * K;
+				cut += (uf.size(c) - 1) / K;
+				if (cut > 0) {
+					if (uf.cut(c)) cut += 1;
+				}
+			}
+		}
+		out.println(students + " " + cut);
+		out.close();
+	}
+}
