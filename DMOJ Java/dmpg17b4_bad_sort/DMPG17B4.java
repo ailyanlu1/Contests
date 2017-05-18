75c09a86fe877de68e7d5be1221262206e78d9a8
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-18 00:31:24 -0400

    DMPG17B4 (Bad Sort) Completed

---
+++ DMOJ Java/dmpg17b4_bad_sort/DMPG17B4.java
@@ -0,0 +1,71 @@
+package dmpg17b4_bad_sort;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class DMPG17B4 {
+	private static DMPG17B4 o = new DMPG17B4();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	private static Reader in = o.new Reader(System.in);
+	private static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+	private static int[] arr = new int[1024];
+	
+	public static void main(String[] args) throws IOException {
+		generate();
+		for (int i = 0; i < arr.length; i++) {
+			out.println(arr[i]);
+		}
+		out.close();
+	}
+	
+	private static void generate() {
+		int cur = arr.length / 2;
+		for (int i = 0; i < arr.length; i++) {
+			if (i % 2 == 0) cur += i;
+			else cur -= i;
+			arr[cur] = i;
+		}
+	}
+}
