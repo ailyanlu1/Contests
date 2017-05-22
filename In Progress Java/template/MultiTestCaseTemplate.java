90a2163ebf47d7f7f3d169ff7f44ea85ac3bdb39
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-22 13:09:01 -0400

    MultiTestCaseTemplate

---
+++ In Progress Java/template/MultiTestCaseTemplate.java
@@ -0,0 +1,66 @@
+package template;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MultiTestCaseTemplate {
+	private static MultiTestCaseTemplate o = new MultiTestCaseTemplate();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	private static Reader in = o.new Reader(System.in);
+	private static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+	
+	private static final int NUM_OF_TEST_CASES = 10; // TODO CHANGE NUMBER OF TEST CASES
+	
+	public static void main(String[] args) throws IOException {
+		for (int i = 0; i < NUM_OF_TEST_CASES; i++) {
+			run();
+		}
+		out.close();
+	}
+	
+	public static void run() throws IOException {
+		// TODO INSERT CODE HERE
+	}
+}
