8ac7719bb62f6e4435ed7ff93580951ec5213621
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-22 17:53:38 -0400

    IOI00P1 (Palindrome) Completed in Java and C++

---
+++ IOI Java/ioi00p1_palindrome/IOI00P1.java
@@ -0,0 +1,76 @@
+package ioi00p1_palindrome;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class IOI00P1 {
+	private static IOI00P1 o = new IOI00P1();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	private static Reader in = o.new Reader(System.in);
+	private static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws IOException {
+		int N = in.nextInt();
+		String str = in.next();
+		out.println(N - process(str, new StringBuilder(str).reverse().toString()));
+		out.close();
+	}
+	
+	private static int process(String s1, String s2) {
+		int[][] dp = new int[2][s2.length() + 1];
+		int s = 1;
+		for (int i = 1; i <= s1.length(); i++) {
+			s = 1 - s;
+			for (int j = 1; j <= s2.length(); j++) {
+				if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+					dp[s][j] = 1 + dp[1 - s][j - 1];
+				} else {
+					dp[s][j] = Math.max(dp[1 - s][j], dp[s][j - 1]);
+				}
+			}
+		}
+		return dp[s][s2.length()];
+	}
+}
