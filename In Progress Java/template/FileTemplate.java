ee56db758729c4cb0dc0867e570049079eb796f8
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-22 18:50:55 -0400

    File Templates added

---
+++ In Progress Java/template/FileTemplate.java
@@ -0,0 +1,61 @@
+package template;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class FileTemplate {
+	private static FileTemplate o = new FileTemplate();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	private static Reader in;
+	private static PrintWriter out;
+	
+	public static void main(String[] args) throws IOException {
+		// TODO CHANGE FILE NAMES
+		in = o.new Reader("template/input.in");
+		out = new PrintWriter("template/output.out");
+		// TODO INSERT CODE HERE
+		out.close();
+	}
+}
---
+++ In Progress Java/template/MultiTestCaseFileTemplate.java
@@ -0,0 +1,69 @@
+package template;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class MultiTestCaseFileTemplate {
+	private static MultiTestCaseFileTemplate o = new MultiTestCaseFileTemplate();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	private static Reader in;
+	private static PrintWriter out;
+	
+	private static final int NUM_OF_TEST_CASES = 10; // TODO CHANGE NUMBER OF TEST CASES
+	
+	public static void main(String[] args) throws IOException {
+		// TODO CHANGE FILE NAMES
+		in = o.new Reader("template/input.in");
+		out = new PrintWriter("template/output.out");
+		for (int i = 0; i < NUM_OF_TEST_CASES; i++) {
+			run();
+		}
+		out.close();
+	}
+	
+	public static void run() throws IOException {
+		// TODO INSERT CODE HERE
+	}
+}
