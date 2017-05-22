376520e584af7a158f93be97fe6b0f8b81c229d0
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-22 18:52:32 -0400

    Removed FileWriter import statement

--- In Progress Java/template/MultiTestCaseFileTemplate.java
+++ In Progress Java/template/MultiTestCaseFileTemplate.java
@@ -3,7 +3,6 @@ package template;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
