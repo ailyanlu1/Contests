e8c02aaef092e4c711a755b406e65e4647ba28b8
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-26 09:19:28 -0400

    CCC01J2 (Mod Inverse) Completed

---
+++ CCC Java/ccc01j2_mod_inverse/CCC01J2.java
@@ -0,0 +1,64 @@
+package ccc01j2_mod_inverse;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class CCC01J2 {
+	private static CCC01J2 o = new CCC01J2();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	private static Reader in = o.new Reader(System.in);
+	private static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	
+	public static void main(String[] args) throws IOException {
+		try {
+			out.println(new BigInteger(in.next()).modInverse(new BigInteger(in.next())));
+		} catch (ArithmeticException e) {
+			out.println("No such integer exists.");
+		}
+		out.close();
+	}
+}
