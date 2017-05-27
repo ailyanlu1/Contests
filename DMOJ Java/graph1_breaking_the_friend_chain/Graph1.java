9d56f47f8e92120586b4b0e2efee990b4dc1c0aa
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-27 18:14:58 -0400

    Graph1 (Breaking the Friend Chain) Completed

---
+++ DMOJ Java/graph1_breaking_the_friend_chain/Graph1.java
@@ -0,0 +1,638 @@
+package graph1_breaking_the_friend_chain;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Graph1 {
+	private static Graph1 o = new Graph1();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	public class Queue<Item> implements Iterable<Item> {
+	    private Node<Item> first;    // beginning of queue
+	    private Node<Item> last;     // end of queue
+	    private int n;               // number of elements on queue
+
+	    // helper linked list class
+	    private class Node<Item> {
+	        private Item item;
+	        private Node<Item> next;
+	    }
+
+	    /**
+	     * Initializes an empty queue.
+	     */
+	    public Queue() {
+	        first = null;
+	        last  = null;
+	        n = 0;
+	    }
+
+	    /**
+	     * Returns true if this queue is empty.
+	     *
+	     * @return {@code true} if this queue is empty; {@code false} otherwise
+	     */
+	    public boolean isEmpty() {
+	        return first == null;
+	    }
+
+	    /**
+	     * Returns the number of items in this queue.
+	     *
+	     * @return the number of items in this queue
+	     */
+	    public int size() {
+	        return n;
+	    }
+
+	    /**
+	     * Returns the item least recently added to this queue.
+	     *
+	     * @return the item least recently added to this queue
+	     * @throws NoSuchElementException if this queue is empty
+	     */
+	    public Item peek() {
+	        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+	        return first.item;
+	    }
+
+	    /**
+	     * Adds the item to this queue.
+	     *
+	     * @param  item the item to add
+	     */
+	    public void enqueue(Item item) {
+	        Node<Item> oldlast = last;
+	        last = new Node<Item>();
+	        last.item = item;
+	        last.next = null;
+	        if (isEmpty()) first = last;
+	        else           oldlast.next = last;
+	        n++;
+	    }
+
+	    /**
+	     * Removes and returns the item on this queue that was least recently added.
+	     *
+	     * @return the item on this queue that was least recently added
+	     * @throws NoSuchElementException if this queue is empty
+	     */
+	    public Item dequeue() {
+	        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+	        Item item = first.item;
+	        first = first.next;
+	        n--;
+	        if (isEmpty()) last = null;   // to avoid loitering
+	        return item;
+	    }
+
+	    /**
+	     * Returns a string representation of this queue.
+	     *
+	     * @return the sequence of items in FIFO order, separated by spaces
+	     */
+	    public String toString() {
+	        StringBuilder s = new StringBuilder();
+	        for (Item item : this) {
+	            s.append(item);
+	            s.append(' ');
+	        }
+	        return s.toString();
+	    } 
+
+	    /**
+	     * Returns an iterator that iterates over the items in this queue in FIFO order.
+	     *
+	     * @return an iterator that iterates over the items in this queue in FIFO order
+	     */
+	    public Iterator<Item> iterator()  {
+	        return new ListIterator<Item>(first);  
+	    }
+
+	    // an iterator, doesn't implement remove() since it's optional
+	    private class ListIterator<Item> implements Iterator<Item> {
+	        private Node<Item> current;
+
+	        public ListIterator(Node<Item> first) {
+	            current = first;
+	        }
+
+	        public boolean hasNext()  { return current != null;                     }
+	        public void remove()      { throw new UnsupportedOperationException();  }
+
+	        public Item next() {
+	            if (!hasNext()) throw new NoSuchElementException();
+	            Item item = current.item;
+	            current = current.next; 
+	            return item;
+	        }
+	    }
+	}
+	
+	public class UF {
+
+	    private int[] parent;  // parent[i] = parent of i
+	    private byte[] rank;   // rank[i] = rank of subtree rooted at i (never more than 31)
+	    private int count;     // number of components
+
+	    /**
+	     * Initializes an empty union–find data structure with {@code n} sites
+	     * {@code 0} through {@code n-1}. Each site is initially in its own 
+	     * component.
+	     *
+	     * @param  n the number of sites
+	     * @throws IllegalArgumentException if {@code n < 0}
+	     */
+	    public UF(int n) {
+	        if (n < 0) throw new IllegalArgumentException();
+	        count = n;
+	        parent = new int[n];
+	        rank = new byte[n];
+	        for (int i = 0; i < n; i++) {
+	            parent[i] = i;
+	            rank[i] = 0;
+	        }
+	    }
+
+	    /**
+	     * Returns the component identifier for the component containing site {@code p}.
+	     *
+	     * @param  p the integer representing one site
+	     * @return the component identifier for the component containing site {@code p}
+	     * @throws IndexOutOfBoundsException unless {@code 0 <= p < n}
+	     */
+	    public int find(int p) {
+	        validate(p);
+	        while (p != parent[p]) {
+	            parent[p] = parent[parent[p]];    // path compression by halving
+	            p = parent[p];
+	        }
+	        return p;
+	    }
+
+	    /**
+	     * Returns the number of components.
+	     *
+	     * @return the number of components (between {@code 1} and {@code n})
+	     */
+	    public int count() {
+	        return count;
+	    }
+	  
+	    /**
+	     * Returns true if the the two sites are in the same component.
+	     *
+	     * @param  p the integer representing one site
+	     * @param  q the integer representing the other site
+	     * @return {@code true} if the two sites {@code p} and {@code q} are in the same component;
+	     *         {@code false} otherwise
+	     * @throws IndexOutOfBoundsException unless
+	     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+	     */
+	    public boolean connected(int p, int q) {
+	        return find(p) == find(q);
+	    }
+	  
+	    /**
+	     * Merges the component containing site {@code p} with the 
+	     * the component containing site {@code q}.
+	     *
+	     * @param  p the integer representing one site
+	     * @param  q the integer representing the other site
+	     * @throws IndexOutOfBoundsException unless
+	     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+	     */
+	    public void union(int p, int q) {
+	        int rootP = find(p);
+	        int rootQ = find(q);
+	        if (rootP == rootQ) return;
+
+	        // make root of smaller rank point to root of larger rank
+	        if      (rank[rootP] < rank[rootQ]) parent[rootP] = rootQ;
+	        else if (rank[rootP] > rank[rootQ]) parent[rootQ] = rootP;
+	        else {
+	            parent[rootQ] = rootP;
+	            rank[rootP]++;
+	        }
+	        count--;
+	    }
+
+	    // validate that p is a valid index
+	    private void validate(int p) {
+	        int n = parent.length;
+	        if (p < 0 || p >= n) {
+	            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n-1));  
+	        }
+	    }
+	}
+	
+	public class Graph {
+	    private final String NEWLINE = System.getProperty("line.separator");
+
+	    private final int V;
+	    private int E;
+	    private HashSet<Integer>[] adj;
+	    private HashSet<Edge> removed;
+	    
+	    /**
+	     * Initializes an empty graph with {@code V} vertices and 0 edges.
+	     * param V the number of vertices
+	     *
+	     * @param  V number of vertices
+	     * @throws IllegalArgumentException if {@code V < 0}
+	     */
+	    public Graph(int V) {
+	        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+	        this.V = V;
+	        this.E = 0;
+	        adj = (HashSet<Integer>[]) new HashSet[V];
+	        removed = new HashSet<Edge>();
+	        for (int v = 0; v < V; v++) {
+	            adj[v] = new HashSet<Integer>();
+	        }
+	    }
+
+	    /**
+	     * Initializes a new graph that is a deep copy of {@code G}.
+	     *
+	     * @param  G the graph to copy
+	     */
+	    public Graph(Graph G) {
+	        this(G.V());
+	        this.E = G.E();
+	        for (int v = 0; v < G.V(); v++) {
+	            // reverse so that adjacency list is in same order as original
+	            Stack<Integer> reverse = new Stack<Integer>();
+	            for (int w : G.adj[v]) {
+	                reverse.push(w);
+	            }
+	            for (int w : reverse) {
+	                adj[v].add(w);
+	            }
+	        }
+	    }
+
+	    /**
+	     * Returns the number of vertices in this graph.
+	     *
+	     * @return the number of vertices in this graph
+	     */
+	    public int V() {
+	        return V;
+	    }
+
+	    /**
+	     * Returns the number of edges in this graph.
+	     *
+	     * @return the number of edges in this graph
+	     */
+	    public int E() {
+	        return E;
+	    }
+
+	    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+	    private void validateVertex(int v) {
+	        if (v < 0 || v >= V)
+	            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+	    }
+
+	    /**
+	     * Adds the undirected edge v-w to this graph.
+	     *
+	     * @param  v one vertex in the edge
+	     * @param  w the other vertex in the edge
+	     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+	     */
+	    public void addEdge(int v, int w) {
+	        validateVertex(v);
+	        validateVertex(w);
+	        E++;
+	        adj[v].add(w);
+	        adj[w].add(v);
+	    }
+	    
+	    /**
+	     * Removes the edge from from this graph.
+	     *
+	     * @param  v one vertex in the edge
+	     * @param  w the other vertex in the edge
+	     * @throws IllegalArgumentException unless endpoints of edge are between {@code 0}
+	     *         and {@code V-1}
+	     */
+	    public void removeEdge(int v, int w) {
+	        validateVertex(v);
+	        validateVertex(w);
+	        removed.add(new Edge(v, w));
+	        adj[v].remove(w);
+	        adj[w].remove(v);
+	        E--;
+	    }
+	    
+	    /**
+	     * Restores all the edges removed from this graph.
+	     */
+	    public void restoreEdges() {
+	    	for (Edge e: removed) {
+	            addEdge(e.v, e.w);
+	    	}
+	    	removed.clear();
+	    }
+	    
+	    /**
+	     * Clears the edges removed from this graph so they can no longer
+	     * be restored
+	     */
+	    public void clearRemoved() {
+	    	removed.clear();
+	    }
+
+
+	    /**
+	     * Returns the vertices adjacent to vertex {@code v}.
+	     *
+	     * @param  v the vertex
+	     * @return the vertices adjacent to vertex {@code v}, as an iterable
+	     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+	     */
+	    public Iterable<Integer> adj(int v) {
+	        validateVertex(v);
+	        return adj[v];
+	    }
+
+	    /**
+	     * Returns the degree of vertex {@code v}.
+	     *
+	     * @param  v the vertex
+	     * @return the degree of vertex {@code v}
+	     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+	     */
+	    public int degree(int v) {
+	        validateVertex(v);
+	        return adj[v].size();
+	    }
+
+
+	    /**
+	     * Returns a string representation of this graph.
+	     *
+	     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+	     *         followed by the <em>V</em> adjacency lists
+	     */
+	    public String toString() {
+	        StringBuilder s = new StringBuilder();
+	        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+	        for (int v = 0; v < V; v++) {
+	            s.append(v + ": ");
+	            for (int w : adj[v]) {
+	                s.append(w + " ");
+	            }
+	            s.append(NEWLINE);
+	        }
+	        return s.toString();
+	    }
+	    
+	    public class Edge {
+	    	public int v;
+	    	public int w;
+	    	
+	    	public Edge(int v, int w) {
+	    		this.v = Math.min(v, w);
+	    		this.w = Math.max(v, w);
+	    	}
+	    	
+			@Override
+			public int hashCode() {
+				return 31 * v + w;
+			}
+	    		
+			@Override
+			public boolean equals(Object o) {
+				if (o == this) return true;
+			    if (!(o instanceof Edge)) {
+			        return false;
+			    }
+			    Edge e = (Edge) o;
+				return e.v == v && e.w == w;
+			}
+	    }
+	}
+	
+	public class BreadthFirstSinglePath {
+	    private static final int INFINITY = Integer.MAX_VALUE;
+	    private boolean[] marked;  // marked[v] = is there an s-v path
+	    private int[] edgeTo;      // edgeTo[v] = previous edge on shortest s-v path
+	    private int[] distTo;      // distTo[v] = number of edges shortest s-v path
+	    private int f;             // the end vertex
+
+	    /**
+	     * Computes the shortest path between the source vertex {@code s}
+	     * and every other vertex in the graph {@code G}.
+	     * @param G the graph
+	     * @param s the source vertex
+	     * @throws IllegalArgumentException unless {@code 0 <= s < V}
+	     */
+	    public BreadthFirstSinglePath(Graph G, int s, int f) {
+	        marked = new boolean[G.V()];
+	        distTo = new int[G.V()];
+	        edgeTo = new int[G.V()];
+	        validateVertex(s);
+	        validateVertex(f);
+	        this.f = f;
+	        bfs(G, s);
+	    }
+
+	    /**
+	     * Computes the shortest path between any one of the source vertices in {@code sources}
+	     * and every other vertex in graph {@code G}.
+	     * @param G the graph
+	     * @param sources the source vertices
+	     * @throws IllegalArgumentException unless {@code 0 <= s < V} for each vertex
+	     *         {@code s} in {@code sources}
+	     */
+	    public BreadthFirstSinglePath(Graph G, Iterable<Integer> sources, int f) {
+	        marked = new boolean[G.V()];
+	        distTo = new int[G.V()];
+	        edgeTo = new int[G.V()];
+	        for (int v = 0; v < G.V(); v++)
+	            distTo[v] = INFINITY;
+	        validateVertices(sources);
+	        validateVertex(f);
+	        this.f = f;
+	        bfs(G, sources);
+	    }
+
+
+	    // breadth-first search from a single source
+	    private void bfs(Graph G, int s) {
+	        Queue<Integer> q = new Queue<Integer>();
+	        for (int v = 0; v < G.V(); v++)
+	            distTo[v] = INFINITY;
+	        distTo[s] = 0;
+	        marked[s] = true;
+	        q.enqueue(s);
+
+	        while (!q.isEmpty()) {
+	            int v = q.dequeue();
+	            if (v == f) return;
+	            for (int w : G.adj(v)) {
+	                if (!marked[w]) {
+	                    edgeTo[w] = v;
+	                    distTo[w] = distTo[v] + 1;
+	                    marked[w] = true;
+	                    q.enqueue(w);
+	                }
+	            }
+	        }
+	    }
+
+	    // breadth-first search from multiple sources
+	    private void bfs(Graph G, Iterable<Integer> sources) {
+	        Queue<Integer> q = new Queue<Integer>();
+	        for (int s : sources) {
+	            marked[s] = true;
+	            distTo[s] = 0;
+	            q.enqueue(s);
+	        }
+	        while (!q.isEmpty()) {
+	            int v = q.dequeue();
+	            if (v == f) return;
+	            for (int w : G.adj(v)) {
+	                if (!marked[w]) {
+	                    edgeTo[w] = v;
+	                    distTo[w] = distTo[v] + 1;
+	                    marked[w] = true;
+	                    if (w == f) return;
+	                    q.enqueue(w);
+	                }
+	            }
+	        }
+	    }
+
+	    /**
+	     * Is there a path from the source {@code s} (or sources) to the end vertex {@code f}?
+	     * @return {@code true} if there is a path, {@code false} otherwise
+	     */
+	    public boolean hasPath() {
+	        return marked[f];
+	    }
+
+	    /**
+	     * Is the vertex on the path from the source {@code s} (or sources) to the end vertex {@code f}?
+	     * @param v the vertex
+	     * @return {@code true} if the point is on the path, {@code false} otherwise
+	     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+	     */
+	    public boolean onPath(int v) {
+	        validateVertex(v);
+	        return marked[v];
+	    }
+
+	    /**
+	     * Returns the number of edges in a shortest path from the source {@code s}
+	     * (or sources) to the end vertex {@code f}
+	     * @return the number of edges in a shortest path
+	     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+	     */
+	    public int distTo() {
+	        return distTo[f];
+	    }
+
+	    /**
+	     * Returns a shortest path from {@code s} (or sources) to the end vertex {@code f}, or
+	     * {@code null} if no such path.
+	     * @return the sequence of vertices on a shortest path, as an Iterable
+	     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+	     */
+	    public Iterable<Integer> path() {
+	        if (!hasPath()) return null;
+	        Stack<Integer> path = new Stack<Integer>();
+	        int x;
+	        for (x = f; distTo[x] != 0; x = edgeTo[x])
+	            path.push(x);
+	        path.push(x);
+	        return path;
+	    }
+
+	    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+	    private void validateVertex(int v) {
+	        int V = marked.length;
+	        if (v < 0 || v >= V)
+	            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+	    }
+
+	    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+	    private void validateVertices(Iterable<Integer> vertices) {
+	        if (vertices == null) {
+	            throw new IllegalArgumentException("argument is null");
+	        }
+	        int V = marked.length;
+	        for (int v : vertices) {
+	            if (v < 0 || v >= V) {
+	                throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+	            }
+	        }
+	    }
+	}
+
+	private static Reader in = o.new Reader(System.in);
+	private static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	
+	public static void main(String[] args) throws IOException {
+		int N = in.nextInt();
+		int M = in.nextInt();
+		Graph G = o.new Graph(N);
+		for (int i = 0; i < M; i++) {
+			G.addEdge(in.nextInt() - 1, in.nextInt() - 1);
+		}
+		BreadthFirstSinglePath bfs = o.new BreadthFirstSinglePath(G, in.nextInt() - 1, in.nextInt() - 1);
+		if (bfs.hasPath()) out.println(1);
+		else out.println(0);
+		out.close();
+	}
+}
