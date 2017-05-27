7f6cd333bb0c7750efc1fc364b232a16237e2105
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-27 00:39:03 -0400

    CCO17P4 (Rainfall Storage) Completed

---
+++ CCO C++/cco17p4_rainfall_storage/CCO17P4.cpp
@@ -0,0 +1,45 @@
+/*
+ * CCO17P4.cpp
+ *
+ *  Created on: May 27, 2017
+ *      Author: Wesley Leung
+ */
+
+#include <bits/stdc++.h>
+
+#define MAXN 500
+#define MAXH 50
+
+using namespace std;
+
+int N;
+vector<int> h;
+set<int> heights;
+bool dp[2][MAXH * MAXN + 1];
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		int hi;
+		scanf("%d", &hi);
+		h.push_back(hi);
+	}
+	sort(h.begin(), h.end());
+	reverse(h.begin(), h.end());
+	dp[1][0] = true;
+	heights.insert(h[1]);
+	for (int i = 2; i < N; i++) {
+		for (int j = 0; j <= MAXH * N; j++) {
+			if (dp[1-i%2][j]) dp[i%2][j] = true;
+			for (int k: heights) {
+				if (j < (k - h[i])) continue;
+				if (dp[1-i%2][j - k + h[i]]) dp[i%2][j] = true;
+			}
+		}
+		heights.insert(h[i]);
+	}
+	for (int i = 0; i <= MAXH * N; i++) {
+		if (dp[1-N%2][i]) printf("%d ", i);
+	}
+	return 0;
+}
