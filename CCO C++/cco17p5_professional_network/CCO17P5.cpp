1d0541e88bfd85862902ce39d1b4fd06dd9efb74
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-28 17:31:16 -0400

    Small Optimizations

--- CCO C++/cco17p5_professional_network/CCO17P5.cpp
+++ CCO C++/cco17p5_professional_network/CCO17P5.cpp
@@ -13,28 +13,28 @@ using namespace std;
 
 int N;
 int cost;
-pair<int, int> arr[MAXN];
+vector<pair<int, int>> arr;
 priority_queue<int> paid;
-queue<pair<int, int>> hold;
+queue<int> hold;
 
 int main() {
 	scanf("%d", &N);
 	for (int i = 0; i < N; i++) {
 		int A, B;
 		scanf("%d%d", &A, &B);
-		arr[i] = make_pair(A, B);
+		arr.push_back(make_pair(A, B));
 	}
-	sort(arr, arr + N);
+	sort(arr.begin(), arr.end());
 	for (int i = 0; i < N; i++) {
-		while (!hold.empty() && i >= hold.front().first) {
-			paid.push(hold.front().second);
+		while (!hold.empty() && i >= arr[hold.front()].first) {
+			paid.push(arr[hold.front()].second);
 			hold.pop();
 		}
 		if (i >= arr[i].first) {
 			if (!paid.empty() && arr[i].second < paid.top()) {
 				cost += arr[i].second - paid.top();
 				paid.pop();
-				hold.push(arr[i]);
+				hold.push(i);
 			}
 		} else {
 			cost += arr[i].second;
@@ -42,7 +42,7 @@ int main() {
 				cost -= paid.top();
 				paid.pop();
 			}
-			hold.push(arr[i]);
+			hold.push(i);
 		}
 	}
 	printf("%d\n", cost);
