b7723367f0619066261a460d1c3849e6151b70e3
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-28 13:18:14 -0400

    DMPG17S3 (Black and White III) Completed in Python

---
+++ DMOJ Python/dmpg17s3_black_and_white_iii/DMPG17S3.py
@@ -0,0 +1,7 @@
+import sys
+MOD = 10**9 + 7
+K, M = map(int, input().split())
+black = 0;
+for i in range(1 << K):
+    black += input().count('#')
+print(pow(pow(2, pow(4, M, MOD - 1), MOD) - 1, black, MOD))
