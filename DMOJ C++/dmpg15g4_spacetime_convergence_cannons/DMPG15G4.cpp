4e87d04459938da50b02770a394feff990ee2033
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-04 13:53:15 -0400

    DMPG15G4 (Spacetime Convergence Cannons) Completed

---
+++ DMOJ C++/dmpg15g4_spacetime_convergence_cannons/DMPG15G4.cpp
@@ -0,0 +1,154 @@
+/*
+ * DMPG15G4.cpp
+ *
+ *  Created on: Jun 4, 2017
+ *      Author: Wesley Leung
+ */
+
+#include <bits/stdc++.h>
+
+#define INT_INF 0x3f3f3f
+#define pb push_back
+#define mp make_pair
+#define l(x) x << 1
+#define r(x) x << 1 | 1
+#define m(x, y) x + (y - x) / 2
+#define x first
+#define y second
+#define ri(x) scanf("%d", &x);
+#define rll(x) scanf("%lld", &x);
+#define rllu(x) scanf('%llu', &x)
+#define rd(x) scanf("%lf", &x);
+#define rc(x) scanf("%c", &x);
+#define pri(x) printf("%d", x);
+#define prll(x) printf("%lld", x);
+#define prllu(x) printf("%llu", x);
+#define prd(x) printf("%f", x);
+#define prs(x) printf(x);
+#define prsi(x, s) printf("%d" s, x);
+#define prsll(x, s) printf("%lld" s, x);
+#define prsllu(x, s) printf("%llu" s, x);
+#define prsd(x, s) printf("%f" s, x);
+#define For(i, a, n) for (int i = a; i < n; i++)
+#define Fore(i, a, n) for (int i = a; i <= n; i++)
+#define For0(i, n) For(i, 0, n)
+#define For1(i, n) Fore(i, 1, n)
+#define Forc(i, c) for (int i = c.begin(); i != c.end(); i++)
+#define Rev(i, n, a) for (int i = n; i > a; i--)
+#define Reve(i, n, a) for (int i = n; i >= a; i--)
+#define Rev0(i, n) Reve(i, n - 1, 0)
+#define Rev1(i, n) Reve(i, n, 1)
+#define Revc(i, c) for (int i = c.end(); i-- != c.begin();)
+#define Fill(a, x) memset(a, x, sizeof(a))
+#define All(a) a.begin(), a.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<llu, llu> pllu;
+typedef map<int, int> mii;
+typedef map<int, ll> mill;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, ll> umill;
+
+#define MAXN 200000
+
+int N, X;
+ll ans;
+pdd cannons[MAXN];
+double theta[2 * MAXN];
+pii C[MAXN];
+
+struct FenwickTree {
+private:
+	int* array;
+	int size;
+
+public:
+	FenwickTree(int size) {
+		this->size = size;
+		array = new int[size + 1];
+	}
+
+    /**
+     * Range Sum query from 1 to ind
+     * ind is 1-indexed
+     * <p>
+     * Time-Complexity:    O(log(n))
+     *
+     * @param  ind index
+     * @return sum
+     */
+	int rsq(int ind) {
+		int sum = 0;
+		for (int x = ind; x > 0; x -= (x & -x)) {
+			sum += array[x];
+		}
+		return sum;
+	}
+
+    /**
+     * Range Sum Query from a to b.
+     * Search for the sum from array index from a to b
+     * a and b are 1-indexed
+     * <p>
+     * Time-Complexity:    O(log(n))
+     *
+     * @param  a left index
+     * @param  b right index
+     * @return sum
+     */
+	int rsq(int a, int b) {
+		return rsq(b) - rsq(a - 1);
+	}
+
+    /**
+     * Update the array at ind and all the affected regions above ind.
+     * ind is 1-indexed
+     * <p>
+     * Time-Complexity:    O(log(n))
+     *
+     * @param  ind   index
+     * @param  value value
+     */
+	void update(int ind, int value) {
+		for (int x = ind; x <= size; x += (x & -x)) {
+			array[x] += value;
+		}
+	}
+
+	int getSize() {
+		return size;
+	}
+} ft(2 * MAXN);
+
+int main() {
+	ri(N);
+	ri(X);
+	For0(i, N) {
+		int x, y;
+		ri(x);
+		ri(y);
+		double theta1 = atan2(y, x);
+		double theta2 = atan2(y, X - x);
+		cannons[i] = mp(theta1, theta2);
+		theta[l(i)] = theta1;
+		theta[r(i)] = theta2;
+	}
+	sort(theta, theta + 2 * N);
+	For0(i, N) {
+		C[i] = mp(2 * N - (lower_bound(theta, theta + 2 * N, cannons[i].x) - theta), 2 * N - (lower_bound(theta, theta + 2 * N, cannons[i].y) - theta));
+	}
+	sort(C, C + N);
+	For0(i, N) {
+		ll q = ft.rsq(C[i].y);
+		ans += q * q;
+		ft.update(C[i].y, 1);
+	}
+	prll(ans);
+	return 0;
+}
