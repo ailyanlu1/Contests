0a827d95cef66226c267c018b57ee217605c3909
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-04 11:17:17 -0400

    DMPG15G5 (ExpedColle) Completed, Implemented DMPG16S5 with own Deque

--- DMOJ Java/dmpg16s5_pizza_bag/DMPG16S5.java
+++ DMOJ Java/dmpg16s5_pizza_bag/DMPG16S5.java
@@ -8,8 +8,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
 public class DMPG16S5 {
@@ -87,7 +87,184 @@ public class DMPG16S5 {
 			return query(end) - query(start - 1);
 		}
 	}
+	
+	public class Deque<Item> implements Iterable<Item> {
+	    private Node<Item> first;    // beginning of deque
+	    private Node<Item> last;     // end of deque
+	    private int n;               // number of elements on deque
 
+	    // helper linked list class
+	    private class Node<Item> {
+	        private Item item;
+	        private Node<Item> next;
+	        private Node<Item> prev;
+	    }
+
+	    /**
+	     * Initializes an empty deque.
+	     */
+	    public Deque() {
+	        first = null;
+	        last  = null;
+	        n = 0;
+	    }
+
+	    /**
+	     * Returns true if this deque is empty.
+	     *
+	     * @return {@code true} if this deque is empty; {@code false} otherwise
+	     */
+	    public boolean isEmpty() {
+	        return n == 0;
+	    }
+
+	    /**
+	     * Returns the number of items in this deque.
+	     *
+	     * @return the number of items in this deque
+	     */
+	    public int size() {
+	        return n;
+	    }
+
+	    /**
+	     * Returns the item at the front of the deque.
+	     *
+	     * @return the item at the front of the deque
+	     * @throws NoSuchElementException if this deque is empty
+	     */
+	    public Item peekFirst() {
+	        if (isEmpty()) throw new NoSuchElementException("deque underflow");
+	        return first.item;
+	    }
+	    
+	    /**
+	     * Returns the item at the end of the deque.
+	     *
+	     * @return the item at the end of the deque
+	     * @throws NoSuchElementException if this deque is empty
+	     */
+	    public Item peekLast() {
+	        if (isEmpty()) throw new NoSuchElementException("deque underflow");
+	        return last.item;
+	    }
+	    
+	    /**
+	     * Adds the item to the front of the deque.
+	     *
+	     * @param  item the item to add
+	     */
+	    public void addFirst(Item item) {
+	        n++;
+	        if (first == null) {
+	        	Node<Item> newFirst = new Node<Item>();
+	        	newFirst.item = item;
+	        	first = newFirst;
+	        	last = newFirst;
+	        } else {
+	        	Node<Item> newFirst = new Node<Item>();
+	        	newFirst.item = item;
+	        	newFirst.next = first;
+	        	first.prev = newFirst;
+	        	first = newFirst;
+	        }
+	    }
+
+	    /**
+	     * Adds the item to the end of the deque.
+	     *
+	     * @param  item the item to add
+	     */
+	    public void addLast(Item item) {
+	    	n++;
+	    	if (last == null) {
+	    		Node<Item> newLast = new Node<Item>();
+	    		newLast.item = item;
+	    		first = newLast;
+	    		last = newLast;
+	    	} else {
+	    		Node<Item> newLast = new Node<Item>();
+	    		newLast.item = item;
+	    		newLast.prev = last;
+	    		last.next = newLast;
+	    		last = newLast;
+	    	}
+	    }
+
+	    /**
+	     * Removes and returns the item at the front of the deque
+	     *
+	     * @return the item on at the front of the deque
+	     * @throws NoSuchElementException if this deque is empty
+	     */
+	    public Item removeFirst() {
+	        if (isEmpty()) throw new NoSuchElementException("deque underflow");
+	        n--;
+	        Item item = first.item;
+	        first = first.next;
+	        if (first == null) last = null;
+	        else first.prev = null;
+	        return item;
+	    }
+	    
+	    /**
+	     * Removes and returns the item at the end of the deque
+	     *
+	     * @return the item on at the end of the deque
+	     * @throws NoSuchElementException if this deque is empty
+	     */
+	    public Item removeLast() {
+	        if (isEmpty()) throw new NoSuchElementException("deque underflow");
+	        n--;
+	        Item item = last.item;
+	        last = last.prev;
+	        if (last == null) first = null;
+	        else last.next = null;
+	        return item;
+	    }
+
+	    /**
+	     * Returns a string representation of this deque.
+	     *
+	     * @return the sequence of items from from to back
+	     */
+	    public String toString() {
+	        StringBuilder s = new StringBuilder();
+	        for (Item item : this) {
+	            s.append(item);
+	            s.append(' ');
+	        }
+	        return s.toString();
+	    } 
+
+	    /**
+	     * Returns an iterator that iterates over the items in this deque from front to back
+	     *
+	     * @return an iterator that iterates over the items in this deque from front to back
+	     */
+	    public Iterator<Item> iterator()  {
+	        return new ListIterator<Item>(first);  
+	    }
+
+	    // an iterator, doesn't implement remove() since it's optional
+	    private class ListIterator<Item> implements Iterator<Item> {
+	        private Node<Item> current;
+
+	        public ListIterator(Node<Item> first) {
+	            current = first;
+	        }
+
+	        public boolean hasNext()  { return current != null;                     }
+	        public void remove()      { throw new UnsupportedOperationException();  }
+
+	        public Item next() {
+	            if (!hasNext()) throw new NoSuchElementException();
+	            Item item = current.item;
+	            current = current.next; 
+	            return item;
+	        }
+	    }
+	}
 	
 	public class Pair<Item, Item2> {
 		public Item first;
@@ -111,19 +288,19 @@ public class DMPG16S5 {
 			if (i + N < arr.length) arr[i+N] = d;
 		}
 		PrefixSumArray psa = o.new PrefixSumArray(arr);
-		Deque<Pair<Integer, Long>> dq = new ArrayDeque<Pair<Integer, Long>>();
+		Deque<Pair<Integer, Long>> dq = o.new Deque<Pair<Integer, Long>>();
 		dq.addFirst(o.new Pair<Integer, Long>(-1, 0L));
 		long ans = 0;
 		for (int i = 0; i < arr.length; i++) {
 			Pair<Integer, Long> add = o.new Pair<Integer, Long>(i, psa.query(i));
-			while (!dq.isEmpty() && dq.getLast().second >= psa.query(i)) {
+			while (!dq.isEmpty() && dq.peekLast().second >= psa.query(i)) {
 				dq.removeLast();
 			}
 			dq.addLast(add);
-			while (dq.getFirst().first < i - K) {
+			while (dq.peekFirst().first < i - K) {
 				dq.removeFirst();
 			}
-			ans = Math.max(ans, psa.query(i) - dq.getFirst().second);
+			ans = Math.max(ans, psa.query(i) - dq.peekFirst().second);
 		}
 		out.println(ans);
 		out.close();
