beb35c1f6a05b02079f2cbecf99e8c7126e2c2b7
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-07 23:26:35 -0400

    RTE16 Junior and Senior Completed (5th and 6th place)

---
+++ RTE Java/rte16j3_mule_wars/RTE16J3.java
@@ -0,0 +1,214 @@
+package rte16j3_mule_wars;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class RTE16J3 {
+	private static RTE16J3 o = new RTE16J3();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		}
+
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		}
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			}
+			return st.nextToken();
+		}
+
+		public String next(String delim) throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			}
+			return st.nextToken(delim);
+		}
+
+		/*
+		public BigInteger nextBigInteger() throws IOException {
+			return new BigInteger(next());
+		}
+		*/
+
+		public byte nextByte() throws IOException {
+			return Byte.parseByte(next());
+		}
+
+		public byte nextByte(String delim) throws IOException {
+			return Byte.parseByte(next(delim));
+		}
+
+		public char nextChar() throws IOException {
+			return next().charAt(0);
+		}
+
+		public char nextChar(String delim) throws IOException {
+			return next(delim).charAt(0);
+		}
+
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		}
+
+		public double nextDouble(String delim) throws IOException {
+			return Double.parseDouble(next(delim));
+		}
+
+		public float nextFloat() throws IOException {
+			return Float.parseFloat(next());
+		}
+
+		public float nextFloat(String delim) throws IOException {
+			return Float.parseFloat(next(delim));
+		}
+
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		}
+
+		public int nextInt(String delim) throws IOException {
+			return Integer.parseInt(next(delim));
+		}
+
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		}
+
+		public long nextLong(String delim) throws IOException {
+			return Long.parseLong(next(delim));
+		}
+
+		public short nextShort() throws IOException {
+			return Short.parseShort(next());
+		}
+
+		public short nextShort(String delim) throws IOException {
+			return Short.parseShort(next(delim));
+		}
+
+		public String nextLine() throws IOException {
+			st = null;
+			return in.readLine();
+		}
+	} // Reader class
+	
+	public class FenwickTree {
+
+	    int[] array; // 1-indexed array, In this array We save cumulative information to perform efficient range queries and updates
+
+	    public FenwickTree(int size) {
+	        array = new int[size + 1];
+	    }
+
+	    /**
+	     * Range Sum query from 1 to ind
+	     * ind is 1-indexed
+	     * <p>
+	     * Time-Complexity:    O(log(n))
+	     *
+	     * @param  ind index
+	     * @return sum
+	     */
+	    public int rsq(int ind) {
+	    	assert ind > 0;
+	        int sum = 0;
+	        for (int x = ind; x > 0; x -= (x & -x)) {
+	        	sum += array[x];
+	        }
+	        return sum;
+	    }
+
+	    /**
+	     * Range Sum Query from a to b.
+	     * Search for the sum from array index from a to b
+	     * a and b are 1-indexed
+	     * <p>
+	     * Time-Complexity:    O(log(n))
+	     *
+	     * @param  a left index
+	     * @param  b right index
+	     * @return sum
+	     */
+	    public int rsq(int a, int b) {
+	    	assert b >= a && a > 0 && b > 0;
+	        return rsq(b) - rsq(a - 1);
+	    }
+
+	    /**
+	     * Update the array at ind and all the affected regions above ind.
+	     * ind is 1-indexed
+	     * <p>
+	     * Time-Complexity:    O(log(n))
+	     *
+	     * @param  ind   index
+	     * @param  value value
+	     */
+	    public void update(int ind, int value) {
+	    	assert ind > 0;
+	    	for (int x = ind; x < array.length; x += (x & -x)) {
+	    		array[x] += value;
+	    	}
+	    }
+
+	    public int size() {
+	        return array.length - 1;
+	    }
+	}
+
+	
+	private static Reader in = o.new Reader(System.in);
+	private static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	
+	public static void main(String[] args) throws IOException {
+		int T = in.nextInt();
+		for (int i = 1; i <= T; i++) {
+			run(i);
+			out.flush();
+		}
+		out.close();
+	}
+	
+	public static void run(int testCaseNum) throws IOException {
+		int N = in.nextInt();
+		FenwickTree ft = o.new FenwickTree(N);
+		int C = in.nextInt();
+		HashSet<Integer> lost = new HashSet<Integer>();
+		for (int i = 0; i < C; i++) {
+			char c = in.nextChar();
+			if (c == 'A') {
+				int n = in.nextInt();
+				int x = in.nextInt();
+				if (!lost.contains(x)) ft.update(x, n);
+			} else if (c == 'Q') {
+				int x = in.nextInt();
+				out.println(ft.rsq(x, x));
+			} else if (c == 'G') {
+				out.println(ft.rsq(in.nextInt()));
+			} else if (c == 'L') {
+				out.println(ft.rsq(N - in.nextInt() + 1, N));
+			} else if (c == 'C') {
+				int x = in.nextInt();
+				lost.add(x);
+				ft.update(x, -ft.rsq(x, x));
+			} else {
+				i--;
+			}
+		}
+	}
+}
