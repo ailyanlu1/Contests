e3a84c1c2b35221f74804ddd578893303efeb7d8
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-15 18:19:53 -0400

    April Fools CCCJQRP3 Completed

---
+++ DMOJ C++/april_fools_cccjqrp3/CCCJQRP3.cpp
@@ -0,0 +1,50 @@
+/*
+ * CCCJQRP3.cpp
+ *
+ *  Created on: Jun 15, 2017
+ *      Author: Wesley Leung
+ */
+
+#include <bits/stdc++.h>
+
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 0x3f3f3f3f3f3f3f3f
+#define pb push_back
+#define mp make_pair
+#define left(x) x << 1
+#define right(x) x << 1 | 1
+#define mid(x, y) x + (y - x) / 2
+#define f first
+#define s second
+#define ri(x) scanf("%d", &x)
+#define rll(x) scanf("%lld", &x)
+#define rllu(x) scanf("%llu", &x)
+#define rf(x) scanf("%f", &x)
+#define rd(x) scanf("%lf", &x)
+#define rc(x) scanf("%c", &x)
+#define Fill(a, x) memset(a, x, sizeof(a))
+#define randi(a, b) rand() % (b - a + 1) + a
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+typedef pair<int, int> pii;
+typedef pair<float, float> pff;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<llu, llu> pllu;
+typedef map<int, int> mii;
+typedef map<int, ll> mill;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, ll> umill;
+
+llu A, B, C;
+
+int main() {
+	rllu(A);
+	rllu(B);
+	rllu(C);
+	printf("%llu\n", (A + B + C) % 42069900169420ull);
+	return 0;
+}
