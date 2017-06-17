f68a5853b1c2602f2e8e93df013cc40b34ba03cc
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-17 09:46:38 -0400

    Updated ECOO16R2P4

--- ECOO Java/ecoo16r2p4_hop_skip_and_jump/ECOO16R2P4.java
+++ ECOO Java/ecoo16r2p4_hop_skip_and_jump/ECOO16R2P4.java
@@ -1,17 +1,111 @@
 package ecoo16r2p4_hop_skip_and_jump;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import java.util.Scanner;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class ECOO16R2P4 {
-    private static int W = 0;
-    private static int H = 0;
-    private static char[][] b = new char[H][W];
-    private static final int NUM_OF_TEST_CASES = 10; // TODO CHANGE THIS TO THE NUMBER OF TEST CASES FOR THE QUESTION
     private static ECOO16R2P4 o = new ECOO16R2P4();
+    public class Reader {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public Reader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public Reader(String fileName) throws FileNotFoundException {
+            in = new BufferedReader(new FileReader(fileName));
+        }
+
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(in.readLine().trim());
+            }
+            return st.nextToken();
+        }
+
+        public String next(String delim) throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(in.readLine().trim());
+            }
+            return st.nextToken(delim);
+        }
+
+        /*
+        public BigInteger nextBigInteger() throws IOException {
+            return new BigInteger(next());
+        }
+        */
+
+        public byte nextByte() throws IOException {
+            return Byte.parseByte(next());
+        }
+
+        public byte nextByte(String delim) throws IOException {
+            return Byte.parseByte(next(delim));
+        }
+
+        public char nextChar() throws IOException {
+            return next().charAt(0);
+        }
+
+        public char nextChar(String delim) throws IOException {
+            return next(delim).charAt(0);
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public double nextDouble(String delim) throws IOException {
+            return Double.parseDouble(next(delim));
+        }
+
+        public float nextFloat() throws IOException {
+            return Float.parseFloat(next());
+        }
+
+        public float nextFloat(String delim) throws IOException {
+            return Float.parseFloat(next(delim));
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public int nextInt(String delim) throws IOException {
+            return Integer.parseInt(next(delim));
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public long nextLong(String delim) throws IOException {
+            return Long.parseLong(next(delim));
+        }
+
+        public short nextShort() throws IOException {
+            return Short.parseShort(next());
+        }
+
+        public short nextShort(String delim) throws IOException {
+            return Short.parseShort(next(delim));
+        }
+
+        public String nextLine() throws IOException {
+            st = null;
+            return in.readLine();
+        }
+    } // Reader class
     
     public class Bag<Item> implements Iterable<Item> {
         private Node<Item> first;    // beginning of bag
@@ -518,23 +612,28 @@ public class ECOO16R2P4 {
         }
     }
 
-    public static void main(String[] args) {
-        Scanner fileScanner = new Scanner(new BufferedInputStream(System.in));
+    private static Reader in = o.new Reader(System.in);
+    private static int W = 0;
+    private static int H = 0;
+    private static char[][] b = new char[H][W];
+    private static final int NUM_OF_TEST_CASES = 10; // TODO CHANGE THIS TO THE NUMBER OF TEST CASES FOR THE QUESTION
+    
+    public static void main(String[] args) throws IOException {
         for (int i = 0; i < NUM_OF_TEST_CASES; i++) {
-            run(fileScanner);
+            run();
         } // for i
     } // main method
     
-    public static void run(Scanner f) { // if you change the method of reading input, then the parameter type may have to be changed
-        W = f.nextInt();
-        H = f.nextInt();
+    public static void run() throws IOException {
+        W = in.nextInt();
+        H = in.nextInt();
         int h = 0, s = 0, j = 0, C = 0, F = 0, T = 0;
         b = new char[H][W];
         Digraph Hop = o.new Digraph(H*W);
         Digraph Skip = o.new Digraph(H*W);
         Digraph Jump = o.new Digraph(H*W);
         for (int i = 0; i < H; i++) {
-            String line = f.next();
+            String line = in.next();
             for (int k = 0; k < W; k++) {
                 char c = line.charAt(k);
                 if (c == 'h') {
