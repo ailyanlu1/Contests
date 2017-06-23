37bab1187381e4ec0f42c11e598597fa8cf262c9
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-23 08:49:46 -0400

    CCC11S2 (Multiple Choice) Completed

---
+++ CCC Java/ccc11s2_multiple_choice/CCC11S2.java
@@ -0,0 +1,126 @@
+package ccc11s2_multiple_choice;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CCC11S2 {
+    private static CCC11S2 o = new CCC11S2();
+    public class Reader {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public Reader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public Reader(String fileName) throws FileNotFoundException {
+            in = new BufferedReader(new FileReader(fileName));
+        }
+
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(in.readLine().trim());
+            }
+            return st.nextToken();
+        }
+
+        public String next(String delim) throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(in.readLine().trim());
+            }
+            return st.nextToken(delim);
+        }
+
+        /*
+        public BigInteger nextBigInteger() throws IOException {
+            return new BigInteger(next());
+        }
+        */
+
+        public byte nextByte() throws IOException {
+            return Byte.parseByte(next());
+        }
+
+        public byte nextByte(String delim) throws IOException {
+            return Byte.parseByte(next(delim));
+        }
+
+        public char nextChar() throws IOException {
+            return next().charAt(0);
+        }
+
+        public char nextChar(String delim) throws IOException {
+            return next(delim).charAt(0);
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public double nextDouble(String delim) throws IOException {
+            return Double.parseDouble(next(delim));
+        }
+
+        public float nextFloat() throws IOException {
+            return Float.parseFloat(next());
+        }
+
+        public float nextFloat(String delim) throws IOException {
+            return Float.parseFloat(next(delim));
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public int nextInt(String delim) throws IOException {
+            return Integer.parseInt(next(delim));
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public long nextLong(String delim) throws IOException {
+            return Long.parseLong(next(delim));
+        }
+
+        public short nextShort() throws IOException {
+            return Short.parseShort(next());
+        }
+
+        public short nextShort(String delim) throws IOException {
+            return Short.parseShort(next(delim));
+        }
+
+        public String nextLine() throws IOException {
+            st = null;
+            return in.readLine();
+        }
+    } // Reader class
+    
+    private static Reader in = o.new Reader(System.in);
+    private static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    
+    public static void main(String[] args) throws IOException {
+        int N = in.nextInt();
+        char[] student = new char[N];
+        for (int i = 0; i < N; i++) {
+            student[i] = in.nextChar();
+        }
+        int correct = 0;
+        for (int i = 0; i < N; i++) {
+            if (student[i] == in.nextChar()) correct++;
+        }
+        out.println(correct);
+        out.close();
+    }
+}
