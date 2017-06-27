d64deb8ca83e44c5f219cdb2ef314ac3712189a9
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-27 13:01:31 -0400

    Changes to CCC17S3

--- CCC Java/ccc17s3_nailed_it/CCC17S3.java
+++ CCC Java/ccc17s3_nailed_it/CCC17S3.java
@@ -1,33 +1,136 @@
 package ccc17s3_nailed_it;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class CCC17S3 {
-    public static Scanner keyboard = new Scanner(System.in);
+    private static CCC17S3 o = new CCC17S3();
+    public class Reader {
+        private BufferedReader in;
+        private StringTokenizer st;
 
-    public static void main(String[] args) {
-        int[] b = new int[2001];
-        int[] s = new int[4001];
-        int n = keyboard.nextInt();
+        public Reader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public Reader(String fileName) throws FileNotFoundException {
+            in = new BufferedReader(new FileReader(fileName));
+        }
+
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(in.readLine().trim());
+            }
+            return st.nextToken();
+        }
+
+        public String next(String delim) throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(in.readLine().trim());
+            }
+            return st.nextToken(delim);
+        }
+
+        /*
+        public BigInteger nextBigInteger() throws IOException {
+            return new BigInteger(next());
+        }
+        */
+
+        public byte nextByte() throws IOException {
+            return Byte.parseByte(next());
+        }
+
+        public byte nextByte(String delim) throws IOException {
+            return Byte.parseByte(next(delim));
+        }
+
+        public char nextChar() throws IOException {
+            return next().charAt(0);
+        }
+
+        public char nextChar(String delim) throws IOException {
+            return next(delim).charAt(0);
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public double nextDouble(String delim) throws IOException {
+            return Double.parseDouble(next(delim));
+        }
+
+        public float nextFloat() throws IOException {
+            return Float.parseFloat(next());
+        }
+
+        public float nextFloat(String delim) throws IOException {
+            return Float.parseFloat(next(delim));
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public int nextInt(String delim) throws IOException {
+            return Integer.parseInt(next(delim));
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public long nextLong(String delim) throws IOException {
+            return Long.parseLong(next(delim));
+        }
+
+        public short nextShort() throws IOException {
+            return Short.parseShort(next());
+        }
+
+        public short nextShort(String delim) throws IOException {
+            return Short.parseShort(next(delim));
+        }
+
+        public String nextLine() throws IOException {
+            st = null;
+            return in.readLine();
+        }
+    } // Reader class
+    
+    private static Reader in = o.new Reader(System.in);
+    
+    private static final int MAXL = 2000;
+
+    public static void main(String[] args) throws IOException {
+        int[] freq = new int[MAXL + 1];
+        int[] sum = new int[MAXL * 2 + 1];
+        int N = in.nextInt();
         int length = 0;
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            int l = keyboard.nextInt();
-            b[l]++;
+        for (int i = 0; i < N; i++) {
+            freq[in.nextInt()]++;
         }
-        for (int i = 1 ; i < b.length; i++) {
-            if (b[i] > 0) {
-                for (int j = i; j < b.length; j++) {
-                    if (i == j) s[i + j] += b[i] >> 1;
-                    else if (b[j] > 0) s[i + j] += Math.min(b[i], b[j]);
+        for (int i = 1; i <= MAXL; i++) {
+            if (freq[i] == 0) continue;
+            for (int j = 1; j < i; j++) {
+                if (freq[i] != 0 && freq[j] != 0) {
+                    sum[i + j] += Math.min(freq[i], freq[j]);
                 }
             }
+            sum[i * 2] += freq[i] / 2;
         }
-        for (int i = 1; i < s.length; i++) {
-            if (s[i] > length) {
-                length = s[i];
+        for (int i = 2; i <= 2 * MAXL; i++) {
+            if (sum[i] > length) {
+                length = sum[i];
                 count = 1;
-            } else if (s[i] == length) {
+            } else if (sum[i] == length) {
                 count++;
             }
         }
