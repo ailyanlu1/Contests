9dcf3af5f1d959889d21a1238c590b8e029cc8be
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-29 15:53:22 -0400

    DMOPC15C7P5 (Ariadne's Thread) Completed

---
+++ DMOJ Java/dmopc15c7p5_ariadnes_thread/DMOPC15C7P5.java
@@ -0,0 +1,374 @@
+package dmopc15c7p5_ariadnes_thread;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class DMOPC15C7P5 {
+    private static DMOPC15C7P5 o = new DMOPC15C7P5();
+    public class Reader {
+        private BufferedReader in;
+        private StringTokenizer st;
+
+        public Reader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public Reader(String fileName) throws FileNotFoundException {
+            in = new BufferedReader(new FileReader(fileName));
+        }
+
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(in.readLine().trim());
+            }
+            return st.nextToken();
+        }
+
+        public String next(String delim) throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(in.readLine().trim());
+            }
+            return st.nextToken(delim);
+        }
+
+        /*
+        public BigInteger nextBigInteger() throws IOException {
+            return new BigInteger(next());
+        }
+        */
+
+        public byte nextByte() throws IOException {
+            return Byte.parseByte(next());
+        }
+
+        public byte nextByte(String delim) throws IOException {
+            return Byte.parseByte(next(delim));
+        }
+
+        public char nextChar() throws IOException {
+            return next().charAt(0);
+        }
+
+        public char nextChar(String delim) throws IOException {
+            return next(delim).charAt(0);
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public double nextDouble(String delim) throws IOException {
+            return Double.parseDouble(next(delim));
+        }
+
+        public float nextFloat() throws IOException {
+            return Float.parseFloat(next());
+        }
+
+        public float nextFloat(String delim) throws IOException {
+            return Float.parseFloat(next(delim));
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public int nextInt(String delim) throws IOException {
+            return Integer.parseInt(next(delim));
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public long nextLong(String delim) throws IOException {
+            return Long.parseLong(next(delim));
+        }
+
+        public short nextShort() throws IOException {
+            return Short.parseShort(next());
+        }
+
+        public short nextShort(String delim) throws IOException {
+            return Short.parseShort(next(delim));
+        }
+
+        public String nextLine() throws IOException {
+            st = null;
+            return in.readLine();
+        }
+    } // Reader class
+    
+    public class Digraph {
+        private final String NEWLINE = System.getProperty("line.separator");
+
+        private final int V;           // number of vertices in this digraph
+        private int E;                 // number of edges in this digraph
+        private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
+        private int[] indegree;        // indegree[v] = indegree of vertex v
+        
+        /**
+         * Initializes an empty digraph with <em>V</em> vertices.
+         *
+         * @param  V the number of vertices
+         * @throws IllegalArgumentException if {@code V < 0}
+         */
+        public Digraph(int V) {
+            if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+            this.V = V;
+            this.E = 0;
+            indegree = new int[V];
+            adj = new ArrayList[V];
+            for (int v = 0; v < V; v++) {
+                adj[v] = new ArrayList<Integer>();
+            }
+        }
+
+        /**
+         * Initializes a new digraph that is a deep copy of the specified digraph.
+         *
+         * @param  G the digraph to copy
+         */
+        public Digraph(Digraph G) {
+            this(G.V());
+            this.E = G.E();
+            for (int v = 0; v < V; v++)
+                this.indegree[v] = G.indegree(v);
+            for (int v = 0; v < G.V(); v++) {
+                // reverse so that adjacency list is in same order as original
+                Stack<Integer> reverse = new Stack<Integer>();
+                for (int w : G.adj[v]) {
+                    reverse.push(w);
+                }
+                for (int w : reverse) {
+                    adj[v].add(w);
+                }
+            }
+        }
+            
+        /**
+         * Returns the number of vertices in this digraph.
+         *
+         * @return the number of vertices in this digraph
+         */
+        public int V() {
+            return V;
+        }
+
+        /**
+         * Returns the number of edges in this digraph.
+         *
+         * @return the number of edges in this digraph
+         */
+        public int E() {
+            return E;
+        }
+
+
+        /**
+         * Adds the directed edge v→w to this digraph.
+         *
+         * @param  v the tail vertex
+         * @param  w the head vertex
+         * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+         */
+        public void addEdge(int v, int w) {
+            adj[v].add(w);
+            indegree[w]++;
+            E++;
+        }
+
+        /**
+         * Returns the vertices adjacent from vertex {@code v} in this digraph.
+         *
+         * @param  v the vertex
+         * @return the vertices adjacent from vertex {@code v} in this digraph, as an iterable
+         * @throws IllegalArgumentException unless {@code 0 <= v < V}
+         */
+        public Iterable<Integer> adj(int v) {
+            return adj[v];
+        }
+
+        /**
+         * Returns the number of directed edges incident from vertex {@code v}.
+         * This is known as the <em>outdegree</em> of vertex {@code v}.
+         *
+         * @param  v the vertex
+         * @return the outdegree of vertex {@code v}               
+         * @throws IllegalArgumentException unless {@code 0 <= v < V}
+         */
+        public int outdegree(int v) {
+            return adj[v].size();
+        }
+
+        /**
+         * Returns the number of directed edges incident to vertex {@code v}.
+         * This is known as the <em>indegree</em> of vertex {@code v}.
+         *
+         * @param  v the vertex
+         * @return the indegree of vertex {@code v}               
+         * @throws IllegalArgumentException unless {@code 0 <= v < V}
+         */
+        public int indegree(int v) {
+            return indegree[v];
+        }
+
+        /**
+         * Returns the reverse of the digraph.
+         *
+         * @return the reverse of the digraph
+         */
+        public Digraph reverse() {
+            Digraph reverse = new Digraph(V);
+            for (int v = 0; v < V; v++) {
+                for (int w : adj(v)) {
+                    reverse.addEdge(w, v);
+                }
+            }
+            return reverse;
+        }
+
+        /**
+         * Returns a string representation of the graph.
+         *
+         * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,  
+         *         followed by the <em>V</em> adjacency lists
+         */
+        public String toString() {
+            StringBuilder s = new StringBuilder();
+            s.append(V + " vertices, " + E + " edges " + NEWLINE);
+            for (int v = 0; v < V; v++) {
+                s.append(String.format("%d: ", v));
+                for (int w : adj[v]) {
+                    s.append(String.format("%d ", w));
+                }
+                s.append(NEWLINE);
+            }
+            return s.toString();
+        }
+    }
+    
+    private static Reader in = o.new Reader(System.in);
+    private static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    
+    /*
+    private static char[][] grid;
+    
+    private static boolean isPoint(int i, int j) {
+        return (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length);
+    }
+    private static int toPoint(int i, int j) {
+        return i * grid[0].length + j;
+    }
+    private static int getI(int point) {
+        return point / grid[0].length;
+    }
+    private static int getJ(int point) {
+        return point % grid[0].length;
+    }
+    */
+    
+    private static Digraph G;
+    private static int[] depth, parent, chain, size, head, first;
+    private static TreeSet<Integer>[] occ;
+    private static int chainNum, count;
+    
+    public static void main(String[] args) throws IOException {
+        int N = in.nextInt();
+        G = o.new Digraph(N);
+        for (int i = 0; i < N; i++) {
+            int C = in.nextInt();
+            for (int j = 0; j < C; j++) {
+                G.addEdge(i, in.nextInt() - 1);
+            }
+        }
+        initLCA();
+        int Q = in.nextInt();
+        for (int i = 0; i < Q; i++) {
+            int u = in.nextInt() - 1;
+            int v = in.nextInt() - 1;
+            int root = lca(u, v);
+            if (first[v] < first[u]) {
+                out.println(first[v] - first[root] + (occ[root].ceiling(first[u]) - occ[root].floor(first[u]) - (depth[u] - depth[root])));
+            } else {
+                out.println(first[v] - first[root] - (depth[u] - depth[root]));
+            }
+        }
+        out.close();
+    }
+    
+    private static void initLCA() {
+        depth = new int[G.V()];
+        parent = new int[G.V()];
+        chain = new int[G.V()];
+        size = new int[G.V()];
+        head = new int[G.V()];
+        first = new int[G.V()];
+        occ = new TreeSet[G.V()];
+        for (int i = 0; i < G.V(); i++) {
+            first[i] = Integer.MAX_VALUE;
+            head[i] = -1;
+            occ[i] = new TreeSet<Integer>();
+        }
+        dfs(0, 0, -1);
+        hld(0, -1);
+    }
+    
+    private static void dfs(int v, int d, int prev) {
+        first[v] = Math.min(first[v], count);
+        occ[v].add(count++);
+        depth[v] = d;
+        parent[v] = prev;
+        size[v] = 1;
+        for (int w: G.adj(v)) {
+            if (w != prev) {
+                dfs(w, d + 1, v);
+                occ[v].add(count++);
+                size[v] += size[w];
+            }
+        }
+    }
+    
+    private static void hld(int v, int prev) {
+        if (head[chainNum] == -1) head[chainNum] = v;
+        chain[v] = chainNum;
+        int maxIndex = -1;
+        for (int w: G.adj(v)) {
+            if (w != prev && (maxIndex == -1 || size[maxIndex] < size[w])) maxIndex = w;
+        }
+        if (maxIndex != -1) hld(maxIndex, v);
+        for (int w: G.adj(v)) {
+            if (w != prev && w != maxIndex) {
+                chainNum++;
+                hld(w, v);
+            }
+        }
+    }
+    
+    /**
+     * Returns the lowest common ancestor of vertex {@code v} and {@code w}.
+     * 
+     * @param v the first vertex
+     * @param w the first vertex
+     * @return the lowest common ancestor of vertex {@code v} and {@code w}
+     */
+    public static int lca(int v, int w) {
+        while (chain[v] != chain[w]) {
+            if (depth[head[chain[v]]] < depth[head[chain[w]]]) w = parent[head[chain[w]]];
+            else v = parent[head[chain[v]]];
+        }
+        if (depth[v] < depth[w]) return v;
+        return w;
+    }
+}
