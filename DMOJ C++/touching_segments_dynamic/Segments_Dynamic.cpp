14f29ce7264ee46e67d18be7104bfc9c24c7d2aa
wesley-a-leung <wesley.a.leung@gmail.com>
2017-07-03 15:09:15 -0400

    Changes to Segments_Dynamic

--- DMOJ C++/touching_segments_dynamic/Segments_Dynamic.cpp
+++ DMOJ C++/touching_segments_dynamic/Segments_Dynamic.cpp
@@ -43,29 +43,18 @@ typedef unordered_map<ll, int> umlli;
 
 struct DynamicLazySegmentTree {
     struct Node {
-        Node *left = nullptr, *right = nullptr;
-        int maxVal = 0, lazy = 0;
-
-        Node(int val) {
-            this->maxVal = val;
-        }
-
-        Node(Node *l, Node *r) {
-            this->left = l;
-            this->right = r;
-            this->maxVal = max(l->maxVal, r->maxVal);
-        }
+        Node *left = nullptr;
+        Node *right = nullptr;
+        int val = 0, lazy = 0;
 
         void propogate() {
             if (this->lazy != 0) {
-                if (left != nullptr) {
-                    left->maxVal += this->lazy;
-                    left->lazy += this->lazy;
-                }
-                if (right != nullptr) {
-                    right->maxVal += this->lazy;
-                    right->lazy += this->lazy;
-                }
+                if (left == nullptr) left = new Node();
+                left->val += this->lazy;
+                left->lazy += this->lazy;
+                if (right == nullptr) right = new Node();
+                right->val += this->lazy;
+                right->lazy += this->lazy;
                 this->lazy = 0;
             }
         }
@@ -74,54 +63,36 @@ struct DynamicLazySegmentTree {
 private:
     Node *root;
     int N;
-    // int *array;
-
-    Node *build(int cL, int cR) {
-        if (cL == cR) return new Node(/*array[cL]*/0);
-        int m = (cL + cR) >> 1;
-        return new Node(build(cL , m), build(m + 1, cR));
-    }
 
     void update(Node *cur, int cL, int cR, int l, int r, int val) {
-        cur->propogate();
+        if (cL != cR) cur->propogate();
         if (cL > r || cR < l) return;
         if (cL >= l && cR <= r) {
-            cur->maxVal += val;
+            cur->val += val;
             cur->lazy += val;
             return;
         }
-        int m = (cL + cR) >> 1;
+        int m = cL + (cR - cL) / 2;
+        if (cur->left == nullptr) cur->left = new Node();
         update(cur->left, cL, m, l, r, val);
+        if (cur->right == nullptr) cur->right = new Node();
         update(cur->right, m + 1, cR, l, r, val);
-        cur->maxVal = max(cur->left->maxVal, cur->right->maxVal);
+        cur->val = max(cur->left->val, cur->right->val);
     }
 
     int rMaxQ(Node *cur, int cL, int cR, int l, int r) {
-        cur->propogate();
-        if (cL > r || cR < l) return INT_MIN;
-        if (cL >= l && cR <= r) return cur->maxVal;
-        int m = (cL + cR) >> 1;
+        if (cur == nullptr || cL > r || cR < l) return 0;
+        if (cL != cR) cur->propogate();
+        if (cL >= l && cR <= r) return cur->val;
+        int m = cL + (cR - cL) / 2;
         int left = rMaxQ(cur->left, cL, m, l, r);
         int right = rMaxQ(cur->right, m + 1, cR, l, r);
         return max(left, right);
     }
 
 public:
-    /*DynamicLazySegmentTree(int size, int *arr) {
-        array = new int[size + 1];
-        for (int i = 1; i <= size; i++) {
-            array[i] = arr[i - 1];
-        }
-        root = build(1, size);
-        N = size;
-    }*/
-
     DynamicLazySegmentTree(int size) {
-        /*array = new int[size + 1];
-        for (int i = 1; i <= size; i++) {
-            array[i] = 0;
-        }*/
-        root = build(1, size);
+        root = new Node();
         N = size;
     }
 
@@ -131,6 +102,10 @@ public:
 
     int rMaxQ(int l, int r) {
         return rMaxQ(root, 1, N, l, r);
+    }
+
+    int size() {
+        return N;
     }
 };
 
