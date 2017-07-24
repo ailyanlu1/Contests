f084246777f34a0312fc37ffee34466fcabc6d27
wesley-a-leung <wesley.a.leung@gmail.com>
2017-07-23 20:22:19 -0400

    CCC02J1 (0123456789) Completed

---
+++ CCC Python/ccc02j1_0123456789/CCC02J1.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+hor = ' *' * 3 + '\n'
+ver2 = ('*' + ' ' * 5 + '*\n') * 3
+verL = ('*' + ' ' * 6 + '\n') * 3
+verR = (' ' * 6 + '*\n') * 3
+if N == 0:
+    print(hor + ver2 + '\n' + ver2 + hor)
+elif N == 1:
+    print('\n' + verR + '\n' + verR + '\n')
+elif N == 2:
+    print(hor + verR + hor + verL + hor)
+elif N == 3:
+    print(hor + verR + hor + verR + hor)
+elif N == 4:
+    print('\n' + ver2 + hor + verR + '\n')
+elif N == 5:
+    print(hor + verL + hor + verR + hor)
+elif N == 6:
+    print(hor + verL + hor + ver2 + hor)
+elif N == 7:
+    print(hor + verR + '\n' + verR + '\n')
+elif N == 8:
+    print(hor + ver2 + hor + ver2 + hor)
+else:  # N == 9
+    print(hor + ver2 + hor + verR + hor)
