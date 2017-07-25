463a680b661346d2e28c88da59cbbb50c957ec6b
wesley-a-leung <wesley.a.leung@gmail.com>
2017-07-25 11:02:40 -0400

    Added Factorial in C++

---
+++ In Progress C++/factorial2/Factorial2.cpp
@@ -0,0 +1,67 @@
+/*
+ * Factorial2.cpp
+ *
+ *  Created on: Jul 25, 2017
+ *      Author: Wesley Leung
+ */
+
+#include <bits/stdc++.h>
+
+#define SHORT_INF 0x3f3f
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 0x3f3f3f3f3f3f3f3f
+#define D_INF numeric_limits<double>::infinity()
+#define pb push_back
+#define mp make_pair
+#define l(x) x << 1
+#define r(x) x << 1 | 1
+#define mid(x, y) x + (y - x) / 2
+#define f first
+#define s second
+#define ri(x) scanf("%d", &x)
+#define rll(x) scanf("%lld", &x)
+#define rllu(x) scanf("%llu", &x)
+#define rf(x) scanf("%f", &x)
+#define rd(x) scanf("%lf", &x)
+#define rc(x) scanf(" %c", &x)
+#define rs(x) scanf("%s", x)
+#define Fill(a, x) memset(a, x, sizeof(a))
+#define randi(a, b) rand() % (b - a + 1) + a
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+typedef pair<int, int> pii;
+typedef pair<float, float> pff;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<llu, llu> pllu;
+typedef map<int, int> mii;
+typedef map<int, ll> mill;
+typedef map<ll, int> mlli;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, ll> umill;
+typedef unordered_map<ll, int> umlli;
+
+const llu MOD = (1LLU << 32) - 5;
+
+int N;
+llu n, ans;
+
+int main() {
+    ri(N);
+    for (int i = 0; i < N; i++) {
+        rllu(n);
+        if (n >= MOD) {
+            printf("0\n");
+            continue;
+        }
+        ans = 1;
+        for (int j = 2; j <= n; j++) {
+            ans = (ans * j) % MOD;
+        }
+        printf("%llu\n", ans);
+    }
+    return 0;
+}
