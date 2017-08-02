440576efa675cd2098362f1ffbbe95536be545ed
wesley-a-leung <wesley.a.leung@gmail.com>
2017-08-02 08:17:18 +0000

    CCC05J5 (Bananas) Completed

---
+++ CCC Python/ccc05j5_bananas/CCC05J5.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+
+def isAWord(word):
+    if word == 'A':
+        return True
+    if len(word) >= 1 and word[0] == 'B' and isMonkey(word[1:-1]) and word[-1] == 'S':
+        return True
+    return False
+
+
+def isMonkey(word):
+    if isAWord(word):
+        return True
+    for i in range(len(word)):
+        if word[i] == 'N' and isAWord(word[:i]) and isMonkey(word[i + 1:]):
+            return True
+    return False
+
+
+word = input().strip()
+while word != 'X':
+    if isMonkey(word):
+        print('YES')
+    else:
+        print('NO')
+    word = input().strip()
