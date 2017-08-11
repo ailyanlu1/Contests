7217dbf3cdf73adcec1f300f44112b3c74029c54
wesley-a-leung <wesley.a.leung@gmail.com>
2017-08-11 18:19:18 -0400

    IOI16P3 (Shortcut) Completed

--- Templates C/Template.c
+++ Templates C/Template.c
@@ -1,16 +1,23 @@
-#include <bits/stdc++.h>
+/*
+ * Template.c
+ *
+ *  Created on: Aug 11, 2017
+ *      Author: Wesley Leung
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
 
 #define SHORT_INF 0x3f3f
 #define INT_INF 0x3f3f3f3f
 #define LL_INF 0x3f3f3f3f3f3f3f3f
 #define D_INF numeric_limits<double>::infinity()
 #define pb push_back
-#define mp make_pair
 #define l(x) (x << 1)
 #define r(x) (x << 1 | 1)
 #define mid(x, y) (x + (y - x) / 2)
-#define f first
-#define s second
+#define min(a, b) (a < b ? a : b)
+#define max(a, b) (a > b ? a : b)
 #define ri(x) scanf("%d", &x)
 #define rll(x) scanf("%lld", &x)
 #define rllu(x) scanf("%llu", &x)
@@ -21,27 +28,12 @@
 #define Fill(a, x) memset(a, x, sizeof(a))
 #define randi(a, b) (rand() % (b - a + 1) + a)
 
-using namespace std;
-
 typedef long long ll;
 typedef unsigned long long llu;
-typedef pair<int, int> pii;
-typedef pair<float, float> pff;
-typedef pair<double, double> pdd;
-typedef pair<ll, ll> pll;
-typedef pair<llu, llu> pllu;
-typedef map<int, int> mii;
-typedef map<int, ll> mill;
-typedef map<ll, int> mlli;
-typedef unordered_map<int, int> umii;
-typedef unordered_map<int, ll> umill;
-typedef unordered_map<ll, int> umlli;
 
 int main() {
     // TODO INSERT CODE HERE
     // freopen("in.txt", "r", stdin);
     // freopen("out.txt", "w", stdout);
-    // cin.sync_with_stdio(0);
-    // cin.tie(0);
     return 0;
 }
