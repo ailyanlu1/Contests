aee1895f047cc74693004ae2b4188ff6e3575e7b
wesley-a-leung <wesley.a.leung@gmail.com>
2017-08-15 18:13:40 -0400

    COCI08C3P5 (BST) Completed

---
+++ COCI C++/coci08c3p5_bst/COCI08C3P5.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define SHORT_INF 0x3f3f
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 0x3f3f3f3f3f3f3f3f
+#define D_INF numeric_limits<double>::infinity()
+#define pb push_back
+#define mp make_pair
+#define l(x) (x << 1)
+#define r(x) (x << 1 | 1)
+#define mid(x, y) (x + (y - x) / 2)
+#define f first
+#define s second
+#define ri(x) scanf("%d", &x)
+#define rll(x) scanf("%lld", &x)
+#define rllu(x) scanf("%llu", &x)
+#define rf(x) scanf("%f", &x)
+#define rd(x) scanf("%lf", &x)
+#define rc(x) scanf(" %c", &x)
+#define rs(x) scanf("%s", x)
+#define Fill(a, x) memset(a, x, sizeof(a))
+#define randi(a, b) (rand() % (b - a + 1) + a)
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+typedef pair<int, int> pii;
+typedef pair<float, float> pff;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<llu, llu> pllu;
+typedef map<int, int> mii;
+typedef map<int, ll> mill;
+typedef map<ll, int> mlli;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, ll> umill;
+typedef unordered_map<ll, int> umlli;
+
+int N, x;
+ll cnt = 0;
+mii depth;
+mii::iterator iter;
+
+int main() {
+    ri(N);
+    for (int i = 0; i < N; i++) {
+        ri(x);
+        iter = depth.lower_bound(x);
+        depth[x] = max(iter != depth.end() ? iter->second + 1 : 0, iter != depth.begin() ? (--iter)->second + 1 : 0);
+        cnt += depth[x];
+        printf("%lld\n", cnt);
+    }
+    return 0;
+}
