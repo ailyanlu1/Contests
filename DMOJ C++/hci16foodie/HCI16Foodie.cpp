44ff2298753ced23eeb75112a8a67912fce1a482
wesley-a-leung <wesley.a.leung@gmail.com>
2017-08-20 14:53:59 -0400

    HCI16Foodie Completed

---
+++ DMOJ C++/hci16foodie/HCI16Foodie.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+#define SHORT_INF 0x3f3f
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 0x3f3f3f3f3f3f3f3f
+#define D_INF numeric_limits<double>::infinity()
+#define pb push_back
+#define mp make_pair
+#define l(x) (x << 1)
+#define r(x) (x << 1 | 1)
+#define mid(x, y) (x + (y - x) / 2)
+#define f first
+#define s second
+#define ri(x) scanf("%d", &x)
+#define rll(x) scanf("%lld", &x)
+#define rllu(x) scanf("%llu", &x)
+#define rf(x) scanf("%f", &x)
+#define rd(x) scanf("%lf", &x)
+#define rc(x) scanf(" %c", &x)
+#define rs(x) scanf("%s", x)
+#define Fill(a, x) memset(a, x, sizeof(a))
+#define randi(a, b) (rand() % (b - a + 1) + a)
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+typedef pair<int, int> pii;
+typedef pair<float, float> pff;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef pair<llu, llu> pllu;
+typedef map<int, int> mii;
+typedef map<int, ll> mill;
+typedef map<ll, int> mlli;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, ll> umill;
+typedef unordered_map<ll, int> umlli;
+
+#define MAXC 1000005
+
+int C, N, c;
+bitset<MAXC> dp;
+
+int main() {
+    ri(C);
+    ri(N);
+    dp[0] = 1;
+    for (int i = 0; i < N; i++) {
+        ri(c);
+        dp |= (dp << c);
+    }
+    for (int i = C - 1; i >= 0; i--) {
+        if (dp[i]) {
+            printf("%d\n", i);
+            return 0;
+        }
+    }
+    printf("0\n");
+    return 0;
+}
