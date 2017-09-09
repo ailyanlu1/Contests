635adae52e6004717cb201e528979e7ad861f5f4
wesley-a-leung <wesley.a.leung@gmail.com>
2017-09-09 16:25:14 -0400

    BFS Reorganized

--- Templates C++/Template.cpp
+++ Templates C++/Template.cpp
@@ -57,5 +57,10 @@ typedef unordered_map<ll, int> umlli;
 template<typename T1, typename T2> struct pair_hash {size_t operator ()(const pair<T1, T2> &p) const {return 31 * hash<T1> {}(p.first) + hash<T2> {}(p.second);}};
 
 int main() {
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    // cin.sync_with_stdio(0);
+    // cin.tie(0);
+    // TODO INSERT CODE HERE
     return 0;
 }
