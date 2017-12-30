3218947029b3645104ecc0620748162d8038c36a
wesley-a-leung <wesley.a.leung@gmail.com>
2017-12-30 02:06:46 -0500

    HourRank Practice

---
+++ HackerRank C++/hr24p1_strong_password/HR24P1.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+#define SHORT_INF 0x3f3f
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 0x3f3f3f3f3f3f3f3f
+#define D_INF numeric_limits<double>::infinity()
+#define pb push_back
+#define mp make_pair
+#define l(x) ((x) << 1)
+#define r(x) ((x) << 1 | 1)
+#define m(x, y) ((x) + ((y) - (x)) / 2)
+#define MIN(a, b) ((a) = min((a), (b)))
+#define MAX(a, b) ((a) = max((a), (b)))
+#define f first
+#define s second
+#define ri(x) scanf("%d", &x)
+#define rll(x) scanf("%lld", &x)
+#define rllu(x) scanf("%llu", &x)
+#define rf(x) scanf("%f", &x)
+#define rd(x) scanf("%lf", &x)
+#define rc(x) scanf(" %c", &x)
+#define rs(x) scanf("%s", x)
+#define For(i, a, b) for (int i = (a); i < (b); i++)
+#define FOR(i, b) For(i, 0, b)
+#define Forit(i, c) for (auto i = (c).begin(); i != (c).end(); i++)
+#define Rev(i, a, b) for (int i = (a); i > (b); i--)
+#define REV(i, a) Rev(i, a, -1)
+#define Revit(i, c) for (auto i = (c).rbegin(); i != (c).rend(); i++)
+#define SET0(a) memset((a), 0, sizeof(a))
+#define Fill(a, x, n) FOR(_, n) (a)[_] = (x)
+#define randi(a, b) (rand() % ((b) - (a) + 1) + (a))
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<int, double> pid;
+typedef pair<double, int> pdi;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pill;
+typedef pair<ll, int> plli;
+typedef pair<llu, llu> pllu;
+typedef pair<int, llu> pillu;
+typedef pair<llu, int> pllui;
+typedef map<int, int> mii;
+typedef map<int, ll> mill;
+typedef map<ll, int> mlli;
+typedef map<ll, ll> mll;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, ll> umill;
+typedef unordered_map<ll, int> umlli;
+typedef unordered_map<ll, ll> umll;
+
+template<typename T1, typename T2> struct pair_hash {size_t operator ()(const pair<T1, T2> &p) const {return 31 * hash<T1> {}(p.first) + hash<T2> {}(p.second);}};
+
+int N;
+string S;
+string special = "!@#$%^&*()-+";
+
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> S;
+    int num = 1, low = 1, up = 1, spe = 1;
+    FOR(i, N) {
+        if ('0' <= S[i] && S[i] <= '9') num = 0;
+        if ('a' <= S[i] && S[i] <= 'z') low = 0;
+        if ('A' <= S[i] && S[i] <= 'Z') up = 0;
+        FOR(j, special.length()) {
+            if (S[i] == special[j]) {
+                spe = 0;
+                break;
+            }
+        }
+    }
+    int left = num + low + up + spe;
+    if (N + left < 6) left += 6 - (N + left);
+    printf("%d\n", left);
+    return 0;
+}
---
+++ HackerRank C++/powers_game/PowersGame.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+#define SHORT_INF 0x3f3f
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 0x3f3f3f3f3f3f3f3f
+#define D_INF numeric_limits<double>::infinity()
+#define pb push_back
+#define mp make_pair
+#define l(x) ((x) << 1)
+#define r(x) ((x) << 1 | 1)
+#define m(x, y) ((x) + ((y) - (x)) / 2)
+#define MIN(a, b) ((a) = min((a), (b)))
+#define MAX(a, b) ((a) = max((a), (b)))
+#define f first
+#define s second
+#define ri(x) scanf("%d", &x)
+#define rll(x) scanf("%lld", &x)
+#define rllu(x) scanf("%llu", &x)
+#define rf(x) scanf("%f", &x)
+#define rd(x) scanf("%lf", &x)
+#define rc(x) scanf(" %c", &x)
+#define rs(x) scanf("%s", x)
+#define For(i, a, b) for (int i = (a); i < (b); i++)
+#define FOR(i, b) For(i, 0, b)
+#define Forit(i, c) for (auto i = (c).begin(); i != (c).end(); i++)
+#define Rev(i, a, b) for (int i = (a); i > (b); i--)
+#define REV(i, a) Rev(i, a, -1)
+#define Revit(i, c) for (auto i = (c).rbegin(); i != (c).rend(); i++)
+#define SET0(a) memset((a), 0, sizeof(a))
+#define Fill(a, x, n) FOR(_, n) (a)[_] = (x)
+#define randi(a, b) (rand() % ((b) - (a) + 1) + (a))
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<int, double> pid;
+typedef pair<double, int> pdi;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pill;
+typedef pair<ll, int> plli;
+typedef pair<llu, llu> pllu;
+typedef pair<int, llu> pillu;
+typedef pair<llu, int> pllui;
+typedef map<int, int> mii;
+typedef map<int, ll> mill;
+typedef map<ll, int> mlli;
+typedef map<ll, ll> mll;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, ll> umill;
+typedef unordered_map<ll, int> umlli;
+typedef unordered_map<ll, ll> umll;
+
+template<typename T1, typename T2> struct pair_hash {size_t operator ()(const pair<T1, T2> &p) const {return 31 * hash<T1> {}(p.first) + hash<T2> {}(p.second);}};
+
+int T, n;
+
+int main() {
+    ri(T);
+    FOR(i, T) {
+        ri(n);
+        printf(n % 8 == 0 ? "Second\n" : "First\n");
+    }
+    return 0;
+}
