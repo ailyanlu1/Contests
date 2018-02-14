3e3cc19c62b359693ab25a0376cf11319a041852
wesley-a-leung <wesley.a.leung@gmail.com>
2018-02-13 22:45:18 -0500

    Fxed CCC02J1

--- CCC Python/ccc02j1_0123456789/CCC02J1.py
+++ CCC Python/ccc02j1_0123456789/CCC02J1.py
@@ -4,25 +4,25 @@ input = sys.stdin.readline
 N = int(input())
 hor = ' *' * 3 + '\n'
 ver2 = ('*' + ' ' * 5 + '*\n') * 3
-verL = ('*' + ' ' * 6 + '\n') * 3
+verL = ('*\n') * 3
 verR = (' ' * 6 + '*\n') * 3
 if N == 0:
-    print(hor + ver2 + '\n' + ver2 + hor)
+    print(hor + ver2 + '\n' + ver2 + hor, end="")
 elif N == 1:
-    print('\n' + verR + '\n' + verR + '\n')
+    print('\n' + verR + '\n' + verR + '\n', end="")
 elif N == 2:
-    print(hor + verR + hor + verL + hor)
+    print(hor + verR + hor + verL + hor, end="")
 elif N == 3:
-    print(hor + verR + hor + verR + hor)
+    print(hor + verR + hor + verR + hor, end="")
 elif N == 4:
-    print('\n' + ver2 + hor + verR + '\n')
+    print('\n' + ver2 + hor + verR + '\n', end="")
 elif N == 5:
-    print(hor + verL + hor + verR + hor)
+    print(hor + verL + hor + verR + hor, end="")
 elif N == 6:
-    print(hor + verL + hor + ver2 + hor)
+    print(hor + verL + hor + ver2 + hor, end="")
 elif N == 7:
-    print(hor + verR + '\n' + verR + '\n')
+    print(hor + verR + '\n' + verR + '\n', end="")
 elif N == 8:
-    print(hor + ver2 + hor + ver2 + hor)
+    print(hor + ver2 + hor + ver2 + hor, end="")
 else:  # N == 9
-    print(hor + ver2 + hor + verR + hor)
+    print(hor + ver2 + hor + verR + hor, end="")
