1a8f02f4b97234a00fd6ee53a4f4ba224fd5faa7
wesley-a-leung <wesley.a.leung@gmail.com>
2018-04-24 09:52:42 -0400

    Solved NCCO1D1P3 (A Segment Tree Problem) with a segment tree

--- CCO C++/ncco1d1p3_a_segment_tree_problem/NCCO1D1P3.cpp
+++ CCO C++/ncco1d1p3_a_segment_tree_problem/NCCO1D1P3.cpp
@@ -1,58 +1,29 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <deque>
 using namespace std;
 
-#define INT_INF 0x3f3f3f3f
-#define LL_INF 0x3f3f3f3f3f3f3f3f
-#define D_INF numeric_limits<double>::infinity()
-#define MIN(a, b) ((a) = min((a), (b)))
-#define MAX(a, b) ((a) = max((a), (b)))
-#define pb push_back
-#define eb emplace_back
-#define mp make_pair
-#define f first
-#define s second
-#define all(a) (a).begin(), (a).end()
-#define For(i, a, b) for (auto i = (a); i < (b); i++)
-#define FOR(i, b) For(i, 0, b)
-#define Rev(i, a, b) for (auto i = (a); i > (b); i--)
-#define REV(i, a) Rev(i, a, -1)
-#define sz(a) ((int) (a).size())
-#define nl '\n'
-#define sp ' '
-
-#define ll long long
-#define ld long double
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define pill pair<int, ll>
-#define plli pair<ll, int>
-#define pdd pair<double, double>
-#define uset unordered_set
-#define umap unordered_map
-#define pq priority_queue
-template<typename T> using minpq = pq<T, vector<T>, greater<T>>;
-template<typename T> using maxpq = pq<T, vector<T>, less<T>>;
-
-template<typename T1, typename T2> struct pair_hash {size_t operator ()(const pair<T1, T2> &p) const {return 31 * hash<T1> {}(p.first) + hash<T2> {}(p.second);}};
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
 
 int main() {
 //    freopen("in.txt", "r", stdin);
 //    freopen("out.txt", "w", stdout);
-//    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int N, M, C;
     bool ans = false;
-    scanf("%d %d %d", &N, &M, &C);
-    deque<pii> mindq, maxdq;
+    scan(N);
+    scan(M);
+    scan(C);
+    deque<pair<int, int>> mindq, maxdq;
     int a;
-    FOR(i, N) {
-        scanf("%d", &a);
-        while (!mindq.empty() && mindq.front().f <= i - M) mindq.pop_front();
-        while (!maxdq.empty() && maxdq.front().f <= i - M) maxdq.pop_front();
-        while (!mindq.empty() && mindq.back().s >= a) mindq.pop_back();
-        while (!maxdq.empty() && maxdq.back().s <= a) maxdq.pop_back();
-        mindq.push_back({i, a});
-        maxdq.push_back({i, a});
-        if (i >= M - 1 && maxdq.front().s - mindq.front().s <= C) {
+    for (int i = 0; i < N; i++) {
+        scan(a);
+        while (!mindq.empty() && mindq.front().first <= i - M) mindq.pop_front();
+        while (!maxdq.empty() && maxdq.front().first <= i - M) maxdq.pop_front();
+        while (!mindq.empty() && mindq.back().second >= a) mindq.pop_back();
+        while (!maxdq.empty() && maxdq.back().second <= a) maxdq.pop_back();
+        mindq.emplace_back(i, a);
+        maxdq.emplace_back(i, a);
+        if (i >= M - 1 && maxdq.front().second - mindq.front().second <= C) {
             ans = true;
             printf("%d\n", i - M + 2);
         }
---
+++ CCO C/ncco1d1p3_a_segment_tree_problem_segtree/NCCO1D1P3_SegTree.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define INT_INF 0x3f3f3f3f
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+#define MAXM 10005
+
+int N, M, C, TMAX[2 * MAXM], TMIN[2 * MAXM];
+
+void update(int i, int v) {
+    i += M - 1;
+    for (TMAX[i] = TMIN[i] = v; i >>= 1;) {
+        TMAX[i] = max(TMAX[i << 1], TMAX[i << 1 | 1]);
+        TMIN[i] = min(TMIN[i << 1], TMIN[i << 1 | 1]);
+    }
+}
+
+int main() {
+//    freopen("in.txt", "r", stdin);
+//    freopen("out.txt", "w", stdout);
+    char ans = 0;
+    scan(N);
+    scan(M);
+    scan(C);
+    int a;
+    for (int i = 0; i < 2 * M; i++) {
+        TMAX[i] = -INT_INF;
+        TMIN[i] = INT_INF;
+    }
+    for (int i = 0; i < N; i++) {
+        scan(a);
+        update(i % M + 1, a);
+        if (i >= M - 1 && TMAX[1] - TMIN[1] <= C) {
+            ans = 1;
+            printf("%d\n", i - M + 2);
+        }
+    }
+    if (!ans) printf("NONE\n");
+    return 0;
+}
