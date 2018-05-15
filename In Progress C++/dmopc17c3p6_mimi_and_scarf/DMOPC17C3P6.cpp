ae38fd0307da7a38c44ca03fce4944cf2af27237
wesley-a-leung <wesley.a.leung@gmail.com>
2018-05-14 21:47:20 -0400

    A few problems completed and in progress

--- In Progress C++/dmopc17c3p6_mimi_and_scarf/DMOPC17C3P6.cpp
+++ In Progress C++/dmopc17c3p6_mimi_and_scarf/DMOPC17C3P6.cpp
@@ -1,72 +1,72 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-#define SHORT_INF 0x3f3f
 #define INT_INF 0x3f3f3f3f
 #define LL_INF 0x3f3f3f3f3f3f3f3f
 #define D_INF numeric_limits<double>::infinity()
-#define pb push_back
-#define mp make_pair
-#define l(x) ((x) << 1)
-#define r(x) ((x) << 1 | 1)
-#define m(x, y) ((x) + ((y) - (x)) / 2)
 #define MIN(a, b) ((a) = min((a), (b)))
 #define MAX(a, b) ((a) = max((a), (b)))
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
 #define f first
 #define s second
-#define ri(x) scanf("%d", &x)
-#define rll(x) scanf("%lld", &x)
-#define rllu(x) scanf("%llu", &x)
-#define rf(x) scanf("%f", &x)
-#define rd(x) scanf("%lf", &x)
-#define rc(x) scanf(" %c", &x)
-#define rs(x) scanf("%s", x)
-#define For(i, a, b) for (int i = (a); i < (b); i++)
+#define all(a) (a).begin(), (a).end()
+#define For(i, a, b) for (auto i = (a); i < (b); i++)
 #define FOR(i, b) For(i, 0, b)
-#define Forit(i, c) for (auto i = (c).begin(); i != (c).end(); i++)
-#define Rev(i, a, b) for (int i = (a); i > (b); i--)
+#define Rev(i, a, b) for (auto i = (a); i > (b); i--)
 #define REV(i, a) Rev(i, a, -1)
-#define Revit(i, c) for (auto i = (c).rbegin(); i != (c).rend(); i++)
-#define FILL(a, x) memset((a), (x), sizeof(a))
-#define Fill(a, x, n) FOR(_, n) (a)[_] = (x)
-#define randi(a, b) (rand() % ((b) - (a) + 1) + (a))
+#define sz(a) ((int) (a).size())
+#define nl '\n'
+#define sp ' '
 
-using namespace std;
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pill pair<int, ll>
+#define plli pair<ll, int>
+#define pdd pair<double, double>
+#define uset unordered_set
+#define umap unordered_map
+#define pq priority_queue
+template<typename T> using minpq = pq<T, vector<T>, greater<T>>;
+template<typename T> using maxpq = pq<T, vector<T>, less<T>>;
 
-typedef long long ll;
-typedef unsigned long long llu;
-typedef pair<int, int> pii;
-typedef pair<double, double> pdd;
-typedef pair<int, double> pid;
-typedef pair<double, int> pdi;
-typedef pair<ll, ll> pll;
-typedef pair<int, ll> pill;
-typedef pair<ll, int> plli;
-typedef pair<llu, llu> pllu;
-typedef pair<int, llu> pillu;
-typedef pair<llu, int> pllui;
-typedef map<int, int> mii;
-typedef map<int, ll> mill;
-typedef map<ll, int> mlli;
-typedef map<ll, ll> mll;
-typedef unordered_map<int, int> umii;
-typedef unordered_map<int, ll> umill;
-typedef unordered_map<ll, int> umlli;
-typedef unordered_map<ll, ll> umll;
+template<typename T1, typename T2, typename H1 = hash<T1>, typename H2 = hash<T2>>
+struct pair_hash {size_t operator ()(const pair<T1, T2> &p) const {return 31 * H1 {}(p.first) + H2 {}(p.second);}};
 
-template<typename T1, typename T2> struct pair_hash {size_t operator ()(const pair<T1, T2> &p) const {return 31 * hash<T1> {}(p.first) + hash<T2> {}(p.second);}};
+#define MAXN 200005
 
-#define MAXN 1000010
-
-int N, K, a, b;
-ll C[MAXN];
+int N, K, ans, C[MAXN], ALL[MAXN], maxKey[MAXN], maxKey2[MAXN];
+bool exclude[MAXN];
 vector<int> adj[MAXN];
+umap<int, pii> large[MAXN], large2[MAXN];
+umap<int, int> small;
+uset<int> adjColors;
 
-inline void addEdge(int v, int w) {
-    adj[v].pb(w);
-    adj[w].pb(v);
+int getSize(int v, int prev) {
+    int size = 1;
+    for (int w : adj[v]) if (w != prev && !exclude[w]) size += getSize(w, v);
+    return size;
 }
 
-int findLongest(int v, int prev, ll even, ll odd, bool hasEven, bool hasOdd, int len, int run) {
+int getCentroid(int v, int prev, int treeSize) {
+    int n = treeSize;
+    int size = 1;
+    bool hasCentroid = true;
+    for (int w : adj[v]) {
+        if (w == prev || exclude[w]) continue;
+        int ret = getCentroid(w, v, treeSize);
+        if (ret >= 0) return ret;
+        hasCentroid &= -ret <= n / 2;
+        size += -ret;
+    }
+    hasCentroid &= n - size <= n / 2;
+    return hasCentroid ? v : -size;
+}
+
+int dfs(int v, int prev, int even, int odd, bool hasEven, bool hasOdd, int len, int run, int startRun, bool hasStartRun) {
     if (len % 2 == 0) {
         if (hasEven && C[v] == even) run++;
         else {
@@ -82,52 +82,99 @@ int findLongest(int v, int prev, ll even, ll odd, bool hasEven, bool hasOdd, int
             run = hasEven ? 2 : 1;
         }
     }
-    if (run >= K) return -1;
+    if (run >= K) return len - 1;
+    if (!hasStartRun && startRun < run) startRun = run;
+    else hasStartRun = true;
+    MAX(small[startRun], len);
     int ret = len;
     for (int w : adj[v]) {
-        if (w == prev) continue;
-        MAX(ret, findLongest(w, v, even, odd, hasEven, hasOdd, len + 1, run));
+        if (w == prev || exclude[w]) continue;
+        MAX(ret, dfs(w, v, even, odd, hasEven, hasOdd, len + 1, run, startRun, hasStartRun));
     }
     return ret;
-}
-
-pii findDiameter(int v, int prev, int dist) {
-    pii ret = {dist, v};
-    for (int w : adj[v]) {
-        if (w == prev) continue;
-        MAX(ret, findDiameter(w, v, dist + 1));
-    }
-    return ret;
-}
-
-int solve1() {
-    int ans = -1;
-    FOR(v, N) {
-        MAX(ans, findLongest(v, -1, -1, -1, false, false, 1, 0));
-    }
-    return ans;
-}
-
-int solve2() {
-    pii furthest = findDiameter(0, -1, 0);
-    return findLongest(furthest.s, -1, -1, -1, false, false, 1, 0);
 }
 
 int main() {
-    ri(N);
-    ri(K);
+//    freopen("in.txt", "r", stdin);
+//    freopen("out.txt", "w", stdout);
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> N >> K;
+    ans = 1;
     FOR(i, N) {
-        rll(C[i]);
+        cin >> C[i];
+        C[i]--;
     }
+    int a, b;
     FOR(i, N - 1) {
-        ri(a);
-        ri(b);
-        addEdge(a - 1, b - 1);
+        cin >> a >> b;
+        a--; b--;
+        adj[a].pb(b);
+        adj[b].pb(a);
     }
-    if (N == K) {
-        printf("%d\n", solve2());
-    } else {
-        printf("%d\n", solve1());
+    queue<int> q;
+    q.push(0);
+    while (!q.empty()) {
+        int v = q.front();
+        int c = getCentroid(v, -1, getSize(v, -1));
+        q.pop();
+        for (int w : adj[c]) {
+            if (exclude[w]) continue;
+            large[C[w]].clear();
+            large2[C[w]].clear();
+            ALL[C[w]] = 0;
+            maxKey[C[w]] = maxKey2[C[w]] = 0;
+            large[C[w]][0] = large2[C[w]][0] = {0, -1};
+        }
+        adjColors.clear();
+        for (int w : adj[c]) {
+            if (exclude[w]) continue;
+            small.clear();
+            small[1] = 1;
+            MAX(ALL[C[w]], dfs(w, c, -1, C[c], false, true, 2, 1, 1, false));
+            MAX(ans, ALL[C[w]]);
+            adjColors.insert(C[w]);
+            for (pii T : small) {
+                if (!large[C[w]].count(T.f - 1) || T.s - 1 >= large[C[w]][T.f - 1].f) {
+                    if (large[C[w]].count(T.f - 1)) {
+                        large2[C[w]][T.f - 1] = mp(large[C[w]][T.f - 1].f, large[C[w]][T.f - 1].s);
+                        MAX(maxKey2[C[w]], T.f - 1);
+                    }
+                    large[C[w]][T.f - 1] = mp(T.s - 1, w);
+                    MAX(maxKey[C[w]], T.f - 1);
+                } else if (!large2[C[w]].count(T.f - 1) || T.s - 1 > large2[C[w]][T.f - 1].f) {
+                    large2[C[w]][T.f - 1] = mp(T.s - 1, w);
+                    MAX(maxKey2[C[w]], T.f - 1);
+                }
+            }
+        }
+        for (int w : adj[c]) {
+            if (exclude[w]) continue;
+            small.clear();
+            small[1] = 1;
+            MAX(ALL[C[w]], dfs(w, c, -1, C[c], false, true, 2, 1, 1, false));
+            MAX(ans, ALL[C[w]]);
+            for (pii T : small) {
+                if (large[C[w]][min(maxKey[C[w]], K - T.f - 1)].s == w) {
+                    assert(large2[C[w]][min(maxKey2[C[w]], K - T.f - 1)].s != w);
+                    MAX(ans, T.s + (large2[C[w]][min(maxKey2[C[w]], K - T.f - 1)].f));
+                } else {
+                    MAX(ans, T.s + (large[C[w]][min(maxKey[C[w]], K - T.f - 1)].f));
+                }
+            }
+        }
+        int best1 = 1, best2 = 1;
+        for (int cc : adjColors) {
+            if (ALL[cc] >= best1) {
+                best2 = best1;
+                best1 = ALL[cc];
+            } else if (ALL[cc] > best2) {
+                best2 = ALL[cc];
+            }
+        }
+        MAX(ans, best1 + best2 - 1);
+        exclude[c] = true;
+        for (int w : adj[c]) if (!exclude[w]) q.push(w);
     }
+    cout << ans << nl;
     return 0;
 }
---
+++ SPOJ C#/meteors/METEORS.cs
@@ -0,0 +1,207 @@
+﻿// http://www.spoj.com/problems/METEORS/
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using static System.Diagnostics.Debug;
+
+namespace METEORS {
+    public class Program {
+        public class Reader {
+            private TextReader reader;
+            private Queue<string> tokens;
+
+            public Reader(Stream s) {
+                reader = new StreamReader(s);
+                tokens = new Queue<string>();
+            }
+
+            public Reader(string file) {
+                reader = new StreamReader(file);
+                tokens = new Queue<string>();
+            }
+
+            public string ReadLine() { return reader.ReadLine(); }
+
+            public string ReadToken() {
+                while (tokens.Count() == 0) tokens = new Queue<string>(ReadLine().Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries));
+                return tokens.Dequeue();
+            }
+
+            public char ReadChar() { return ReadToken()[0]; }
+            public sbyte ReadByte() { return sbyte.Parse(ReadToken()); }
+            public short ReadShort() { return short.Parse(ReadToken()); }
+            public int ReadInt() { return int.Parse(ReadToken()); }
+            public long ReadLong() { return long.Parse(ReadToken()); }
+            public float ReadFloat() { return float.Parse(ReadToken(), CultureInfo.InvariantCulture); }
+            public double ReadDouble() { return double.Parse(ReadToken(), CultureInfo.InvariantCulture); }
+            public decimal ReadDecimal() { return decimal.Parse(ReadToken(), CultureInfo.InvariantCulture); }
+            public void Close() { reader.Close(); }
+        }
+
+        private static Reader In;
+        private static TextWriter Out;
+
+        private static int NUM_OF_TEST_CASES = 1; // TODO CHANGE NUMBER OF TEST CASES
+
+        // TODO CHANGE FILE NAMES
+        private static string INPUT_FILE_NAME = "input.txt";
+        private static string OUTPUT_FILE_NAME = "output.txt";
+
+        private static bool stdIn = true;
+        private static bool stdOut = true;
+        private static bool crash = true;
+        private static bool flush = false;
+
+        private static int STACK_MEMORY_LIMIT_MB = 500; // TODO CHANGE MEMORY LIMIT FOR STACK
+
+        public static void Main(string[] args) {
+            Thread T = new Thread(new ThreadStart(start), STACK_MEMORY_LIMIT_MB * 1000000);
+            T.Start();
+        }
+
+        public static void start() {
+            if (stdIn) In = new Reader(Console.OpenStandardInput());
+            else In = new Reader(INPUT_FILE_NAME);
+            if (stdOut) Out = new StreamWriter(Console.OpenStandardOutput());
+            else Out = new StreamWriter(OUTPUT_FILE_NAME);
+            for (int i = 1; i <= NUM_OF_TEST_CASES; i++) {
+                try {
+                    run(i);
+                } catch (Exception e) {
+                    Out.WriteLine("Exception thrown on test case " + i);
+                    Out.WriteLine(e.StackTrace);
+                    Out.Flush();
+                    if (crash) throw new Exception();
+                }
+                if (flush) Out.Flush();
+            }
+            In.Close();
+            Out.Close();
+        }
+
+        public class FenwickTreeRangePoint {
+            private int size;
+            private long[] array;
+            public FenwickTreeRangePoint(int size) {
+                this.size = size;
+                array = new long[size + 1];
+                for (int i = 0; i <= size; i++) {
+                    array[i] = 0;
+                }
+            }
+
+            public long getValue(int ind) {
+                long sum = 0;
+                for (int x = ind; x > 0; x -= (x & -x)) {
+                    sum += array[x];
+                }
+                return sum;
+            }
+
+            public void update(int ind, long value) {
+                for (int x = ind; x <= size; x += (x & -x)) {
+                    array[x] += value;
+                }
+            }
+
+            public void update(int a, int b, long value) {
+                update(a, value);
+                update(b + 1, -value);
+            }
+        };
+
+        static int N, M, K;
+        static int[] ans;
+        static List<int>[] stations;
+        static long[] target;
+        static Tuple<int, int, long>[] A;
+        static FenwickTreeRangePoint ft;
+
+        static void solve(ref List<Tuple<int, long>> ind, int cL, int cR) {
+            if (cL == cR) {
+                foreach (Tuple<int, long> T in ind) ans[T.Item1] = cL;
+                return;
+            }
+            int m = cL + (cR - cL) / 2;
+            List<Tuple<int, long>> left = new List<Tuple<int, long>>(), right = new List<Tuple<int, long>>();
+            for (int i = cL; i <= m; i++) {
+                if (A[i].Item1 <= A[i].Item2) ft.update(A[i].Item1, A[i].Item2, A[i].Item3);
+                else {
+                    ft.update(A[i].Item1, M, A[i].Item3);
+                    ft.update(1, A[i].Item2, A[i].Item3);
+                }
+            }
+            foreach (Tuple<int, long> T in ind) {
+                long curSum = T.Item2;
+                foreach (int i in stations[T.Item1]) {
+                    curSum += ft.getValue(i);
+                    if (curSum >= target[T.Item1]) {
+                        left.Add(new Tuple<int, long>(T.Item1, T.Item2));
+                        break;
+                    }
+                }
+                if (curSum < target[T.Item1]) right.Add(new Tuple<int, long>(T.Item1, curSum));
+            }
+            for (int i = cL; i <= m; i++) {
+                if (A[i].Item1 <= A[i].Item2) ft.update(A[i].Item1, A[i].Item2, -A[i].Item3);
+                else {
+                    ft.update(A[i].Item1, M, -A[i].Item3);
+                    ft.update(1, A[i].Item2, -A[i].Item3);
+                }
+            }
+            solve(ref left, cL, m);
+            solve(ref right, m + 1, cR);
+        }
+
+        // TODO CODE GOES IN THIS METHOD
+        public static void run(int testCaseNum) {
+            N = In.ReadInt();
+            M = In.ReadInt();
+            stations = new List<int>[N + 1];
+            target = new long[N + 1];
+            ans = new int[N + 1];
+            ft = new FenwickTreeRangePoint(M);
+            for (int i = 1; i <= N; i++) stations[i] = new List<int>();
+            for (int i = 1; i <= M; i++) stations[In.ReadInt()].Add(i);
+            for (int i = 1; i <= N; i++) target[i] = In.ReadLong();
+            K = In.ReadInt();
+            A = new Tuple<int, int, long>[K + 1];
+            for (int i = 1; i <= K; i++) A[i] = new Tuple<int, int, long>(In.ReadInt(), In.ReadInt(), In.ReadLong());
+            List<Tuple<int, long>> toCheck = new List<Tuple<int, long>>();
+            for (int i = 1; i <= K; i++) {
+                if (A[i].Item1 <= A[i].Item2) ft.update(A[i].Item1, A[i].Item2, A[i].Item3);
+                else {
+                    ft.update(A[i].Item1, M, A[i].Item3);
+                    ft.update(1, A[i].Item2, A[i].Item3);
+                }
+            }
+            for (int i = 1; i <= N; i++) {
+                long curSum = 0;
+                foreach (int j in stations[i]) {
+                    curSum += ft.getValue(j);
+                    if (curSum >= target[i]) {
+                        toCheck.Add(new Tuple<int, long>(i, 0));
+                        break;
+                    }
+                }
+                if (curSum < target[i]) ans[i] = -1;
+            }
+            for (int i = 1; i <= K; i++) {
+                if (A[i].Item1 <= A[i].Item2) ft.update(A[i].Item1, A[i].Item2, -A[i].Item3);
+                else {
+                    ft.update(A[i].Item1, M, -A[i].Item3);
+                    ft.update(1, A[i].Item2, -A[i].Item3);
+                }
+            }
+            solve(ref toCheck, 1, K);
+            for (int i = 1; i <= N; i++) {
+                if (ans[i] == -1) Out.WriteLine("NIE");
+                else Out.WriteLine(ans[i]);
+            }
+        }
+    }
+}
