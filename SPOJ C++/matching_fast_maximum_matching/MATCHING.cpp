adc57f9b893e3a15d7528471b09206dbd70ffe19
wesley-a-leung <wesley.a.leung@gmail.com>
2018-05-16 00:37:26 -0400

    SPOJ Problems

---
+++ SPOJ C++/matching_fast_maximum_matching/MATCHING.cpp
@@ -0,0 +1,139 @@
+// http://www.spoj.com/problems/MATCHING/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 0x3f3f3f3f3f3f3f3f
+#define D_INF numeric_limits<double>::infinity()
+#define MIN(a, b) ((a) = min((a), (b)))
+#define MAX(a, b) ((a) = max((a), (b)))
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+#define all(a) (a).begin(), (a).end()
+#define For(i, a, b) for (auto i = (a); i < (b); i++)
+#define FOR(i, b) For(i, 0, b)
+#define Rev(i, a, b) for (auto i = (a); i > (b); i--)
+#define REV(i, a) Rev(i, a, -1)
+#define sz(a) ((int) (a).size())
+#define nl '\n'
+#define sp ' '
+
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pill pair<int, ll>
+#define plli pair<ll, int>
+#define pdd pair<double, double>
+#define uset unordered_set
+#define umap unordered_map
+#define pq priority_queue
+template<typename T> using minpq = pq<T, vector<T>, greater<T>>;
+template<typename T> using maxpq = pq<T, vector<T>, less<T>>;
+
+template<typename T1, typename T2, typename H1 = hash<T1>, typename H2 = hash<T2>>
+struct pair_hash {size_t operator ()(const pair<T1, T2> &p) const {return 31 * H1 {}(p.first) + H2 {}(p.second);}};
+
+class HopcroftKarpMaxMatchCompact {
+private:
+    int N;
+    vector<vector<int>> adj;
+    vector<bool> color;
+    vector<int> mate;
+    vector<int> dist;
+    vector<int> typeA;
+    int pathDist = INT_MAX;
+
+    bool hasPath() {
+        queue<int> q;
+        for (int v : typeA) {
+            if (mate[v] == -1) {
+                dist[v] = 0;
+                q.push(v);
+            } else {
+                dist[v] = INT_MAX;
+            }
+        }
+        pathDist = INT_MAX;
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            for (int w : adj[v]) {
+                if (mate[w] == -1) {
+                    if (pathDist == INT_MAX) pathDist = dist[v] + 1;
+                } else if (dist[mate[w]] == INT_MAX) {
+                    dist[mate[w]] = dist[v] + 1;
+                    if (pathDist == INT_MAX) q.push(mate[w]);
+                }
+            }
+        }
+        return pathDist != INT_MAX;
+    }
+
+    bool dfs(int v) {
+        for (int w : adj[v]) {
+            if (mate[w] == -1) {
+                if (pathDist == dist[v] + 1) {
+                    mate[w] = v;
+                    mate[v] = w;
+                    return true;
+                }
+            } else if (dist[mate[w]] == dist[v] + 1) {
+                if (dfs(mate[w])) {
+                    mate[w] = v;
+                    mate[v] = w;
+                    return true;
+                }
+            }
+        }
+        dist[v] = INT_MAX;
+        return false;
+    }
+
+public:
+    HopcroftKarpMaxMatchCompact(int N, vector<bool> &color) : N(N), adj(N), color(color), mate(N), dist(N) {
+        fill(mate.begin(), mate.end(), -1);
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].push_back(w);
+        adj[w].push_back(v);
+    }
+
+    int maxMatch() {
+        int cardinality = 0;
+        for (int v = 0; v < N; v++) if (color[v]) typeA.push_back(v);
+        while (hasPath()) for (int v : typeA) if (mate[v] == -1 && dfs(v)) cardinality++;
+        return cardinality;
+    }
+
+    int getMate(int v) {
+        return mate[v];
+    }
+};
+
+int N, M, P;
+vector<bool> color;
+
+int main() {
+//    freopen("in.txt", "r", stdin);
+//    freopen("out.txt", "w", stdout);
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> N >> M >> P;
+    color.reserve(N + M);
+    FOR(i, N) color.pb(false);
+    FOR(i, M) color.pb(true);
+    HopcroftKarpMaxMatchCompact mm(N + M, color);
+    int a, b;
+    FOR(i, P) {
+        cin >> a >> b;
+        a--; b--;
+        b += N;
+        mm.addEdge(a, b);
+    }
+    cout << mm.maxMatch() << nl;
+    return 0;
+}
