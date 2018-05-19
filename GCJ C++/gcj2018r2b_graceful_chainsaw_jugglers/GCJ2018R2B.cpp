a92054e32cdf324aecb6d3e830b1f05c7c36c88d
wesley-a-leung <wesley.a.leung@gmail.com>
2018-05-19 13:00:48 -0400

    GCJ 2018 Round 2

---
+++ GCJ C++/gcj2018r2b_graceful_chainsaw_jugglers/GCJ2018R2B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 0x3f3f3f3f3f3f3f3f
+#define D_INF numeric_limits<double>::infinity()
+#define MIN(a, b) ((a) = min((a), (b)))
+#define MAX(a, b) ((a) = max((a), (b)))
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+#define all(a) (a).begin(), (a).end()
+#define For(i, a, b) for (auto i = (a); i < (b); i++)
+#define FOR(i, b) For(i, 0, b)
+#define Rev(i, a, b) for (auto i = (a); i > (b); i--)
+#define REV(i, a) Rev(i, a, -1)
+#define sz(a) ((int) (a).size())
+#define nl '\n'
+#define sp ' '
+
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pill pair<int, ll>
+#define plli pair<ll, int>
+#define pdd pair<double, double>
+#define uset unordered_set
+#define umap unordered_map
+#define pq priority_queue
+template<typename T> using minpq = pq<T, vector<T>, greater<T>>;
+template<typename T> using maxpq = pq<T, vector<T>, less<T>>;
+
+template<typename T1, typename T2, typename H1 = hash<T1>, typename H2 = hash<T2>>
+struct pair_hash {size_t operator ()(const pair<T1, T2> &p) const {return 31 * H1 {}(p.first) + H2 {}(p.second);}};
+
+#define MAXRB 505
+#define CUTOFF 35
+
+int T, R, B, dp[MAXRB][MAXRB];
+
+void preCompute() {
+    FOR(i, MAXRB) FOR(j, MAXRB) dp[i][j] = 0;
+    FOR(i, CUTOFF) FOR(j, CUTOFF) if (i != 0 || j != 0) REV(ii, MAXRB - i - 1) REV(jj, MAXRB - j - 1) MAX(dp[i + ii][j + jj], dp[ii][jj] + 1);
+}
+
+int main() {
+//    freopen("in.txt", "r", stdin);
+//    freopen("out.txt", "w", stdout);
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> T;
+    preCompute();
+    For(t, 1, T + 1) {
+        cin >> R >> B;
+        cout << "Case #" << t << ": " << dp[R][B] << nl;
+    }
+    return 0;
+}
