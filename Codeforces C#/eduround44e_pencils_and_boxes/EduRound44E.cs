346d3b89a9d172f6296bec890df59649f38a98a4
wesley-a-leung <wesley.a.leung@gmail.com>
2018-05-22 18:02:09 -0400

    Educational Codeforces Round 44

---
+++ Codeforces C#/eduround44e_pencils_and_boxes/EduRound44E.cs
@@ -0,0 +1,102 @@
+﻿// http://codeforces.com/contest/985/problem/E
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using static System.Diagnostics.Debug;
+
+namespace EduRound44E {
+    public class Program {
+        public class Reader {
+            private TextReader reader;
+            private Queue<string> tokens;
+
+            public Reader(Stream s) {
+                reader = new StreamReader(s);
+                tokens = new Queue<string>();
+            }
+
+            public Reader(string file) {
+                reader = new StreamReader(file);
+                tokens = new Queue<string>();
+            }
+
+            public string ReadLine() { return reader.ReadLine(); }
+
+            public string ReadToken() {
+                while (tokens.Count() == 0) tokens = new Queue<string>(ReadLine().Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries));
+                return tokens.Dequeue();
+            }
+
+            public char ReadChar() { return ReadToken()[0]; }
+            public sbyte ReadByte() { return sbyte.Parse(ReadToken()); }
+            public short ReadShort() { return short.Parse(ReadToken()); }
+            public int ReadInt() { return int.Parse(ReadToken()); }
+            public long ReadLong() { return long.Parse(ReadToken()); }
+            public float ReadFloat() { return float.Parse(ReadToken(), CultureInfo.InvariantCulture); }
+            public double ReadDouble() { return double.Parse(ReadToken(), CultureInfo.InvariantCulture); }
+            public decimal ReadDecimal() { return decimal.Parse(ReadToken(), CultureInfo.InvariantCulture); }
+            public void Close() { reader.Close(); }
+        }
+
+        private static Reader In;
+        private static TextWriter Out;
+
+        private static int NUM_OF_TEST_CASES = 1; // TODO CHANGE NUMBER OF TEST CASES
+
+        // TODO CHANGE FILE NAMES
+        private static string INPUT_FILE_NAME = "input.txt";
+        private static string OUTPUT_FILE_NAME = "output.txt";
+
+        private static bool stdIn = true;
+        private static bool stdOut = true;
+        private static bool crash = true;
+        private static bool flush = false;
+
+        private static int STACK_MEMORY_LIMIT_MB = 150; // TODO CHANGE MEMORY LIMIT FOR STACK
+
+        public static void Main(string[] args) {
+            Thread T = new Thread(new ThreadStart(start), STACK_MEMORY_LIMIT_MB * 1000000);
+            T.Start();
+        }
+
+        public static void start() {
+            if (stdIn) In = new Reader(Console.OpenStandardInput());
+            else In = new Reader(INPUT_FILE_NAME);
+            if (stdOut) Out = new StreamWriter(Console.OpenStandardOutput());
+            else Out = new StreamWriter(OUTPUT_FILE_NAME);
+            for (int i = 1; i <= NUM_OF_TEST_CASES; i++) {
+                try {
+                    run(i);
+                } catch (Exception e) {
+                    Out.WriteLine("Exception thrown on test case " + i);
+                    Out.WriteLine(e.StackTrace);
+                    Out.Flush();
+                    if (crash) throw new Exception();
+                }
+                if (flush) Out.Flush();
+            }
+            In.Close();
+            Out.Close();
+        }
+
+        // TODO CODE GOES IN THIS METHOD
+        public static void run(int testCaseNum) {
+            int N = In.ReadInt(), K = In.ReadInt(), D = In.ReadInt();
+            int[] A = new int[N + 1], pre = new int[N + 1];
+            for (int i = 1; i <= N; i++) A[i] = In.ReadInt();
+            Array.Sort(A);
+            pre[0] = 1;
+            for (int i = 1, j = 1; i <= N; i++) {
+                while (A[i] - A[j] > D) j++;
+                pre[i] = pre[i - 1];
+                if (i - K >= 0 && pre[i - K] - (j - 2 < 0 ? 0 : pre[j - 2]) > 0) pre[i]++;
+            }
+            if (pre[N] - pre[N - 1] > 0) Out.WriteLine("YES");
+            else Out.WriteLine("NO");
+        }
+    }
+}
