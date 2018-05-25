f82d63199f3d83e0a0db79759e5ee516070bbd30
wesley-a-leung <wesley.a.leung@gmail.com>
2018-05-25 03:19:35 -0400

    ACM SGU Questions

---
+++ ACM Kotlin/sgu130_circle/SGU130.kt
@@ -0,0 +1,59 @@
+// http://codeforces.com/problemsets/acmsguru/problem/99999/130
+import java.io.*
+import java.math.*
+import java.util.*
+
+class Reader {
+    private val In: BufferedReader
+    private var st: StringTokenizer? = null
+    constructor(inputStream: InputStream) {
+        In = BufferedReader(InputStreamReader(inputStream))
+    }
+    constructor(fileName: String) {
+        In = BufferedReader(FileReader(fileName))
+    }
+    fun next(): String {
+        while (st == null || !st!!.hasMoreTokens()) {
+            st = StringTokenizer(In.readLine().trim())
+        }
+        return st!!.nextToken()
+    }
+    fun nextLine(): String {
+        st = null
+        return In.readLine()
+    }
+    fun nextChar(): Char = next()[0]
+    fun nextDouble(): Double = next().toDouble()
+    fun nextInt(): Int = next().toInt()
+    fun nextLong(): Long  = next().toLong()
+    fun close() : Unit = In.close();
+} // Reader class
+
+val INPUT_FILE_NAME = "input.txt"
+val OUTPUT_FILE_NAME = "output.txt"
+
+val In: Reader = Reader(System.`in`)
+val Out: PrintWriter = PrintWriter(System.out)
+//val In: Reader = Reader(INPUT_FILE_NAME)
+//val Out: PrintWriter = PrintWriter(OUTPUT_FILE_NAME)
+
+var NUM_OF_TEST_CASES: Int = 1
+val flush: Boolean = false
+
+fun main(args : Array<String>) {
+    for (i in 1..NUM_OF_TEST_CASES) {
+        run(i)
+        if (flush) Out.flush()
+    }
+    In.close()
+    Out.close()
+}
+
+fun run(testCaseNum: Int) {
+    var N = In.nextInt()
+    var comb = LongArray(N + 1)
+    comb[0] = 1
+    comb[1] = 1
+    for (i in 2..N) for (j in 0 until i) comb[i] += comb[j] * comb[i - j - 1];
+    Out.println("${comb[N]} ${N + 1}")
+}
