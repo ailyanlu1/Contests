ab32a64f0c3382ed7a4ca303d666f39eef447b48
wesley-a-leung <wesley.a.leung@gmail.com>
2018-06-05 03:44:32 -0400

    Renamed Fibonacci2

--- Templates Kotlin/TemplateKotlin.kt
+++ Templates Kotlin/TemplateKotlin.kt
@@ -28,11 +28,6 @@ class Reader {
     fun close() : Unit = In.close()
 }
 
-val In: Reader = Reader(System.`in`)
-val Out: PrintWriter = PrintWriter(System.out)
-//val In: Reader = Reader(INPUT_FILE_NAME)
-//val Out: PrintWriter = PrintWriter(OUTPUT_FILE_NAME)
-
 var NUM_OF_TEST_CASES: Int = 1 // TODO CHANGE NUMBER OF TEST CASES
 
 // TODO CHANGE FILE NAMES
@@ -41,6 +36,11 @@ val OUTPUT_FILE_NAME = "output.txt"
 
 val crash: Boolean = true
 val flush: Boolean = false
+
+val In: Reader = Reader(System.`in`)
+val Out: PrintWriter = PrintWriter(System.out)
+//val In: Reader = Reader(INPUT_FILE_NAME)
+//val Out: PrintWriter = PrintWriter(OUTPUT_FILE_NAME)
 
 fun main(args: Array<String>) {
     for (i in 1..NUM_OF_TEST_CASES) {
