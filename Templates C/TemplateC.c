261ba6a2d7157b678a4801357e115aaacdb3b7c4
wesley-a-leung <wesley.a.leung@gmail.com>
2018-06-11 17:47:00 -0400

    Compressed Template

--- Templates C/TemplateC.c
+++ Templates C/TemplateC.c
@@ -28,17 +28,17 @@
 
 #define _bufferSize 4096
 #define _maxNumLength 128
-char _inputBuffer[_bufferSize + 1], *_inputPtr = _inputBuffer, _c, _sign;
+char _inputBuffer[_bufferSize + 1],*_inputPtr=_inputBuffer,_c,_sign;
 int _cur;
-#define _getchar() (*_inputPtr ? *_inputPtr++ : (_inputBuffer[fread(_inputPtr = _inputBuffer, 1, _bufferSize, stdin)] = '\0', *_inputPtr++))
-#define _readSignAndNum(x) do { (x) = _getchar(); } while ((x) <= ' '); _sign = (x) == '-'; if (_sign) (x) = _getchar(); for ((x) -= '0'; (_c = _getchar()) >= '0'; (x) = (x) * 10 + _c - '0')
-#define _readFloatingPoint(x, T) for (T _div = 1.0; (_c = _getchar()) >= '0'; (x) += (_c - '0') / (_div *= 10))
-#define rc(x) do { do { (x) = _getchar(); } while ((x) <= ' '); } while (0)
-#define ri(x) do { _readSignAndNum(x); if (_sign) (x) = -(x); } while (0)
-#define rd(x) do { _readSignAndNum(x); if (_c == '.') _readFloatingPoint(x, double); if (_sign) (x) = -(x); } while (0)
-#define rld(x) do { _readSignAndNum(x); if (_c == '.') _readFloatingPoint(x, ld); if (_sign) (x) = -(x); } while (0)
-#define rcs(x) do{ _cur = 0; do { _c = _getchar(); } while (_c <= ' '); do { (x)[_cur++] = _c; } while ((_c = _getchar()) > ' '); (x)[_cur] = '\0'; } while (0)
-#define rcln(x) do { _cur = 0; do { _c = _getchar(); } while (_c <= ' '); do { (x)[_cur++] = _c; } while ((_c = _getchar()) >= ' '); (x)[_cur] = '\0'; } while (0)
+#define _getchar() (*_inputPtr?*_inputPtr++:(_inputBuffer[fread(_inputPtr=_inputBuffer,1,_bufferSize,stdin)]='\0',*_inputPtr++))
+#define _readSignAndNum(x) do{(x)=_getchar();}while((x)<=' ');_sign=(x)=='-';if(_sign)(x)=_getchar();for((x)-='0';(_c=_getchar())>='0';(x)=(x)*10+_c-'0')
+#define _readFloatingPoint(x,T) for(T _div=1.0;(_c=_getchar())>='0';(x)+=(_c-'0')/(_div*=10))
+#define rc(x) do{do{(x)=_getchar();}while((x)<=' ');}while(0)
+#define ri(x) do{_readSignAndNum(x);if(_sign)(x)=-(x);}while(0)
+#define rd(x) do{_readSignAndNum(x);if(_c=='.')_readFloatingPoint(x,double);if(_sign)(x)=-(x);}while(0)
+#define rld(x) do{_readSignAndNum(x);if(_c=='.')_readFloatingPoint(x,ld);if(_sign)(x)=-(x);}while(0)
+#define rcs(x) do{_cur=0;do{_c=_getchar();}while(_c<=' ');do{(x)[_cur++]=_c;}while((_c=_getchar())>' ');(x)[_cur]='\0';}while(0)
+#define rcln(x) do{_cur=0;do{_c=_getchar();}while(_c<=' ');do{(x)[_cur++]=_c;}while((_c=_getchar())>=' ');(x)[_cur]='\0';}while(0)
 
 int main() {
 //    freopen("in.txt", "r", stdin);
