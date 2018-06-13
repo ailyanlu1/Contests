17e2f8cc9d7317067b4db7271c5ab4594af009d3
wesley-a-leung <wesley.a.leung@gmail.com>
2018-06-13 17:39:08 -0400

    Testing fastio

--- TLE C++/tle17c8p5_battle_plan_2/TLE17C8P5.cpp
+++ TLE C++/tle17c8p5_battle_plan_2/TLE17C8P5.cpp
@@ -38,67 +38,50 @@ struct pair_hash {size_t operator ()(const pair<T1, T2> &p) const {return 31 * H
 
 #define _bufferSize 4096
 #define _maxNumLength 128
-char _inputBuffer[_bufferSize + 1], *_inputPtr = _inputBuffer, _outputBuffer[_bufferSize], _c, _sign, *_tempInputBuf = nullptr, _numBuf[_maxNumLength], _tempOutputBuf[_maxNumLength], _fill = ' ';
-const char* _delimiter = " ";
-int _cur, _outputPtr = 0, _numPtr = 0, _precision = 6, _width = 0, _tempOutputPtr = 0, _cnt;
-ll _precisionBase = 1000000;
-#define _getchar() (*_inputPtr ? *_inputPtr++ : (_inputBuffer[fread(_inputPtr = _inputBuffer, 1, _bufferSize, stdin)] = '\0', *_inputPtr++))
-#define _readSignAndNum(x) do { (x) = _getchar(); } while ((x) <= ' '); _sign = (x) == '-'; if (_sign) (x) = _getchar(); for ((x) -= '0'; (_c = _getchar()) >= '0'; (x) = (x) * 10 + _c - '0')
-#define _readFloatingPoint(x, T) for (T _div = 1.0; (_c = _getchar()) >= '0'; (x) += (_c - '0') / (_div *= 10))
-#define rc(x) do { do { (x) = _getchar(); } while ((x) <= ' '); } while (0)
-#define ri(x) do { _readSignAndNum(x); if (_sign) (x) = -(x); } while (0)
-#define rd(x) do { _readSignAndNum(x); if (_c == '.') _readFloatingPoint(x, double); if (_sign) (x) = -(x); } while (0)
-#define rld(x) do { _readSignAndNum(x); if (_c == '.') _readFloatingPoint(x, ld); if (_sign) (x) = -(x); } while (0)
-#define rcs(x) do{ _cur = 0; do { _c = _getchar(); } while (_c <= ' '); do { (x)[_cur++] = _c; } while ((_c = _getchar()) > ' '); (x)[_cur] = '\0'; } while (0)
-#define rs(x) do { if (!_tempInputBuf) assert(0); rcs(_tempInputBuf); (x) = string(_tempInputBuf, _cur); } while (0)
-#define rcln(x) do { _cur = 0; do { _c = _getchar(); } while (_c <= ' '); do { (x)[_cur++] = _c; } while ((_c = _getchar()) >= ' '); (x)[_cur] = '\0'; } while (0)
-#define rln(x) do { if (!_tempInputBuf) assert(0); rcln(_tempInputBuf); (x) = string(_tempInputBuf, _cur); } while (0)
-#define setLength(x) do { if (_tempInputBuf) delete[](_tempInputBuf); _tempInputBuf = new char[(x) + 1]; } while (0)
-void read(char &x) { rc(x); }
-void read(int &x) { ri(x); }
-void read(uint &x) { ri(x); }
-void read(ll &x) { ri(x); }
-void read(ull &x) { ri(x); }
-void read(double &x) { rd(x); }
-void read(ld &x) { rld(x); }
-void read(char* x) { rcs(x); }
-void read(string &x) { rs(x); }
-template <typename T, typename ...Ts> void read(T &&x, Ts &&...xs) { read(x); read(forward<Ts>(xs)...); }
-#define flush() (fwrite(_outputBuffer, 1, _outputPtr, stdout), _outputPtr = 0)
-#define _putchar(x) (_outputPtr == _bufferSize ? flush() : _outputBuffer[_outputPtr++] = (x))
-#define _writeTempBuf(x) (_tempOutputBuf[_tempOutputPtr++] = (x))
-#define _writeOutput() for (int _i = 0; _i < _tempOutputPtr; _putchar(_tempOutputBuf[_i++])); _tempOutputPtr = 0
-#define _writeNum(x, T, digits) _cnt = 0; for (T _y = (x); _y; _y /= 10, _cnt++) _numBuf[_numPtr++] = '0' + _y % 10; _flushNumBuf(); for (; _cnt < digits; _cnt++) _writeTempBuf('0')
-#define _writeFloatingPoint(x, T) _writeNum((ull) (x), ull, 1); _writeTempBuf('.'); _writeNum(((x) - ((ull) (x))) * _precisionBase + (T) (0.5), ull, _precision)
-#define _checkFinite(x) if (isnan(x)) { wcs("NaN"); } else if (isinf(x)) { wcs("Inf"); }
-#define _flushNumBuf() for (; _numPtr; _writeTempBuf(_numBuf[--_numPtr]))
-#define _fillBuf(x) for (int _i = 0; _i < (x); _i++) _putchar(_fill)
-#define _flushTempBuf() int _tempLen = _tempOutputPtr; _fillBuf(_width - _tempLen); _writeOutput(); _fillBuf(-_width - _tempLen)
-#define wc(x) do { _writeTempBuf(x); _flushTempBuf(); } while (0)
-#define wi(x) do { if ((x) < 0) { _writeTempBuf('-'); _writeNum(-(x), uint, 1); } else { _writeNum(x, uint, 1); } _flushTempBuf(); } while (0)
-#define wll(x) do { if ((x) < 0) { _writeTempBuf('-'); _writeNum(-(x), ull, 1); } else { _writeNum(x, ull, 1); } _flushTempBuf(); } while (0)
-#define wd(x) do { _checkFinite(x) else if ((x) < 0) { _writeTempBuf('-'); _writeFloatingPoint(-(x), double); } else { _writeFloatingPoint(x, double); } _flushTempBuf(); } while (0)
-#define wld(x) do { _checkFinite(x) else if ((x) < 0) { _writeTempBuf('-'); _writeFloatingPoint(-(x), ld); } else { _writeFloatingPoint(x, ld); } _flushTempBuf(); } while (0)
-#define wcs(x) do { int _slen = strlen(x); _fillBuf(_width - _slen); for (const char *_p = (x); *_p; _putchar(*_p++)); _fillBuf(-_width - _slen); } while (0)
-#define ws(x) do { _fillBuf(_width - (int) (x).length()); for (int _i = 0; _i < (int) (x).length(); _putchar(x[_i++])); _fillBuf(-_width - (int) (x).length()); } while (0)
-#define setPrecision(x) do { _precision = (x); _precisionBase = 1; for (int _i = 0; _i < (x); _i++, _precisionBase *= 10); } while (0)
-#define setDelimiter(x) do { _delimiter = (x); } while (0)
-#define setWidth(x) do { _width = (x); } while (0)
-#define setFill(x) do { _fill = (x); } while (0)
-void write(char x) { wc(x); }
-void write(int x) { wi(x); }
-void write(uint x) { wi(x); }
-void write(ll x) { wll(x); }
-void write(ull x) { wll(x); }
-void write(double x) { wd(x); }
-void write(ld x) { wld(x); }
-void write(const char* x) { wcs(x); }
-void write(string &x) { ws(x); }
-template <typename T, typename ...Ts> void write(T &&x, Ts &&...xs) { write(x); write(_delimiter); write(forward<Ts>(xs)...); }
-void writeln() { write('\n'); }
-template <typename ...Ts> void writeln(Ts &&...xs) { write(forward<Ts>(xs)...); write('\n'); }
-class IOManager { public: ~IOManager() { flush(); } };
-unique_ptr<IOManager> _iomanager(new IOManager());
+char _inputBuffer[_bufferSize+1],*_inputPtr=_inputBuffer,_outputBuffer[_bufferSize],_c,_sign,*_tempInputBuf=nullptr,_numBuf[_maxNumLength],_tempOutputBuf[_maxNumLength],_fill=' ';
+const char*_delimiter=" ";int _cur,_outputPtr=0,_numPtr=0,_precision=6,_width=0,_tempOutputPtr=0,_cnt;ll _precisionBase=1000000;
+#define _getchar() (*_inputPtr?*_inputPtr++:(_inputBuffer[fread(_inputPtr=_inputBuffer,1,_bufferSize,stdin)]='\0',*_inputPtr++))
+#define _readSignAndNum(x) do{(x)=_getchar();}while((x)<=' ');_sign=(x)=='-';if(_sign)(x)=_getchar();for((x)-='0';(_c=_getchar())>='0';(x)=(x)*10+_c-'0')
+#define _readFloatingPoint(x,T) for(T _div=1.0;(_c=_getchar())>='0';(x)+=(_c-'0')/(_div*=10))
+#define rc(x) do{do{(x)=_getchar();}while((x)<=' ');}while(0)
+#define ri(x) do{_readSignAndNum(x);if(_sign)(x)=-(x);}while(0)
+#define rd(x) do{_readSignAndNum(x);if(_c=='.')_readFloatingPoint(x,double);if(_sign)(x)=-(x);}while(0)
+#define rld(x) do{_readSignAndNum(x);if(_c=='.')_readFloatingPoint(x,ld);if(_sign)(x)=-(x);}while(0)
+#define rcs(x) do{_cur=0;do{_c=_getchar();}while(_c<=' ');do{(x)[_cur++]=_c;}while((_c=_getchar())>' ');(x)[_cur]='\0';}while(0)
+#define rs(x) do{if(!_tempInputBuf)assert(0);rcs(_tempInputBuf);(x)=string(_tempInputBuf,_cur);}while(0)
+#define rcln(x) do{_cur=0;do{_c=_getchar();}while(_c<=' ');do{(x)[_cur++]=_c;}while((_c=_getchar())>=' ');(x)[_cur]='\0';}while(0)
+#define rln(x) do{if(!_tempInputBuf)assert(0);rcln(_tempInputBuf);(x)=string(_tempInputBuf,_cur);}while(0)
+#define setLength(x) do{if(_tempInputBuf)delete[](_tempInputBuf);_tempInputBuf=new char[(x)+1];}while(0)
+void read(int&x){ri(x);}void read(uint&x){ri(x);}void read(ll&x){ri(x);}void read(ull&x){ri(x);}void read(double&x){rd(x);}void read(ld&x){rld(x);}
+void read(char&x){rc(x);}void read(char*x){rcs(x);}void read(string&x){rs(x);}
+template<typename T,typename...Ts>void read(T&&x,Ts&&...xs){read(x);read(forward<Ts>(xs)...);}
+#define _flush() (fwrite(_outputBuffer,1,_outputPtr,stdout),_outputPtr=0)
+#define _putchar(x) (_outputBuffer[_outputPtr==_bufferSize?_flush():_outputPtr]=(x),_outputPtr++)
+#define _writeTempBuf(x) (_tempOutputBuf[_tempOutputPtr++]=(x))
+#define _writeOutput() for(int _i=0;_i<_tempOutputPtr;_putchar(_tempOutputBuf[_i++]));_tempOutputPtr=0
+#define _writeNum(x,T,digits) _cnt=0;for(T _y=(x);_y;_y/=10,_cnt++)_numBuf[_numPtr++]='0'+_y%10;_flushNumBuf();for(;_cnt<digits;_cnt++)_writeTempBuf('0')
+#define _writeFloatingPoint(x,T) _writeNum((ull)(x),ull,1);_writeTempBuf('.');_writeNum(((x)-((ull)(x)))*_precisionBase+(T)(0.5),ull,_precision)
+#define _checkFinite(x) if(isnan(x)){wcs("NaN");}else if(isinf(x)){wcs("Inf");}
+#define _flushNumBuf() for(;_numPtr;_writeTempBuf(_numBuf[--_numPtr]))
+#define _fillBuf(x) for(int _i=0;_i<(x);_i++)_putchar(_fill)
+#define _flushTempBuf() int _tempLen=_tempOutputPtr;_fillBuf(_width-_tempLen);_writeOutput();_fillBuf(-_width-_tempLen)
+#define wc(x) do{_writeTempBuf(x); _flushTempBuf();}while(0)
+#define wi(x) do{if((x)<0){_writeTempBuf('-');_writeNum(-(x),uint,1);}else{_writeNum(x,uint,1);}_flushTempBuf();}while(0)
+#define wll(x) do{if((x)<0){_writeTempBuf('-');_writeNum(-(x),ull,1);}else{_writeNum(x,ull,1);}_flushTempBuf();}while(0)
+#define wd(x) do{_checkFinite(x)else if((x)<0){_writeTempBuf('-');_writeFloatingPoint(-(x),double);}else{_writeFloatingPoint(x,double);}_flushTempBuf();}while(0)
+#define wld(x) do{_checkFinite(x)else if((x)<0){_writeTempBuf('-');_writeFloatingPoint(-(x),ld);}else{_writeFloatingPoint(x,ld);}_flushTempBuf();}while(0)
+#define wcs(x) do{int _slen=strlen(x);_fillBuf(_width-_slen);for(const char*_p=(x);*_p;_putchar(*_p++));_fillBuf(-_width-_slen);}while(0)
+#define ws(x) do{_fillBuf(_width-(int)(x).length());for(int _i=0;_i<(int)(x).length();_putchar(x[_i++]));_fillBuf(-_width-(int)(x).length());}while(0)
+#define setPrecision(x) do{_precision=(x);_precisionBase=1;for(int _i=0;_i<(x);_i++,_precisionBase*=10);}while(0)
+#define setDelimiter(x) do{_delimiter=(x);}while(0)
+#define setWidth(x) do{_width=(x);}while(0)
+#define setFill(x) do{_fill=(x);}while(0)
+void write(int x){wi(x);}void write(uint x){wi(x);}void write(ll x){wll(x);}void write(ull x){wll(x);}void write(double x){wd(x);}void write(ld x){wld(x);}
+void write(char x){wc(x);}void write(char*x){wcs(x);}void write(const char*x){wcs(x);}void write(string&x){ws(x);}
+template<typename T,typename...Ts>void write(T&&x,Ts&&...xs){write(x);for(const char*_p=_delimiter;*_p;_putchar(*_p++));write(forward<Ts>(xs)...);}
+void writeln(){_putchar('\n');}template<typename...Ts>void writeln(Ts&&...xs){write(forward<Ts>(xs)...);_putchar('\n');}
+void flush(){_flush();}
+class IOManager{public:~IOManager(){flush();}};unique_ptr<IOManager>_iomanager=make_unique<IOManager>();
 
 #define MAXN 200005
 #define LGN 20
