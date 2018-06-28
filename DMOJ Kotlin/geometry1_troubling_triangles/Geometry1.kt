9d8dbe28b266767f522e64f1ab6020062e528c43
wesley-a-leung <wesley.a.leung@gmail.com>
2018-06-28 00:50:25 -0400

    Changes to kotlin template

--- DMOJ Kotlin/geometry1_troubling_triangles/Geometry1.kt
+++ DMOJ Kotlin/geometry1_troubling_triangles/Geometry1.kt
@@ -12,9 +12,7 @@ class Reader {
         In = BufferedReader(FileReader(fileName))
     }
     fun next(): String {
-        while (st == null || !st!!.hasMoreTokens()) {
-            st = StringTokenizer(In.readLine().trim())
-        }
+        while (st == null || !st!!.hasMoreTokens()) st = StringTokenizer(In.readLine().trim())
         return st!!.nextToken()
     }
     fun nextLine(): String {
