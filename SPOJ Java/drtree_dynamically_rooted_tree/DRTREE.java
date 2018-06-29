2efec047dd6fabc71c68fb69195be7f6a94b7118
wesley-a-leung <wesley.a.leung@gmail.com>
2018-06-29 18:50:29 -0400

    SPOJ, Kattis, Summer Institute, Codeforces Problems

---
+++ SPOJ Java/drtree_dynamically_rooted_tree/DRTREE.java
@@ -0,0 +1,253 @@
+// https://www.spoj.com/problems/DRTREE/
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class DRTREE {
+    public static class Reader {
+        private final int BUFFER_SIZE = 1 << 12;
+        private int LENGTH = -1;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader(InputStream inputStream) {
+            din = new DataInputStream(inputStream);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c;
+            do {
+                c = read();
+            } while (c <= ' ');
+            while (c <= ' ') c = read();
+            boolean neg = (c == '-');
+            if (neg) c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0');
+            if (neg) return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c;
+            do {
+                c = read();
+            } while (c <= ' ');
+            boolean neg = (c == '-');
+            if (neg) c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0');
+            if (neg) return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c;
+            do {
+                c = read();
+            } while (c <= ' ');
+            boolean neg = (c == '-');
+            if (neg) c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0');
+            if (c == '.') while ((c = read()) >= '0') ret += (c - '0') / (div *= 10);
+            if (neg) return -ret;
+            return ret;
+        }
+
+        public char nextChar() throws IOException {
+            byte c;
+            do {
+                c = read();
+            } while (c <= ' ');
+            return (char) c;
+        }
+
+        public String next() throws IOException {
+            byte[] buf = new byte[LENGTH];
+            int cnt = 0, c;
+            do {
+                c = read();
+            } while (c <= ' ');
+            do {
+                buf[cnt++] = (byte) c;
+            } while ((c = read()) > ' ');
+            return new String(buf, 0, cnt);
+        }
+
+        public String nextLine() throws IOException {
+            byte[] buf = new byte[LENGTH];
+            int cnt = 0, c;
+            do {
+                c = read();
+            } while (c <= ' ');
+            do {
+                buf[cnt++] = (byte) c;
+            } while ((c = read()) >= ' ');
+            return new String(buf, 0, cnt);
+        }
+        
+        public void setLength(int length) {
+            LENGTH = length;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null) return;
+            din.close();
+        }
+    }
+    
+    private static Reader in;
+    private static PrintWriter out;
+    
+    private static int NUM_OF_TEST_CASES = 1; // TODO CHANGE NUMBER OF TEST CASES
+    
+    // TODO CHANGE FILE NAMES
+    private static final String INPUT_FILE_NAME = "input.txt";
+    private static final String OUTPUT_FILE_NAME = "output.txt";
+    
+    private static boolean stdIn = true;
+    private static boolean stdOut = true;
+    private static boolean crash = true;
+    private static boolean flush = false;
+    
+    public static void main(String[] args) throws Exception {
+        if (stdIn) in = new Reader(System.in);
+        else in = new Reader(INPUT_FILE_NAME);
+        if (stdOut) out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        else out = new PrintWriter(new BufferedWriter(new FileWriter(OUTPUT_FILE_NAME)));
+        for (int i = 1; i <= NUM_OF_TEST_CASES; i++) {
+            try {
+                run(i);
+            } catch (Exception e) {
+                out.println("Exception thrown on test case " + i);
+                e.printStackTrace(out);
+                out.flush();
+                if (crash) throw new Exception();
+            }
+            if (flush) out.flush();
+        }
+        in.close();
+        out.close();
+    }
+    
+    static int N, Q, curInd, chainNum, ROOT;
+    static ArrayList<Integer>[] adj;
+    static int[] depth, parent, size, head, chain, pre, post, vertex;
+    static long[] T, W;
+    
+    static long querySum(int l, int r) {
+        return T[r] - T[l - 1];
+    }
+    
+    static void dfs(int v) {
+        size[v] = 1;
+        for (int w : adj[v]) {
+            parent[w] = v;
+            depth[w] = depth[v] + 1;
+            dfs(w);
+            size[v] += size[w];
+        }
+    }
+    static void hld(int v) {
+        if (head[chainNum] == -1) head[chainNum] = v;
+        chain[v] = chainNum;
+        pre[v] = ++curInd;
+        vertex[curInd] = v;
+        int maxIndex = -1;
+        for (int w : adj[v]) if (maxIndex == -1 || size[maxIndex] < size[w]) maxIndex = w;
+        if (maxIndex != -1) hld(maxIndex);
+        for (int w : adj[v]) {
+            if (w == maxIndex) continue;
+            chainNum++;
+            hld(w);
+        }
+        post[v] = curInd;
+    }
+    
+    // is w in the subtree of v
+    static boolean inSubtree(int v, int w) {
+        return pre[v] < pre[w] && post[v] >= post[w];
+    }
+
+    // finds the direct child of v that is the root of the subtree containing w
+    static int findDirectChild(int v, int w) {
+        int prev = -1;
+        while (chain[w] != chain[v]) {
+            prev = w;
+            w = parent[head[chain[w]]];
+        }
+        if (v == w) return head[chain[prev]];
+        else return vertex[pre[v] + 1];
+    }
+
+    static long querySubtreeSum(int v) {
+        if (v == ROOT) {
+            return querySum(pre[0], post[0]);
+        } else if (inSubtree(v, ROOT)) {
+            int w = findDirectChild(v, ROOT);
+            return querySum(pre[0], pre[w] - 1) + querySum(post[w] + 1, post[0]);
+        } else {
+            return querySum(pre[v], post[v]);
+        }
+    }
+    
+    public static void run(int testCaseNum) throws Exception {
+        N = in.nextInt();
+        W = new long[N];
+        adj = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            W[i] = in.nextLong();
+            adj[i] = new ArrayList<Integer>();
+        }
+        for (int i = 1; i < N; i++) adj[in.nextInt() - 1].add(i);
+        curInd = chainNum = 0;
+        depth = new int[N];
+        parent = new int[N];
+        size = new int[N];
+        head = new int[N];
+        chain = new int[N];
+        pre = new int[N];
+        post = new int[N];
+        vertex = new int[N + 1];
+        ROOT = 0;
+        depth[ROOT] = 0;
+        parent[ROOT] = -1;
+        for (int i = 0; i < N; i++) head[i] = -1;
+        dfs(ROOT);
+        hld(ROOT);
+        T = new long[N + 1];
+        T[0] = 0;
+        for (int i = 1; i <= N; i++) T[i] = T[i - 1] + W[vertex[i]];
+        Q = in.nextInt();
+        for (int i = 0; i < Q; i++) {
+            if (in.nextChar() == 'R') ROOT = in.nextInt() - 1;
+            else out.println(querySubtreeSum(in.nextInt() - 1));
+        }
+    }
+}
