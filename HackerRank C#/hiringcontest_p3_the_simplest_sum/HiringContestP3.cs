a506d98c7c853b42cc4f8f682f1f951038909019
wesley-a-leung <wesley.a.leung@gmail.com>
2018-07-02 17:30:59 -0400

    HackerRank Questions

---
+++ HackerRank C#/hiringcontest_p3_the_simplest_sum/HiringContestP3.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using static System.Diagnostics.Debug;
+
+namespace HiringContestP3 {
+    public class Program {
+        public class Reader {
+            private TextReader reader;
+            private string[] tokens;
+            private int currentToken;
+
+            public Reader(Stream s) {
+                reader = new StreamReader(s);
+                tokens = new string[0];
+                currentToken = 0;
+            }
+
+            public Reader(string file) {
+                reader = new StreamReader(file);
+                tokens = new string[0];
+                currentToken = 0;
+            }
+
+            public string ReadLine() { return reader.ReadLine();  }
+
+            public string ReadToken() {
+                while (currentToken == tokens.Length) {
+                    tokens = ReadLine().Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries);
+                    currentToken = 0;
+                }
+                return tokens[currentToken++];
+            }
+
+            public char ReadChar() { return ReadToken()[0]; }
+            public int ReadInt() { return int.Parse(ReadToken()); }
+            public long ReadLong() { return long.Parse(ReadToken()); }
+            public double ReadDouble() { return double.Parse(ReadToken(), CultureInfo.InvariantCulture); }
+            public decimal ReadDecimal() { return decimal.Parse(ReadToken(), CultureInfo.InvariantCulture); }
+            public void Close() { reader.Close(); }
+        }
+
+        private static Reader In;
+        private static TextWriter Out;
+
+        private static int NUM_OF_TEST_CASES = 1; // TODO CHANGE NUMBER OF TEST CASES
+
+        // TODO CHANGE FILE NAMES
+        private static string INPUT_FILE_NAME = "input.txt";
+        private static string OUTPUT_FILE_NAME = "output.txt";
+
+        private static bool stdIn = true;
+        private static bool stdOut = true;
+        private static bool crash = true;
+        private static bool flush = false;
+
+        private static int STACK_MEMORY_LIMIT_MB = 10; // TODO CHANGE MEMORY LIMIT FOR STACK
+
+        public static void Main(string[] args) {
+            Thread T = new Thread(new ThreadStart(start), STACK_MEMORY_LIMIT_MB * 1000000);
+            T.Start();
+        }
+
+        public static void start() {
+            if (stdIn) In = new Reader(Console.OpenStandardInput());
+            else In = new Reader(INPUT_FILE_NAME);
+            if (stdOut) Out = new StreamWriter(Console.OpenStandardOutput());
+            else Out = new StreamWriter(OUTPUT_FILE_NAME);
+            for (int i = 1; i <= NUM_OF_TEST_CASES; i++) {
+                try {
+                    run(i);
+                } catch (Exception e) {
+                    Out.WriteLine("Exception thrown on test case " + i);
+                    Out.WriteLine(e.StackTrace);
+                    Out.Flush();
+                    if (crash) throw new Exception();
+                }
+                if (flush) Out.Flush();
+            }
+            In.Close();
+            Out.Close();
+        }
+
+        static long MOD = (long) 1e9 + 7;
+
+        static long f(long k, long n) {
+            long sum = 0;
+            for (long i = 1; i <= n; sum = (sum + i) % MOD, i = i * k + 1);
+            return sum;
+        }
+
+        static long sumf(long k, long n) {
+            long sum = 0;
+            long lo = 0, hi = 1;
+            do {
+                sum += (Math.Max(0, Math.Min(hi, n + 1) - lo) % MOD) * f(k, lo) % MOD;
+                lo = lo * k + 1;
+                hi = hi * k + 1;
+            } while (lo <= n);
+            return sum;
+        }
+
+        public static void run(int testCaseNum) {
+            int Q = In.ReadInt();
+            for (int i = 0; i < Q; i++) {
+                long k = In.ReadLong(), a = In.ReadLong(), b = In.ReadLong();
+                Out.WriteLine((sumf(k, b) - sumf(k, a - 1) + MOD) % MOD);
+            }
+        }
+    }
+}
