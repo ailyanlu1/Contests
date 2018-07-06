1c2ccfe79a94ac64ec22b5ea212f234c5eac45bf
wesley-a-leung <wesley.a.leung@gmail.com>
2018-07-05 22:19:51 -0400

    Cactus on Kattis

---
+++ Kattis Java/cactus/Cactus.java
@@ -0,0 +1,198 @@
+// https://open.kattis.com/problems/cactus
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Cactus {
+    public static class Reader {
+        private final int BUFFER_SIZE = 1 << 12;
+        private int LENGTH = -1;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader(InputStream inputStream) {
+            din = new DataInputStream(inputStream);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c;
+            do {
+                c = read();
+            } while (c <= ' ');
+            while (c <= ' ') c = read();
+            boolean neg = (c == '-');
+            if (neg) c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0');
+            if (neg) return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c;
+            do {
+                c = read();
+            } while (c <= ' ');
+            boolean neg = (c == '-');
+            if (neg) c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0');
+            if (neg) return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c;
+            do {
+                c = read();
+            } while (c <= ' ');
+            boolean neg = (c == '-');
+            if (neg) c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0');
+            if (c == '.') while ((c = read()) >= '0') ret += (c - '0') / (div *= 10);
+            if (neg) return -ret;
+            return ret;
+        }
+
+        public char nextChar() throws IOException {
+            byte c;
+            do {
+                c = read();
+            } while (c <= ' ');
+            return (char) c;
+        }
+
+        public String next() throws IOException {
+            byte[] buf = new byte[LENGTH];
+            int cnt = 0, c;
+            do {
+                c = read();
+            } while (c <= ' ');
+            do {
+                buf[cnt++] = (byte) c;
+            } while ((c = read()) > ' ');
+            return new String(buf, 0, cnt);
+        }
+
+        public String nextLine() throws IOException {
+            byte[] buf = new byte[LENGTH];
+            int cnt = 0, c;
+            do {
+                c = read();
+            } while (c <= ' ');
+            do {
+                buf[cnt++] = (byte) c;
+            } while ((c = read()) >= ' ');
+            return new String(buf, 0, cnt);
+        }
+        
+        public void setLength(int length) {
+            LENGTH = length;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1) buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null) return;
+            din.close();
+        }
+    }
+    
+    private static Reader in;
+    private static PrintWriter out;
+    
+    private static int NUM_OF_TEST_CASES = 1; // TODO CHANGE NUMBER OF TEST CASES
+    
+    // TODO CHANGE FILE NAMES
+    private static final String INPUT_FILE_NAME = "input.txt";
+    private static final String OUTPUT_FILE_NAME = "output.txt";
+    
+    private static boolean stdIn = true;
+    private static boolean stdOut = true;
+    private static boolean crash = true;
+    private static boolean flush = false;
+    
+    public static void main(String[] args) throws Exception {
+        if (stdIn) in = new Reader(System.in);
+        else in = new Reader(INPUT_FILE_NAME);
+        if (stdOut) out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        else out = new PrintWriter(new BufferedWriter(new FileWriter(OUTPUT_FILE_NAME)));
+        for (int i = 1; i <= NUM_OF_TEST_CASES; i++) {
+            try {
+                run(i);
+            } catch (Exception e) {
+                out.println("Exception thrown on test case " + i);
+                e.printStackTrace(out);
+                out.flush();
+                if (crash) throw new Exception();
+            }
+            if (flush) out.flush();
+        }
+        in.close();
+        out.close();
+    }
+    
+    static int N, M, cur = 0;
+    static ArrayList<Integer>[] adj;
+    static int[] pre, low;
+    static boolean[] vis;
+    
+    static boolean dfs(int v, int prev) {
+        int c = 0;
+        pre[v] = low[v] = ++cur;
+        for (int w : adj[v]) {
+            if (pre[w] == -1) {
+                if (!dfs(w, v)) return false;
+                if (pre[v] > low[w] && ++c > 1) return false;
+                low[v] = Math.min(low[v], low[w]);
+            } else {
+                if (vis[w]) return false;
+                if (pre[w] < low[v] && ++c > 1) return false;
+                low[v] = Math.min(low[v], pre[w]);
+            }
+        }
+        vis[v] = true;
+        return v == prev || c == 1;
+    }
+    
+    public static void run(int testCaseNum) throws Exception {
+        N = in.nextInt();
+        M = in.nextInt();
+        adj = new ArrayList[N];
+        pre = new int[N];
+        low = new int[N];
+        vis = new boolean[N];
+        for (int i = 0; i < N; i++) {
+            adj[i] = new ArrayList<Integer>();
+            pre[i] = low[i] = -1;
+            vis[i] = false;
+        }
+        for (int i = 0; i < M; i++) adj[in.nextInt()].add(in.nextInt());
+        if (dfs(0, 0) && cur == N) out.println("YES");
+        else out.println("NO");
+    }
+}
