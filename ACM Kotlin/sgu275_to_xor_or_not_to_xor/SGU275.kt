3c21cf5838e0ad964760d416fdca3dc6e6292b72
wesley-a-leung <wesley.a.leung@gmail.com>
2018-07-26 21:29:32 -0400

    Enchanced some solutions

--- ACM Kotlin/sgu275_to_xor_or_not_to_xor/SGU275.kt
+++ ACM Kotlin/sgu275_to_xor_or_not_to_xor/SGU275.kt
@@ -4,45 +4,168 @@ import java.math.*
 import java.util.*
 
 class Reader {
-    private val In: BufferedReader
-    private var st: StringTokenizer? = null
-    constructor(inputStream: InputStream) {
-        In = BufferedReader(InputStreamReader(inputStream))
-    }
-    constructor(fileName: String) {
-        In = BufferedReader(FileReader(fileName))
-    }
-    fun next(): String {
-        while (st == null || !st!!.hasMoreTokens()) {
-            st = StringTokenizer(In.readLine().trim())
-        }
-        return st!!.nextToken()
-    }
-    fun nextLine(): String {
-        st = null
-        return In.readLine()
-    }
-    fun nextChar(): Char = next()[0]
-    fun nextDouble(): Double = next().toDouble()
-    fun nextInt(): Int = next().toInt()
-    fun nextLong(): Long  = next().toLong()
-    fun close() : Unit = In.close()
-} // Reader class
+    private val BUFFER_SIZE: Int = 1 shl 12
+    private var LENGTH: Int = -1
+    private val din: DataInputStream 
+    private val buffer: ByteArray = ByteArray(BUFFER_SIZE)
+    private var bufferPointer: Int = 0
+    private var bytesRead: Int = 0
 
+    constructor(inputStream: InputStream) {
+        din = DataInputStream(inputStream)
+    }
+
+    constructor(fileName: String) {
+        din = DataInputStream(FileInputStream(fileName))
+    }
+
+    fun nextInt(): Int {
+        var ret: Int = 0
+        var c: Byte
+        do {
+            c = read()
+        } while (c <= 32)
+        var neg: Boolean = c == 45.toByte()
+        if (neg) c = read()
+        do {
+            ret = ret * 10 + c - 48
+            c = read()
+        } while (c >= 48)
+        if (neg) return -ret
+        return ret
+    }
+
+    fun nextLong(): Long {
+        var ret: Long = 0L
+        var c: Byte
+        do {
+            c = read()
+        } while (c <= 32)
+        var neg: Boolean = c == 45.toByte()
+        if (neg) c = read()
+        do {
+            ret = ret * 10 + c - 48
+            c = read()
+        } while (c >= 48)
+        if (neg) return -ret
+        return ret
+    }
+
+    fun nextDouble(): Double {
+        var ret: Double = 0.0
+        var div: Double = 1.0
+        var c: Byte
+        do {
+            c = read()
+        } while (c <= 32)
+        var neg: Boolean = c == 45.toByte()
+        if (neg) c = read()
+        do {
+            ret = ret * 10 + c - 48
+            c = read()
+        } while (c >= 48)
+        if (c == 46.toByte()) {
+            c = read()
+            while (c >= 48) {
+                div *= 10
+                ret += (c - 48) / div
+                c = read()
+            }
+        }
+        if (neg) return -ret
+        return ret
+    }
+
+    fun nextChar(): Char {
+        var c: Byte
+        do {
+            c = read()
+        } while (c <= 32)
+        return c.toChar()
+    }
+
+    fun next(): String {
+        val buf: CharArray = CharArray(LENGTH)
+        var c: Byte
+        var cnt: Int = 0
+        do {
+            c = read()
+        } while (c <= 32)
+        do {
+            buf[cnt++] = c.toChar()
+            c = read()
+        } while (c > 32)
+        return String(buf, 0, cnt)
+    }
+
+    fun nextLine(): String {
+        val buf: CharArray = CharArray(LENGTH)
+        var c: Byte
+        var cnt: Int = 0
+        do {
+            c = read()
+        } while (c <= 32)
+        do {
+            buf[cnt++] = c.toChar()
+            c = read()
+        } while (c >= 32)
+        return String(buf, 0, cnt)
+    }
+
+    fun setLength(length: Int) {
+        LENGTH = length
+    }
+
+    fun hasNext(): Boolean {
+        while (peek() > -1 && peek() <= 32) read()
+        return peek() > -1
+    }
+
+    private fun fillBuffer() {
+        bufferPointer = 0
+        bytesRead = din.read(buffer, bufferPointer, BUFFER_SIZE)
+        if (bytesRead == -1) buffer[0] = -1
+    }
+
+    private fun read(): Byte {
+        if (bufferPointer == bytesRead) fillBuffer()
+        return buffer[bufferPointer++]
+    }
+
+    private fun peek(): Byte {
+        if (bufferPointer == bytesRead) fillBuffer()
+        return buffer[bufferPointer]
+    }
+
+    fun close() {
+        din.close()
+    }
+}
+
+var NUM_OF_TEST_CASES: Int = 1 // TODO CHANGE NUMBER OF TEST CASES
+
+// TODO CHANGE FILE NAMES
 val INPUT_FILE_NAME = "input.txt"
 val OUTPUT_FILE_NAME = "output.txt"
 
-val In: Reader = Reader(System.`in`)
-val Out: PrintWriter = PrintWriter(System.out)
-//val In: Reader = Reader(INPUT_FILE_NAME)
-//val Out: PrintWriter = PrintWriter(OUTPUT_FILE_NAME)
-
-var NUM_OF_TEST_CASES: Int = 1
+val stdIn: Boolean = true
+val stdOut: Boolean = true
+val crash: Boolean = true
 val flush: Boolean = false
 
-fun main(args : Array<String>) {
+val In: Reader = if (stdIn) Reader(System.`in`) else Reader(INPUT_FILE_NAME)
+val Out: PrintWriter = if (stdOut) PrintWriter(System.out) else PrintWriter(OUTPUT_FILE_NAME)
+
+fun main(args: Array<String>) {
     for (i in 1..NUM_OF_TEST_CASES) {
-        run(i)
+        try {
+            run(i)
+        } catch (e: Exception) {
+            System.err.println("Exception thrown on test case $i")
+            e.printStackTrace(System.err)
+            Out.flush()
+            if (crash) throw Exception()
+        }
         if (flush) Out.flush()
     }
     In.close()
@@ -50,19 +173,16 @@ fun main(args : Array<String>) {
 }
 
 fun run(testCaseNum: Int) {
-    var N = In.nextInt()
-    var A = LongArray(N)
-    for (i in 0 until N) A[i] = In.nextLong()
-    var ans = 0L
-    var cur = 0L
+    val N = In.nextInt()
+    val A = LongArray(N, {In.nextLong()})
+    var k = 0
     for (i in 60 downTo 0) {
-        for (j in 0 until N) {
-            if (((A[j] shr i) and 1L) == 1L) {
-                cur = A[j]
-                if (((ans shr i) and 1L) == 0L) ans = ans xor cur
-                for (k in 0 until N) if (((A[k] shr i) and 1L) == 1L) A[k] = A[k] xor cur
-            }
-        }
+        var maxInd = -1
+        for (j in k until N) if ((maxInd == -1 || A[j] > A[maxInd]) && (A[j] shr i) and 1L == 1L) maxInd = j
+        if (maxInd == -1) continue
+        A[k] = A[maxInd].also{A[maxInd] = A[k]}
+        for (j in 0 until N) if (j != k && (A[j] shr i) and 1L == 1L) A[j] = A[j] xor A[k]
+        k++
     }
-    Out.println(ans)
+    Out.println(A.reduce{res, a -> res xor a})
 }
