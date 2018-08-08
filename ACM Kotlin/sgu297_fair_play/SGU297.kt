8763e17a7bfeca538261d442b6d4c8633752694e
wesley-a-leung <wesley.a.leung@gmail.com>
2018-08-08 18:28:40 -0400

    ACM, Codeforces, Bubble Cup Questions

---
+++ ACM Kotlin/sgu297_fair_play/SGU297.kt
@@ -0,0 +1,179 @@
+// https://codeforces.com/problemsets/acmsguru/problem/99999/297
+import java.io.*
+import java.math.*
+import java.util.*
+
+class Reader {
+    private val BUFFER_SIZE: Int = 1 shl 12
+    private var LENGTH: Int = -1
+    private val din: DataInputStream 
+    private val buffer: ByteArray = ByteArray(BUFFER_SIZE)
+    private var bufferPointer: Int = 0
+    private var bytesRead: Int = 0
+
+    constructor(inputStream: InputStream) {
+        din = DataInputStream(inputStream)
+    }
+
+    constructor(fileName: String) {
+        din = DataInputStream(FileInputStream(fileName))
+    }
+
+    fun nextInt(): Int {
+        var ret: Int = 0
+        var c: Byte
+        do {
+            c = read()
+        } while (c <= 32)
+        var neg: Boolean = c == 45.toByte()
+        if (neg) c = read()
+        do {
+            ret = ret * 10 + c - 48
+            c = read()
+        } while (c >= 48)
+        if (neg) return -ret
+        return ret
+    }
+
+    fun nextLong(): Long {
+        var ret: Long = 0L
+        var c: Byte
+        do {
+            c = read()
+        } while (c <= 32)
+        var neg: Boolean = c == 45.toByte()
+        if (neg) c = read()
+        do {
+            ret = ret * 10 + c - 48
+            c = read()
+        } while (c >= 48)
+        if (neg) return -ret
+        return ret
+    }
+
+    fun nextDouble(): Double {
+        var ret: Double = 0.0
+        var div: Double = 1.0
+        var c: Byte
+        do {
+            c = read()
+        } while (c <= 32)
+        var neg: Boolean = c == 45.toByte()
+        if (neg) c = read()
+        do {
+            ret = ret * 10 + c - 48
+            c = read()
+        } while (c >= 48)
+        if (c == 46.toByte()) {
+            c = read()
+            while (c >= 48) {
+                div *= 10
+                ret += (c - 48) / div
+                c = read()
+            }
+        }
+        if (neg) return -ret
+        return ret
+    }
+
+    fun nextChar(): Char {
+        var c: Byte
+        do {
+            c = read()
+        } while (c <= 32)
+        return c.toChar()
+    }
+
+    fun next(): String {
+        val buf: CharArray = CharArray(LENGTH)
+        var c: Byte
+        var cnt: Int = 0
+        do {
+            c = read()
+        } while (c <= 32)
+        do {
+            buf[cnt++] = c.toChar()
+            c = read()
+        } while (c > 32)
+        return String(buf, 0, cnt)
+    }
+
+    fun nextLine(): String {
+        val buf: CharArray = CharArray(LENGTH)
+        var c: Byte
+        var cnt: Int = 0
+        do {
+            c = read()
+        } while (c <= 32)
+        do {
+            buf[cnt++] = c.toChar()
+            c = read()
+        } while (c >= 32)
+        return String(buf, 0, cnt)
+    }
+
+    fun setLength(length: Int) {
+        LENGTH = length
+    }
+
+    fun hasNext(): Boolean {
+        while (peek() > -1 && peek() <= 32) read()
+        return peek() > -1
+    }
+
+    private fun fillBuffer() {
+        bufferPointer = 0
+        bytesRead = din.read(buffer, bufferPointer, BUFFER_SIZE)
+        if (bytesRead == -1) buffer[0] = -1
+    }
+
+    private fun read(): Byte {
+        if (bufferPointer == bytesRead) fillBuffer()
+        return buffer[bufferPointer++]
+    }
+
+    private fun peek(): Byte {
+        if (bufferPointer == bytesRead) fillBuffer()
+        return buffer[bufferPointer]
+    }
+
+    fun close() {
+        din.close()
+    }
+}
+
+var NUM_OF_TEST_CASES: Int = 1 // TODO CHANGE NUMBER OF TEST CASES
+
+// TODO CHANGE FILE NAMES
+val INPUT_FILE_NAME = "input.txt"
+val OUTPUT_FILE_NAME = "output.txt"
+
+val stdIn: Boolean = true
+val stdOut: Boolean = true
+val crash: Boolean = true
+val flush: Boolean = false
+
+val In: Reader = if (stdIn) Reader(System.`in`) else Reader(INPUT_FILE_NAME)
+val Out: PrintWriter = if (stdOut) PrintWriter(System.out) else PrintWriter(OUTPUT_FILE_NAME)
+
+fun main(args: Array<String>) {
+    for (i in 1..NUM_OF_TEST_CASES) {
+        try {
+            run(i)
+        } catch (e: Exception) {
+            System.err.println("Exception thrown on test case $i")
+            e.printStackTrace(System.err)
+            Out.flush()
+            if (crash) throw Exception()
+        }
+        if (flush) Out.flush()
+    }
+    In.close()
+    Out.close()
+}
+
+fun run(testCaseNum: Int) {
+    val N = In.nextInt()
+    val sum = (0 until In.nextInt()).fold(0) { acc, _ -> acc + In.nextInt() }
+    Out.println((N - (((sum + N - 1) / N) * N) + sum) % N)
+}
---
+++ Bubble Cup C++/bbc08i_robots_protection/BBC08I.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 0x3f3f3f3f3f3f3f3f
+#define D_INF numeric_limits<double>::infinity()
+#define MIN(a, b) ((a) = min((a), (b)))
+#define MAX(a, b) ((a) = max((a), (b)))
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+#define all(a) (a).begin(), (a).end()
+#define For(i, a, b) for (auto i = (a); i < (b); i++)
+#define FOR(i, b) For(i, 0, b)
+#define Rev(i, a, b) for (auto i = (a); i > (b); i--)
+#define REV(i, a) Rev(i, a, -1)
+#define sz(a) ((int) (a).size())
+#define nl '\n'
+#define sp ' '
+#define ll long long
+#define ld long double
+#define uint unsigned int
+#define ull unsigned long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pill pair<int, ll>
+#define plli pair<ll, int>
+#define pdd pair<double, double>
+#define uset unordered_set
+#define umap unordered_map
+#define pq priority_queue
+template<typename T> using minpq = pq<T, vector<T>, greater<T>>;
+template<typename T> using maxpq = pq<T, vector<T>, less<T>>;
+template<typename T1,typename T2,typename H1=hash<T1>,typename H2=hash<T2>>struct pair_hash{size_t operator()(const pair<T1,T2>&p)const{return 31*H1{}(p.first)+H2{}(p.second);}};
+
+#define _bufferSize 4096
+#define _maxNumLength 128
+char _inputBuffer[_bufferSize+1],*_inputPtr=_inputBuffer,_outputBuffer[_bufferSize],_c,_sign,*_tempInputBuf=nullptr,_numBuf[_maxNumLength],_tempOutputBuf[_maxNumLength],_fill=' ';
+const char*_delimiter=" ";int _cur,_outputPtr=0,_numPtr=0,_precision=6,_width=0,_tempOutputPtr=0,_cnt;ull _precisionBase=1000000;
+#define _peekchar() (*_inputPtr?*_inputPtr:(_inputBuffer[fread(_inputPtr=_inputBuffer,1,_bufferSize,stdin)]='\0',*_inputPtr))
+#define _getchar() (*_inputPtr?*_inputPtr++:(_inputBuffer[fread(_inputPtr=_inputBuffer,1,_bufferSize,stdin)]='\0',*_inputPtr++))
+#define _hasNext() (*_inputPtr||!feof(stdin))
+#define _readSignAndNum(x) do{(x)=_getchar();}while((x)<=' ');_sign=(x)=='-';if(_sign)(x)=_getchar();for((x)-='0';(_c=_getchar())>='0';(x)=(x)*10+_c-'0')
+#define _readFloatingPoint(x,T) for(T _div=1.0;(_c=_getchar())>='0';(x)+=(_c-'0')/(_div*=10))
+#define rc(x) do{do{(x)=_getchar();}while((x)<=' ');}while(0)
+#define ri(x) do{_readSignAndNum(x);if(_sign)(x)=-(x);}while(0)
+#define rd(x) do{_readSignAndNum(x);if(_c=='.')_readFloatingPoint(x,double);if(_sign)(x)=-(x);}while(0)
+#define rld(x) do{_readSignAndNum(x);if(_c=='.')_readFloatingPoint(x,ld);if(_sign)(x)=-(x);}while(0)
+#define rcs(x) do{_cur=0;do{_c=_getchar();}while(_c<=' ');do{(x)[_cur++]=_c;}while((_c=_getchar())>' ');(x)[_cur]='\0';}while(0)
+#define rs(x) do{if(!_tempInputBuf)assert(0);rcs(_tempInputBuf);(x)=string(_tempInputBuf,_cur);}while(0)
+#define rcln(x) do{_cur=0;do{_c=_getchar();}while(_c<=' ');do{(x)[_cur++]=_c;}while((_c=_getchar())>=' ');(x)[_cur]='\0';}while(0)
+#define rln(x) do{if(!_tempInputBuf)assert(0);rcln(_tempInputBuf);(x)=string(_tempInputBuf,_cur);}while(0)
+#define setLength(x) do{if(_tempInputBuf)delete[](_tempInputBuf);_tempInputBuf=new char[(x)+1];}while(0)
+void read(int&x){ri(x);}void read(uint&x){ri(x);}void read(ll&x){ri(x);}void read(ull&x){ri(x);}void read(double&x){rd(x);}void read(ld&x){rld(x);}
+void read(char&x){rc(x);}void read(char*x){rcs(x);}void read(string&x){rs(x);}bool hasNext(){while(_hasNext()&&_peekchar()<=' ')_getchar();return _hasNext();}
+template<typename T,typename...Ts>void read(T&&x,Ts&&...xs){read(x);read(forward<Ts>(xs)...);}
+#define _flush() do{_flushBuf();fflush(stdout);}while(0)
+#define _flushBuf() (fwrite(_outputBuffer,1,_outputPtr,stdout),_outputPtr=0)
+#define _putchar(x) (_outputBuffer[_outputPtr==_bufferSize?_flushBuf():_outputPtr]=(x),_outputPtr++)
+#define _writeTempBuf(x) (_tempOutputBuf[_tempOutputPtr++]=(x))
+#define _writeOutput() for(int _i=0;_i<_tempOutputPtr;_putchar(_tempOutputBuf[_i++]));_tempOutputPtr=0
+#define _writeNum(x,T,digits) _cnt=0;for(T _y=(x);_y;_y/=10,_cnt++)_numBuf[_numPtr++]='0'+_y%10;for(;_cnt<digits;_cnt++)_numBuf[_numPtr++]='0';_flushNumBuf();
+#define _writeFloatingPoint(x,T) ull _I=(ull)(x);ull _F=((x)-_I)*_precisionBase+(T)(0.5);if(_F>=_precisionBase){_I++;_F=0;}_writeNum(_I,ull,1);_writeTempBuf('.');_writeNum(_F,ull,_precision)
+#define _checkFinite(x) if(std::isnan(x)){wcs("NaN");}else if(std::isinf(x)){wcs("Inf");}
+#define _flushNumBuf() for(;_numPtr;_writeTempBuf(_numBuf[--_numPtr]))
+#define _fillBuf(x) for(int _i=0;_i<(x);_i++)_putchar(_fill)
+#define _flushTempBuf() int _tempLen=_tempOutputPtr;_fillBuf(_width-_tempLen);_writeOutput();_fillBuf(-_width-_tempLen)
+#define wb(x) do{if(x)_writeTempBuf('1');else _writeTempBuf('0');_flushTempBuf();}while(0)
+#define wc(x) do{_writeTempBuf(x); _flushTempBuf();}while(0)
+#define wi(x) do{if((x)<0){_writeTempBuf('-');_writeNum(-(x),uint,1);}else{_writeNum(x,uint,1);}_flushTempBuf();}while(0)
+#define wll(x) do{if((x)<0){_writeTempBuf('-');_writeNum(-(x),ull,1);}else{_writeNum(x,ull,1);}_flushTempBuf();}while(0)
+#define wd(x) do{_checkFinite(x)else if((x)<0){_writeTempBuf('-');_writeFloatingPoint(-(x),double);}else{_writeFloatingPoint(x,double);}_flushTempBuf();}while(0)
+#define wld(x) do{_checkFinite(x)else if((x)<0){_writeTempBuf('-');_writeFloatingPoint(-(x),ld);}else{_writeFloatingPoint(x,ld);}_flushTempBuf();}while(0)
+#define wcs(x) do{int _slen=strlen(x);_fillBuf(_width-_slen);for(const char*_p=(x);*_p;_putchar(*_p++));_fillBuf(-_width-_slen);}while(0)
+#define ws(x) do{_fillBuf(_width-(int)(x).length());for(int _i=0;_i<(int)(x).length();_putchar(x[_i++]));_fillBuf(-_width-(int)(x).length());}while(0)
+#define setPrecision(x) do{_precision=(x);_precisionBase=1;for(int _i=0;_i<(x);_i++,_precisionBase*=10);}while(0)
+#define setDelimiter(x) do{_delimiter=(x);}while(0)
+#define setWidth(x) do{_width=(x);}while(0)
+#define setFill(x) do{_fill=(x);}while(0)
+void write(bool x){wb(x);}void write(int x){wi(x);}void write(uint x){wi(x);}void write(ll x){wll(x);}void write(ull x){wll(x);}void write(double x){wd(x);}void write(ld x){wld(x);}
+void write(char x){wc(x);}void write(char*x){wcs(x);}void write(const char*x){wcs(x);}void write(string&x){ws(x);}
+template<typename T,typename...Ts>void write(T&&x,Ts&&...xs){write(x);for(const char*_p=_delimiter;*_p;_putchar(*_p++));write(forward<Ts>(xs)...);}
+void writeln(){_putchar('\n');}template<typename...Ts>void writeln(Ts&&...xs){write(forward<Ts>(xs)...);_putchar('\n');}
+void flush(){_flush();}class IOManager{public:~IOManager(){flush();}};unique_ptr<IOManager>iomanager;
+
+#define MAXN 5005
+
+int N, Q, ***BIT;
+
+void update(int a, int x, int y, int v) {
+    for (int i = x; i <= MAXN; i += i & -i) for (int j = y; j <= (a == 0 ? MAXN : 2 * MAXN); j += j & -j) BIT[a][i][j] += v;
+}
+
+int rsq(int a, int x, int y) {
+    int sum = 0;
+    for (int i = x; i > 0; i -= i & -i) for (int j = y; j > 0; j -= j & -j) sum += BIT[a][i][j];
+    return sum;
+}
+
+int main() {
+//    freopen("in.txt", "r", stdin);
+//    freopen("out.txt", "w", stdout);
+    iomanager.reset(new IOManager());
+    read(N, Q);
+    BIT = new int**[3];
+    FOR(i, 3) {
+        BIT[i] = new int*[MAXN + 1];
+        FOR(j, MAXN + 1) {
+            if (i == 0) {
+                BIT[i][j] = new int[MAXN + 1];
+                FOR(k, MAXN + 1) BIT[i][j][k] = 0;
+            } else {
+                BIT[i][j] = new int[MAXN * 2 + 1];
+                FOR(k, 2 * MAXN + 1) BIT[i][j][k] = 0;
+            }
+        }
+    }
+    int t, d, x, y, L;
+    FOR(q, Q) {
+        read(t);
+        if (t == 1) {
+            read(d, x, y, L);
+            if (L == 0) {
+                update(0, x, y, 1);
+                update(0, x + 1, y, -1);
+                update(0, x, y + 1, -1);
+                update(0, x + 1, y + 1, 1);
+            } else if (d == 1) {
+                update(0, x, y, 1);
+                update(0, x + L + 1, y, -1);
+                update(1, x, x + y + L + 1, -1);
+                update(1, x + L + 1, x + y + L + 1, 1);
+            } else if (d == 2) {
+                update(0, x, y + 1, -1);
+                update(0, x + L + 1, y + 1, 1);
+                update(2, x, MAXN - x + y - L, 1);
+                update(2, x + L + 1, MAXN - x + y - L, -1);
+            } else if (d == 3) {
+                update(0, x - L, y, 1);
+                update(0, x + 1, y, -1);
+                update(2, x - L, MAXN - x + y + L + 1, -1);
+                update(2, x + 1, MAXN - x + y + L + 1, 1);
+            } else {
+                update(0, x - L, y + 1, -1);
+                update(0, x + 1, y + 1, 1);
+                update(1, x - L, x + y - L, 1);
+                update(1, x + 1, x + y - L, -1);
+            }
+        } else {
+            read(x, y);
+            writeln(rsq(0, x, y) + rsq(1, x, x + y) + rsq(2, x, MAXN - x + y));
+        }
+    }
+    return 0;
+}
